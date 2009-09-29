0e9d16db16ef7ef009b2d8fa8a6f13ff7eab9e04
wakefield <wakefield@bd62deb4-2030-0410-8420-fdd75d726ec8>
2009-09-29 23:20:01 +0000

    pa wtf?

--- types/test/test_portaudio.c
+++ types/test/test_portaudio.c
@@ -7,7 +7,8 @@ PaError err;
 AlloLatticeHeader latticetype;
 AlloLattice inputlattice, outputlattice;
 
-double phase = 0;
+int phase = 0;
+double pincr = 0.06;
 
 int callback(const void *input, void *output, unsigned long frameCount, const PaStreamCallbackTimeInfo* timeInfo, PaStreamCallbackFlags statusFlags, void *userData ) 
 {
@@ -18,18 +19,15 @@ int callback(const void *input, void *output, unsigned long frameCount, const Pa
 	/* do any processing here: */
 	assert(frameCount == outputlattice.header.dim[1]);
 	
-	float * outf = (float *)output;
-	for (int i=0; i<frameCount; i++) {
-		//outf[i] = 1.f;
+	char * outp = outputlattice.data.ptr;
+	int frame_stride = outputlattice.header.stride[1];
+	for (int i=0; i<outputlattice.header.dim[1]; i++) {
+		float * out = (float *)(outp + frame_stride * i);
+		out[1] = sin(pincr * (phase+i));
+		phase += 0.01;
 	}
 	
-	char * outp = outputlattice.data.ptr;
-	int channel_stride = outputlattice.header.stride[0];
-	for (int i=0; i<outputlattice.header.dim[1]; i++) {
-		float * out = (float *)(outp + channel_stride * i);
-		out[1] = sinf(phase); /* right channel */
-		phase += 0.001;
-	}
+	phase += frameCount;
 
 	return 0;
 }
@@ -58,7 +56,7 @@ int main(int argc, char * argv) {
 	latticetype.dim[0] = 2; /* 2 channels */
 	latticetype.dim[1] = 64; /* block size */
 	latticetype.stride[0] = latticetype.components * allo_type_size(latticetype.type); 
-	latticetype.stride[1] = latticetype.stride[1] * latticetype.dim[0]; 
+	latticetype.stride[1] = latticetype.stride[0] * latticetype.dim[0]; 
 	
 	allo_lattice_setheader(&inputlattice, &latticetype);
 	allo_lattice_setheader(&outputlattice, &latticetype);
@@ -66,7 +64,7 @@ int main(int argc, char * argv) {
 	err = Pa_StartStream(stream);
 	if (err != paNoError) goto out;
 	
-	Pa_Sleep(1000);
+	Pa_Sleep(2000);
 	
 	err = Pa_StopStream(stream);
 	if (err != paNoError) goto out;
