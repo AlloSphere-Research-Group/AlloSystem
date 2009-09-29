811f876c948b4e6d065d11c11c0d57466e770e6f
wakefield <wakefield@bd62deb4-2030-0410-8420-fdd75d726ec8>
2009-09-29 23:12:54 +0000

    pa wtf?

--- types/test/test_portaudio.c
+++ types/test/test_portaudio.c
@@ -7,6 +7,8 @@ PaError err;
 AlloLatticeHeader latticetype;
 AlloLattice inputlattice, outputlattice;
 
+double phase = 0;
+
 int callback(const void *input, void *output, unsigned long frameCount, const PaStreamCallbackTimeInfo* timeInfo, PaStreamCallbackFlags statusFlags, void *userData ) 
 {
 	/* convert stream data to AlloLatticeTy */
@@ -14,6 +16,20 @@ int callback(const void *input, void *output, unsigned long frameCount, const Pa
 	outputlattice.data.ptr = output;
 	
 	/* do any processing here: */
+	assert(frameCount == outputlattice.header.dim[1]);
+	
+	float * outf = (float *)output;
+	for (int i=0; i<frameCount; i++) {
+		//outf[i] = 1.f;
+	}
+	
+	char * outp = outputlattice.data.ptr;
+	int channel_stride = outputlattice.header.stride[0];
+	for (int i=0; i<outputlattice.header.dim[1]; i++) {
+		float * out = (float *)(outp + channel_stride * i);
+		out[1] = sinf(phase); /* right channel */
+		phase += 0.001;
+	}
 
 	return 0;
 }
@@ -37,10 +53,12 @@ int main(int argc, char * argv) {
 	/* configure lattice type for audio IO */
 	latticetype.type = AlloFloat32Ty;
 	/* note that portaudio by default gives us interleaved data */
-	latticetype.components = 2; /* 2 channels */
-	latticetype.dimcount = 1; /* 1 dim */
-	latticetype.dim[0] = 64; /* block size */
+	latticetype.components = 1; /* single sample units */
+	latticetype.dimcount = 2; /* 1 dim */
+	latticetype.dim[0] = 2; /* 2 channels */
+	latticetype.dim[1] = 64; /* block size */
 	latticetype.stride[0] = latticetype.components * allo_type_size(latticetype.type); 
+	latticetype.stride[1] = latticetype.stride[1] * latticetype.dim[0]; 
 	
 	allo_lattice_setheader(&inputlattice, &latticetype);
 	allo_lattice_setheader(&outputlattice, &latticetype);
