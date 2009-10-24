f74239bccbc9268cfc3c06f294b239026d465ee3
wakefield <wakefield@bd62deb4-2030-0410-8420-fdd75d726ec8>
2009-10-24 19:40:26 +0000

    all 3 mainloop examples committed

---
+++ projects/MainLoopExampleTool/main.cpp
@@ -0,0 +1,21 @@
+#include "al_mainloop.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void tick(al_nsec time, void * userdata) {
+	
+	al_sec t = time * al_time_ns2s;
+	printf("%f\n", al_main_time_sec());
+	if (t > 3.0) {
+		al_main_exit();
+	}
+}
+
+int main (int argc, char * argv[]) {
+	
+	// enter main loop
+	al_main_enter(0.01, tick, NULL);
+	
+	return al_main_quit();
+}
