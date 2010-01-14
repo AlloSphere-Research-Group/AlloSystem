21a947b59931b4e2ddb337ebd2da9d94ce9e75bd
l.putnam <l.putnam@bd62deb4-2030-0410-8420-fdd75d726ec8>
2010-01-14 08:49:45 +0000

    added license to new header files

--- include/io/al_AudioIO.h
+++ include/io/al_AudioIO.h
@@ -1,6 +1,33 @@
 #ifndef INCLUDE_AL_AUDIO_IO_H
 #define INCLUDE_AL_AUDIO_IO_H
 
+/*
+ *	Audio device and input/output streaming
+ *  AlloSphere Research Group / Media Arts & Technology, UCSB, 2009
+ */
+
+/*
+	Copyright (C) 2006-2008. The Regents of the University of California (REGENTS). 
+	All Rights Reserved.
+
+	Permission to use, copy, modify, distribute, and distribute modified versions
+	of this software and its documentation without fee and without a signed
+	licensing agreement, is hereby granted, provided that the above copyright
+	notice, the list of contributors, this paragraph and the following two paragraphs 
+	appear in all copies, modifications, and distributions.
+
+	IN NO EVENT SHALL REGENTS BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
+	SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS, ARISING
+	OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF REGENTS HAS
+	BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+	REGENTS SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+	THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+	PURPOSE. THE SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF ANY, PROVIDED
+	HEREUNDER IS PROVIDED "AS IS". REGENTS HAS  NO OBLIGATION TO PROVIDE
+	MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+*/
+
 /*	This is a simple example demonstrating how to set up a callback
 	and process input and output buffers.
 	
