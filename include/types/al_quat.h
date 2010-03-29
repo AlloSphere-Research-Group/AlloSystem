c90168de73823261f815ad713199980e348e198a
wakefield <wakefield@bd62deb4-2030-0410-8420-fdd75d726ec8>
2010-03-29 19:09:09 +0000

    quat

---
+++ include/types/al_quat.h
@@ -0,0 +1,21 @@
+#ifndef INCLUDE_AL_QUAT_H
+#define INCLUDE_AL_QUAT_H
+
+#include <math.h>
+
+#define QUAT_ACCURACY_MAX (1.000001)
+#define QUAT_ACCURACY_MIN (0.999999)
+#define QUAT_EPSILON (0.000001)
+#define QUAT_RAD2DEG (57.29577951308)
+#define QUAT_DEG2RAD (0.01745329252)
+#define QUAT_DEG2RAD_BY2 (0.00872664626)
+#define QUAT_PIBY2 (1.57079632679)
+
+typedef struct al_quat {
+	double w;
+	double x;
+	double y;
+	double z;
+} al_quat;
+
+#endif /* include guard */
--- include/types/al_quat.hpp
+++ include/types/al_quat.hpp
@@ -1,20 +1,11 @@
-#ifndef INCLUDE_AL_QUAT_H
-#define INCLUDE_AL_QUAT_H
+#ifndef INCLUDE_AL_QUAT_HPP
+#define INCLUDE_AL_QUAT_HPP
 
-#include <math.h>
-
-#define QUAT_ACCURACY_MAX (1.000001)
-#define QUAT_ACCURACY_MIN (0.999999)
-#define QUAT_EPSILON (0.000001)
-#define QUAT_RAD2DEG (57.29577951308)
-#define QUAT_DEG2RAD (0.01745329252)
-#define QUAT_DEG2RAD_BY2 (0.00872664626)
-#define QUAT_PIBY2 (1.57079632679)
+#include "al_quat.h"
 
 namespace al {
 
-class Quat {
-public:
+class Quat : public al_quat {
 	Quat(double w = 1.0, double x = 0.0, double y = 0.0, double z = 0.0);
 	Quat(Quat * src);
 
