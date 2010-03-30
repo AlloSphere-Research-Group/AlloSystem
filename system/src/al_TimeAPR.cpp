60c774a186e1e8b4bf81f788a2c04be95d7d1b74
l.putnam <l.putnam@bd62deb4-2030-0410-8420-fdd75d726ec8>
2010-03-30 21:57:54 +0000

    fixed nsec conversion error

--- system/src/al_TimeAPR.cpp
+++ system/src/al_TimeAPR.cpp
@@ -1,15 +1,31 @@
 #include "system/al_Time.h"
 #include "apr_time.h"
 
+/*
+APR API documentation
+--------------------------------------------------------------------------------
+typedef apr_int64_t apr_time_t
+number of microseconds since 00:00:00 january 1, 1970 UTC
+--------------------------------------------------------------------------------
+void apr_sleep	(	apr_interval_time_t 	t	 ) 	
+Sleep for the specified number of micro-seconds.
+
+Parameters:
+t 	desired amount of time to sleep.
+Warning:
+May sleep for longer than the specified time.
+--------------------------------------------------------------------------------
+*/
+
 
 al_sec al_time() {
-	const apr_time_t t = apr_time_now(); // microseconds
-	return ((al_sec)t) * 1.0e-6; 
+	const apr_time_t t = apr_time_now();	// microseconds
+	return ((al_sec)t) * 1.0e-6;			// 1 s / 1e6 us
 }
 
 al_nsec al_time_nsec() {
-	const apr_time_t t = apr_time_now(); // microseconds
-	return ((al_nsec)t) * 1e3; 
+	const apr_time_t t = apr_time_now();	// microseconds
+	return ((al_nsec)t) * 1e3;				// 1000 ns / 1 us
 }
 
 void al_sleep(al_sec v) {
@@ -18,7 +34,7 @@ void al_sleep(al_sec v) {
 }
 
 void al_sleep_nsec(al_nsec v) {
-	apr_interval_time_t t = v * 1e3;
+	apr_interval_time_t t = v * 1e-3;
 	apr_sleep(t);
 }
 
