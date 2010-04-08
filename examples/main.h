4133de94da9558f274bb4c7e304757eb30b12c71
wakefield <wakefield@bd62deb4-2030-0410-8420-fdd75d726ec8>
2010-04-08 00:04:39 +0000

    tidy up projects folder

--- examples/main.h
+++ examples/main.h
@@ -1,12 +1,28 @@
 #ifndef INCLUDE_AL_MAIN_H
 #define INCLUDE_AL_MAIN_H 1
 
+#include <assert.h>
+#include <stdio.h>
+
 /* allocore */
-#include "system/al_mainloop.h"
-#include "system/al_time.h"
-#include "types/al_pq.h"
-#include "types/al_tube.h"
+#include "io/al_AudioIO.hpp"
+#include "io/al_WindowGL.hpp"
+#include "protocol/al_OSC.hpp"
+#include "protocol/al_OSCAPR.hpp"
+#include "protocol/al_Serialize.hpp"
+#include "system/al_Time.h"
+#include "system/al_Time.hpp"
+#include "system/al_Thread.hpp"
+#include "system/al_MainLoop.h"
 #include "types/al_types.h"
+#include "types/al_types.hpp"
+#include "types/al_Vec.hpp"
+
+#ifdef AL_UT_NOPRINT	/* disable printing to stdout */
+	#define UT_PRINTF	//
+#else
+	#define UT_PRINTF	printf
+#endif
 
 /* Apache Portable Runtime */
 #include "apr_general.h"
