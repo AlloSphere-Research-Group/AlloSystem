7d62b55a5b978c88c68fddaaba4c7527bd839efe
l.putnam <l.putnam@bd62deb4-2030-0410-8420-fdd75d726ec8>
2010-05-21 00:52:56 +0000

    Made Vec default ctor initialize elements to default values to avoid difficult to track errors due to garbage data

--- include/math/al_Vec.hpp
+++ include/math/al_Vec.hpp
@@ -67,12 +67,8 @@ public:
 	/// Element values
 	T elems[N];
 
-
-	/// Constructor. Elements are not initialized.
-	Vec(){}
-
 	/// @param[in] v		value to initialize all elements to
-	Vec(const T& v){ set(v); }
+	Vec(const T& v=T()){ set(v); }
 
 	/// @param[in] v		vector to initialize all elements to
 	template <class T2>
@@ -540,8 +536,8 @@ struct Vec3 : public Vec<3,T> {
 
 	typedef Vec<3,T> Base;
 
-	Vec3(const Base& v){ *this = v; }
-	Vec3(const T& x=T(), const T& y=T(), const T& z=T()){ set(x,y,z); }
+	Vec3(const Base& v=Base()){ *this = v; }
+	Vec3(const T& x, const T& y, const T& z){ set(x,y,z); }
 
 	Vec3& operator= (const Base& v){ Base::set(v); return *this; }
 };
@@ -553,8 +549,8 @@ struct Vec4 : public Vec<4,T> {
 
 	typedef Vec<4,T> Base;
 	
-	Vec4(const Base& v){ *this = v; }
-	Vec4(const T& x=T(), const T& y=T(), const T& z=T(), const T& w=T()){ set(x,y,z,w); }
+	Vec4(const Base& v=Base()){ *this = v; }
+	Vec4(const T& x, const T& y, const T& z, const T& w){ set(x,y,z,w); }
 	
 	Vec4& operator= (const Base& v){ Base::set(v); return *this; }
 
