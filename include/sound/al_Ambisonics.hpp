b064f523c4ac9e12a12be5ce607d144b0a2398c1
l.putnam <l.putnam@bd62deb4-2030-0410-8420-fdd75d726ec8>
2010-06-30 06:44:01 +0000

    Untemplatized Ambisonics

--- include/sound/al_Ambisonics.hpp
+++ include/sound/al_Ambisonics.hpp
@@ -2,40 +2,26 @@
 #define INCLUDE_AL_AMBISONICS_HPP
 
 #include <stdio.h>
-
-
-#ifdef USE_GAMMA
-	#include "scl.h"
-	#define COS			gam::scl::cosT8
-	#define SIN			gam::scl::sinT7
-	#define WRAP(phase)	phase = gam::scl::wrapPhase(phase)
-#else
-	#include <math.h>
-	#define COS			cos
-	#define SIN			sin
-	#define WRAP(phase)
-#endif
-
-
 //#define MAX_ORDER 3
-
 
 namespace al{
 
 /// Ambisonic base class
 class AmbiBase{
 public:
-	AmbiBase(int dim, int order)
-	:	mDim(dim)
-	{	this->order(order); }
 
-	virtual ~AmbiBase(){}
+	/// @param[in] dim		number of spatial dimensions (2 or 3)
+	/// @param[in] order	highest spherical harmonic order
+	AmbiBase(int dim, int order);
 
-	int dim() const;
-	int order() const;
-	
+	virtual ~AmbiBase();
+
+	int dim() const { return mDim; }
+	int order() const { return mOrder; }
+	const float * weights() const { return mWeights; }
+
 	/// Returns total number of Ambisonic domain channels
-	int numChannels() const;
+	int channels() const { return mChannels; }
 	
 	void order(int order);		///< Set the order.
 	
@@ -43,15 +29,12 @@ public:
 	
 	static int channelsToUniformOrder(int channels);
 	
-	template <class T>
-	static void encodeWeightsFuMa(T * weights, int dim, int order, T azimuth, T elevation);
+	static void encodeWeightsFuMa(float * weights, int dim, int order, float azimuth, float elevation);
+	static void encodeWeightsFuMa(float * ws, int dim, int order, float x, float y, float z);
 	
 	/// Brute force 3rd order.  Weights must be of size 16.
-	template <class T>
-	static void encodeWeightsFuMa16(T * weights, T azimuth, T elevation);
-	
-	template <class T>
-	static void encodeWeightsFuMa16(T * ws, T xn, T yn, T zn);
+	static void encodeWeightsFuMa16(float * weights, float azimuth, float elevation);
+	static void encodeWeightsFuMa16(float * ws, float x, float y, float z);
 	
 	static int orderToChannels(int dim, int order);
 	static int orderToChannelsH(int orderH);
@@ -60,34 +43,32 @@ public:
 protected:
 	int mDim;			// dimensions - 2d or 3d
 	int mOrder;			// order - 0th, 1st, 2nd, or 3rd
-	int mNumChannels;	// cached for efficiency
-
-	static double c1_sqrt2(){ return 0.707106781186548; }
-	static double c8_11(){ return 8./11.; }
-	static double c40_11(){ return 40./11.; }
+	int mChannels;		// cached for efficiency
+	float * mWeights;	// weights for each ambi channel
+	
+	static void resize(float * a, int n);
 };
 
 
 /// Higher Order Ambisonic Decoding class
-template <class T>
 class AmbiDecode : public AmbiBase{
 public:
 	AmbiDecode(int dim, int order, int numSpeakers, int flavor=1);
 	virtual ~AmbiDecode();
 
-	T decode(int speakerNum);	///< Decode speaker's sample from stored ambisonic frame.
+	float decode(int speakerNum);	///< Decode speaker's sample from stored ambisonic frame.
 
-	void decode(T ** dec, const T ** enc, int numDecFrames);
+	void decode(float ** dec, const float ** enc, int numDecFrames);
 
 	void flavor(int type);
 	void numSpeakers(int num);	///< Set number of speakers.  Positions are zeroed upon resize.
-	void setSpeaker(int index, T azimuth, T elevation=0);
-	void setSpeakerDegrees(int index, T azimuth, T elevation=0);
+	void setSpeaker(int index, float azimuth, float elevation=0);
+	void setSpeakerDegrees(int index, float azimuth, float elevation=0);
 	void zero();				///< Zeroes out internal ambisonic frame.
 
-	T * azimuths();				///< Returns pointer to speaker azimuths.
-	T * elevations();			///< Returns pointer to speaker elevations.
-	T * frame() const;			///< Returns pointer to ambisonic channel frame used by decode(int)
+	float * azimuths();				///< Returns pointer to speaker azimuths.
+	float * elevations();			///< Returns pointer to speaker elevations.
+	float * frame() const;			///< Returns pointer to ambisonic channel frame used by decode(int)
 	int numSpeakers();		///< Returns number of speakers.
 	int flavor();			///< Returns decode flavor.
 
@@ -95,54 +76,56 @@ public:
 	
 	void print(FILE * fp = stdout, const char * append = "\n");
 	
-	T decodeWeight(int speaker, int channel){ 
-		return mWChan[channel] * mDecodeMatrix[speaker * mNumChannels + channel];
+	float decodeWeight(int speaker, int channel){ 
+		return mWeights[channel] * mDecodeMatrix[speaker * channels() + channel];
 	}
 
 protected:
 	int mNumSpeakers;
-	int mFlavor;		// decode flavor
+	int mFlavor;				// decode flavor
 
-	T * mDecodeMatrix;		// deccoding matrix for each ambi channel & speaker
-							// cols are channels and rows are speakers								
-	T mWOrder[5];			// weights for each order
-	T * mWChan;				// weights for each ambi channel
-	T * mPositions;			// speakers' azimuths + elevations
-	T * mFrame;				// an ambisonic channel frame used for decode(int)
+	float * mDecodeMatrix;		// deccoding matrix for each ambi channel & speaker
+								// cols are channels and rows are speakers								
+	float mWOrder[5];			// weights for each order
+	float * mPositions;			// speakers' azimuths + elevations
+	float * mFrame;				// an ambisonic channel frame used for decode(int)
 
 	void updateChanWeights();
 	void resizeArrays(int numChannels, int numSpeakers);
 
-	T decode(T * encFrame, int encNumChannels, int speakerNum);	// is this useful?
+	float decode(float * encFrame, int encNumChannels, int speakerNum);	// is this useful?
 	
-	static T flavorWeights[4][5][5];
+	static float flavorWeights[4][5][5];
 };
 
 
 /// Higher Order Ambisonic encoding class
-template <class T>
 class AmbiEncode : public AmbiBase{
 public:
-	AmbiEncode(int dim, int order);
-	virtual ~AmbiEncode();
+
+	/// Encode input sample and set decoder frame.
+	void encode   (const AmbiDecode &dec, float input);
 	
-	void encode   (const AmbiDecode<T> &dec, T input);	///< Encode input sample and set decoder frame.
-	void encodeAdd(const AmbiDecode<T> &dec, T input);	///< Encode input sample and add to decoder frame.
+	/// Encode input sample and add to decoder frame.
+	void encodeAdd(const AmbiDecode &dec, float input);
+
+	template <class XYZ>
+	void encode(float ** ambiChans, const XYZ * pos, const float * input, int numFrames){	
+		for(int c=0; c<channels(); ++c){
+			float * ambi = ambiChans[c];
+			//T
+			
+			for(int i=0; i<numFrames; ++i){
+				ambi[i] = weights()[c] * input[i];
+			}
+		}
+	}
 	
-	//void encodeAdd(AmbiDecode<T> &dec, T input, );
+	/// Set spherical position of source to be encoded
+	void position(float az, float el);
 	
-	void position(T azimuth, T elevation);			///< Set position of source to be encoded.
-	//void position(T x, T y, T z);
-	
-	T * weights();									///< Returns pointer to encoding weights.
-	
-	virtual void onChannelsChange();
-	
-protected:
-	T * mWeights;			// encoding weights
-	int mSizeWeights;
-	
-	void weightsResize(int numChannels);
+	/// Set Cartesian position of source to be encoded
+	void position(float x, float y, float z);
 };
 
 
@@ -152,128 +135,7 @@ protected:
 // Implementation ______________________________________________________________
 
 
-#ifndef TEM
-#define TEM template <class T>
-#endif
-
-#ifndef SAFE_FREE
-#define SAFE_FREE(ptr) if(ptr){ free(ptr); ptr=0; }
-#endif
-
 // AmbiBase
-
-inline int AmbiBase::dim() const { return mDim; }
-inline int AmbiBase::order() const { return mOrder; }
-inline int AmbiBase::numChannels() const { return mNumChannels; }
-
-inline void AmbiBase::order(int o){
-	if(o != mOrder){
-		mOrder = o;
-		mNumChannels = orderToChannels(mDim, mOrder);
-		onChannelsChange();
-	}
-}
-
-inline int AmbiBase::channelsToUniformOrder(int channels){
-	// M = floor(sqrt(N) - 1)
-	return (int)floor(sqrt((double)channels) - 1);
-}
-
-TEM void AmbiBase::encodeWeightsFuMa
-	(T * ws, int dim, int order, T az, T el)
-{	
-	*ws++ = (T)c1_sqrt2;							// W = 1/sqrt(2)
-	
-	if(order > 0){
-		WRAP(az);
-		WRAP(el);
-	
-		T cosel = COS(el);
-		T x = COS(az) * cosel;
-		T y = SIN(az) * cosel;
-		T x2 = x * x;
-		T y2 = y * y;
-		
-		*ws++ = x;							// X = cos(A)cos(E)	
-		*ws++ = y;							// Y = sin(A)cos(E)
-		
-		if(order > 1){
-			x2 = x*x;
-			y2 = y*y;
-			
-			*ws++ = x2 - y2;					// U = cos(2A)cos2(E) = xx-yy
-			*ws++ = (T)2 * x * y;				// V = sin(2A)cos2(E) = 2xy
-			
-			if(order > 2){
-				*ws++ = x * (x2 - (T)3 * y2);		// P = cos(3A)cos3(E) = X(X2-3Y2)
-				*ws++ = y * (y2 - (T)3 * x2);		// Q = sin(3A)cos3(E) = Y(3X2-Y2)
-			}
-		}
-		
-		if(dim == 3){
-			T z = SIN(el);
-			*ws++ = z;							// Z = sin(E)
-			
-			if(order > 1){
-				T z2 = z*z;
-		
-				*ws++ = (T)2 * z * x;				// S = cos(A)sin(2E) = 2zx
-				*ws++ = (T)2 * z * y;				// T = sin(A)sin(2E) = 2yz
-				*ws++ = ((T)1.5 * z2) - (T)0.5;		// R = 1.5sin2(E)-0.5 = 1.5zz-0.5
-				
-				if(order > 2){
-					T pre = (T)c40_11 * z2 - (T)c8_11;
-					
-					*ws++ = z * (x2-y2) * (T)0.5;		// N = cos(2A)sin(E)cos2(E) = Z(X2-Y2)/2
-					*ws++ = x * y * z;					// O = sin(2A)sin(E)cos2(E) = XYZ
-					*ws++ = pre * x;					// L = 8cos(A)cos(E)(5sin2(E) - 1)/11 = 8X(5Z2-1)/11
-					*ws++ = pre * y;					// M = 8sin(A)cos(E)(5sin2(E) - 1)/11 = 8Y(5Z2-1)/11
-					*ws   = z * ((T)2.5 * z2 - (T)1.5);	// K = sin(E)(5sin2(E) - 3)/2 = Z(5Z2-3)/2
-				}
-			}
-		}
-	}
-}
-
-// [x, y, z] is the normalized direction vector
-TEM void AmbiBase::encodeWeightsFuMa16(T * ws, T x, T y, T z){
-	T x2 = x * x;
-	T y2 = y * y;
-	T z2 = z * z;
-	T pre = (T)c40_11 * z2 - (T)c8_11;
-
-	ws[ 0] = (T)c1_sqrt2;					// W channel, shouldn't it be already defined?
-	ws[ 1] = x;								// X = cos(A)cos(E)	
-	ws[ 2] = y;								// Y = sin(A)cos(E)
-	ws[ 3] = z;								// Z = sin(E)
-	ws[ 4] = x2 - y2;						// U = cos(2A)cos2(E) = xx-yy
-	ws[ 5] = (T)2. * x * y;					// V = sin(2A)cos2(E) = 2xy
-	ws[ 6] = (T)2. * z * x;					// S = cos(A)sin(2E) = 2zx
-	ws[ 7] = (T)2. * z * y;					// T = sin(A)sin(2E) = 2yz
-	ws[ 8] = (T)1.5 * z2 - (T)0.5;			// R = 1.5sin2(E)-0.5 = 1.5zz-0.5
-	ws[ 9] = x * (x2 - (T)3. * y2);			// P = cos(3A)cos3(E) = X(X2-3Y2)
-	ws[10] = y * (y2 - (T)3. * x2);			// Q = sin(3A)cos3(E) = Y(3X2-Y2)
-	ws[11] = z * (x2 - y2) * (T)0.5;		// N = cos(2A)sin(E)cos2(E) = Z(X2-Y2)/2
-	ws[12] = x * y * z;						// O = sin(2A)sin(E)cos2(E) = XYZ
-	ws[13] = pre * x;						// L = 8cos(A)cos(E)(5sin2(E) - 1)/11 = 8X(5Z2-1)/11
-	ws[14] = pre * y;						// M = 8sin(A)cos(E)(5sin2(E) - 1)/11 = 8Y(5Z2-1)/11
-	ws[15] = z * ((T)2.5 * z2 - (T)1.5);	// K = sin(E)(5sin2(E) - 3)/2 = Z(5Z2-3)/2	
-}
-
-
-TEM void AmbiBase::encodeWeightsFuMa16(T * ws, T az, T el){
-
-	WRAP(az);
-	WRAP(el);
-	
-	T cosel = COS(el);
-	T x = COS(az) * cosel;
-	T y = SIN(az) * cosel;
-	T z = SIN(el);
-	
-	encodeWeightsFuMa16(ws, x, y, z);
-}
-
 inline int AmbiBase::orderToChannels(int dim, int order){
 	int chans = orderToChannelsH(order);
 	return dim == 2 ? chans : chans + orderToChannelsV(order);
@@ -284,240 +146,43 @@ inline int AmbiBase::orderToChannelsV(int orderV){ return orderV * orderV; }
 
 
 // AmbiEncode
-
-TEM AmbiEncode<T>::AmbiEncode(int dim, int order)
-	: AmbiBase(dim, order), mWeights(0), mSizeWeights(0)
-{
-	onChannelsChange();
+inline void AmbiEncode::encode(const AmbiDecode &dec, float input){	
+	for(int c=0; c<dec.channels(); ++c) dec.frame()[c] = weights()[c] * input;
 }
 
-TEM AmbiEncode<T>::~AmbiEncode(){
-	SAFE_FREE(mWeights);
+inline void AmbiEncode::encodeAdd(const AmbiDecode &dec, float input){
+	for(int c=0; c<dec.channels(); ++c) dec.frame()[c] += weights()[c] * input;
 }
 
-TEM inline void AmbiEncode<T>::position(T az, T el){
+inline void AmbiEncode::position(float az, float el){
 	AmbiBase::encodeWeightsFuMa(mWeights, mDim, mOrder, az, el);
 }
 
-TEM inline void AmbiEncode<T>::encode(const AmbiDecode<T> &dec, T input){	
-	for(int c=0; c<dec.numChannels(); ++c) dec.frame()[c] = weights()[c] * input;
+inline void AmbiEncode::position(float x, float y, float z){
+	AmbiBase::encodeWeightsFuMa(mWeights, mDim, mOrder, x,y,z);
 }
 
-TEM inline void AmbiEncode<T>::encodeAdd(const AmbiDecode<T> &dec, T input){
-	for(int c=0; c<dec.numChannels(); ++c) dec.frame()[c] += weights()[c] * input;
-}
-
-TEM inline T * AmbiEncode<T>::weights(){ return mWeights; }
-
-TEM void AmbiEncode<T>::weightsResize(int numChannels){
-	if(numChannels != mSizeWeights){
-		mSizeWeights = numChannels;
-		mWeights = (T *)realloc(mWeights, mSizeWeights * sizeof(T));
-		memset(mWeights, 0, mSizeWeights * sizeof(T));
-	}
-}
-
-TEM void AmbiEncode<T>::onChannelsChange(){
-	weightsResize(numChannels());
-}
 
 
 // AmbiDecode
 
-TEM T AmbiDecode<T>::flavorWeights[4][5][5] = {
-	{	// none:
-		{1,		1,		1,		1,		1    }, // n = 0, M = 0, 1, 2, 3, 4
-		{0,		1,		1,		1,		1    }, // n = 1, M = 0, 1, 2, 3, 4
-		{0,		0,		1,		1,		1    }, // n = 2, M = 0, 1, 2, 3, 4
-		{0,		0,		0,		1,		1    }, // n = 3, M = 0, 1, 2, 3, 4
-		{0,		0,		0,		0,		1    }  // n = 4, M = 0, 1, 2, 3, 4
-	},{	// default:
-		{1,		0.707,	0.707,	0.707,	0.707},	// n = 0, M = 0, 1, 2, 3, 4
-		{0,		1    ,	0.75 ,	0.75 ,	0.75 },	// n = 1, M = 0, 1, 2, 3, 4
-		{0,		0    ,	0.5  ,	0.5  ,	0.5  },	// n = 2, M = 0, 1, 2, 3, 4
-		{0,		0    ,	0    ,	0.3  ,	0.3  },	// n = 3, M = 0, 1, 2, 3, 4
-		{0,		0    ,	0    ,	0    ,	0.1  } 	// n = 4, M = 0, 1, 2, 3, 4
-	},{	// in phase
-		{1,		1,		1,		1,		1    },	// n = 0, M = 0, 1, 2, 3, 4
-		{0,		0.333,	0.5,	0.6,	0.667},	// n = 1, M = 0, 1, 2, 3, 4
-		{0,		0,		0.1,	0.2,	0.286},	// n = 2, M = 0, 1, 2, 3, 4
-		{0,		0,		0,		0.029,	0.071},	// n = 3, M = 0, 1, 2, 3, 4
-		{0,		0,		0,		0,		0.008}	// n = 4, M = 0, 1, 2, 3, 4	
-	},{	// max-rE
-		{1,		1,		1,		1,		1    },	// n = 0, M = 0, 1, 2, 3, 4
-		{0,		0.577,	0.775,	0.861,	0.906},	// n = 1, M = 0, 1, 2, 3, 4
-		{0,		0,		0.4,	0.612,	0.732},	// n = 2, M = 0, 1, 2, 3, 4
-		{0,		0,		0,		0.305,	0.501},	// n = 3, M = 0, 1, 2, 3, 4
-		{0,		0,		0,		0,		0.246}	// n = 4, M = 0, 1, 2, 3, 4
-	}
-};
-
-TEM AmbiDecode<T>::AmbiDecode(int dim, int order, int numSpeakers, int flavor)
-	: AmbiBase(dim, order),
-	mNumSpeakers(0), mDecodeMatrix(0), mWChan(0), mPositions(0), mFrame(0)
-{
-	resizeArrays(numChannels(), numSpeakers);
-	this->flavor(flavor);
-}
-
-TEM AmbiDecode<T>::~AmbiDecode(){
-	SAFE_FREE(mDecodeMatrix);
-	SAFE_FREE(mWChan);
-	SAFE_FREE(mPositions);
-}
-
-TEM inline T AmbiDecode<T>::decode(T * encFrame, int encNumChannels, int speakerNum){
-	T smp = (T)0;
-	T * dec = mDecodeMatrix + speakerNum * mNumChannels;
-	T * wc = mWChan;
-	for(; encNumChannels>0; encNumChannels--) smp += *dec++ * *wc++ * *encFrame++;
+inline float AmbiDecode::decode(float * encFrame, int encNumChannels, int speakerNum){
+	float smp = 0;
+	float * dec = mDecodeMatrix + speakerNum * channels();
+	float * wc = mWeights;
+	for(int i=0; i<encNumChannels; ++i) smp += *dec++ * *wc++ * *encFrame++;
 	return smp;
 }
 
-TEM inline T AmbiDecode<T>::decode(int speakerNum){
-	return decode(mFrame, numChannels(), speakerNum);
+inline float AmbiDecode::decode(int speakerNum){
+	return decode(mFrame, channels(), speakerNum);
 }
 
-TEM void AmbiDecode<T>::decode(T ** dec, const T ** enc, int numDecFrames){
-		
-	// iterate speakers
-	for(int s=0; s<numSpeakers(); ++s){
-		T * out = dec[s];
-		
-		// iterate ambi channels
-		for(int c=0; c<numChannels(); c++){
-			T * in = enc[c];
-			float w = decodeWeight(s, c);
-			
-			for(int i=0; i<numDecFrames; ++i) out[i] += in[i] * w;		
-		}		
-	}
-}
-
-TEM void AmbiDecode<T>::flavor(int type){
-	if(type < 4){
-		mFlavor = type;
-		for(int i=0; i<4 ; i++) mWOrder[i] = flavorWeights[type][i][mOrder];
-		updateChanWeights();
-	} 
-}
-
-TEM void AmbiDecode<T>::numSpeakers(int num){
-	resizeArrays(numChannels(), num);
-}
-
-TEM void AmbiDecode<T>::zero(){ memset(mFrame, 0, numChannels() * sizeof(T)); }
-
-TEM inline T * AmbiDecode<T>::azimuths(){ return mPositions; }
-TEM inline T * AmbiDecode<T>::elevations(){ return mPositions + mNumSpeakers; }
-TEM inline T * AmbiDecode<T>::frame() const { return mFrame; }
-TEM inline int AmbiDecode<T>::flavor(){ return mFlavor; }
-TEM inline int AmbiDecode<T>::numSpeakers(){ return mNumSpeakers; }
-
-TEM void AmbiDecode<T>::setSpeaker(int index, T az, T el){
-	if (index < numSpeakers()) {	// verify speaker index
-		azimuths()  [index] = az;	// update speaker location	
-		elevations()[index] = el;
-		
-		// update encoding weights
-		//mDecodeMatrix[index][0] *= AmbiBase::c1_sqrt2;
-		encodeWeightsFuMa(mDecodeMatrix + index * numChannels(), mDim, mOrder, az, el);
-	}	
-}
-
-TEM void AmbiDecode<T>::setSpeakerDegrees(int index, T az, T el){
-	setSpeaker(index, az * T(0.01745329252), el * T(0.01745329252));
-}
-
-TEM void AmbiDecode<T>::updateChanWeights(){
-	T * wc = mWChan;
-	*wc++ = mWOrder[0];	
-	
-	if (mOrder > 0) {
-		*wc++ = mWOrder[1];			// X
-		*wc++ = mWOrder[1];			// Y
-		if (mOrder > 1) {
-			*wc++ = mWOrder[2];		// U
-			*wc++ = mWOrder[2];		// V
-			if (mOrder > 2) {
-				*wc++ = mWOrder[3];	// P
-				*wc++ = mWOrder[3];	// Q
-			}
-		}
-		
-		if(3 == mDim){
-			*wc++ = mWOrder[1];			// Z
-			if (mOrder > 1) {
-				*wc++ = mWOrder[2];		// S
-				*wc++ = mWOrder[2];		// T
-				*wc++ = mWOrder[2];		// R
-				if (mOrder > 2) {
-					*wc++ = mWOrder[3];	// N
-					*wc++ = mWOrder[3];	// O
-					*wc++ = mWOrder[3];	// L
-					*wc++ = mWOrder[3];	// M
-					*wc   = mWOrder[3];	// K
-				}
-			}
-		}
-	}
-}
-
-TEM void AmbiDecode<T>::resizeArrays(int numChannels, int numSpeakers){
-
-	int oldSize = mNumChannels * mNumSpeakers;
-	int newSize = numChannels * numSpeakers;
-	
-	if(oldSize != newSize){
-	
-		// resize decoding matrix
-		int bytesSize = newSize * sizeof(T);
-		mDecodeMatrix = (T *)realloc(mDecodeMatrix, bytesSize);
-		memset(mDecodeMatrix, 0, bytesSize);
-		
-		// resize ambi channel weights
-		int bytesChan = numChannels * sizeof(T);
-		mWChan = (T *)realloc(mWChan, bytesChan);
-		memset(mWChan, 0, bytesChan);
-
-		mFrame = (T *)realloc(mFrame, bytesChan);
-		memset(mFrame, 0, bytesChan);
-		
-		// resize number of speakers (?)
-		if(numSpeakers != mNumSpeakers){
-			mNumSpeakers = numSpeakers;
-			int bytesPos = numSpeakers * 2 * sizeof(T);
-			mPositions = (T *)realloc(mPositions, bytesPos);
-			memset(mPositions, 0, bytesPos);
-		}
-		
-		// recompute decode matrix weights
-		for(int i=0; i<numSpeakers; i++){
-			setSpeaker(i, azimuths()[i], elevations()[i]);
-		}
-		
-		updateChanWeights();
-	}
-	
-	
-	mNumChannels = numChannels;
-}
-
-TEM void AmbiDecode<T>::onChannelsChange(){
-	resizeArrays(numChannels(), mNumSpeakers);
-}
-
-TEM void AmbiDecode<T>::print(FILE * fp, const char * append){
-	AmbiBase::print(stdout, ", ");
-	fprintf(fp, "s:%3d%s", mNumSpeakers, append);
-}
+inline float * AmbiDecode::azimuths(){ return mPositions; }
+inline float * AmbiDecode::elevations(){ return mPositions + mNumSpeakers; }
+inline float * AmbiDecode::frame() const { return mFrame; }
+inline int AmbiDecode::flavor(){ return mFlavor; }
+inline int AmbiDecode::numSpeakers(){ return mNumSpeakers; }
 
 } // al::
-
-#undef TEM
-#undef SAFE_FREE
-#undef WRAP
-#undef COS
-#undef SIN
-
 #endif
---
+++ src/sound/al_Ambisonics.cpp
@@ -0,0 +1,314 @@
+#include <string.h>
+#include "sound/al_Ambisonics.hpp"
+
+#ifdef USE_GAMMA
+	#include "scl.h"
+	#define COS			gam::scl::cosT8
+	#define SIN			gam::scl::sinT7
+	#define WRAP(phase)	phase = gam::scl::wrapPhase(phase)
+#else
+	#include <math.h>
+	#define COS			cos
+	#define SIN			sin
+	#define WRAP(phase)
+#endif
+
+
+namespace al{
+
+static double c1_sqrt2	= 0.707106781186548;
+static double c8_11		= 8./11.;
+static double c40_11	= 40./11.;
+
+
+// AmbiBase
+
+AmbiBase::AmbiBase(int dim, int order)
+:	mDim(dim), mWeights(0)
+{	this->order(order); }
+
+AmbiBase::~AmbiBase(){
+	delete[] mWeights;
+}
+
+void AmbiBase::resize(float * a, int n){
+	delete[] a;
+	a = new float[n];
+	memset(a, 0, n*sizeof(float));
+}
+
+void AmbiBase::order(int o){
+	if(o != mOrder){
+		mOrder = o;
+		mChannels = orderToChannels(mDim, mOrder);
+		resize(mWeights, channels());
+		onChannelsChange();
+	}
+}
+
+int AmbiBase::channelsToUniformOrder(int channels){
+	// M = floor(sqrt(N) - 1)
+	return (int)(sqrt((double)channels) - 1);
+}
+
+
+// Compute spherical harmonic weights based on a position in space
+void AmbiBase::encodeWeightsFuMa(float * ws, int dim, int order, float x, float y, float z){
+	*ws++ = c1_sqrt2;								// W = 1/sqrt(2)
+	
+	if(order > 0){
+		float x2 = x * x;
+		float y2 = y * y;
+		
+		*ws++ = x;									// X = cos(A)cos(E)	
+		*ws++ = y;									// Y = sin(A)cos(E)
+		
+		if(order > 1){
+			x2 = x*x;
+			y2 = y*y;
+			
+			*ws++ = x2 - y2;						// U = cos(2A)cos2(E) = xx-yy
+			*ws++ = 2.f * x * y;					// V = sin(2A)cos2(E) = 2xy
+			
+			if(order > 2){
+				*ws++ = x * (x2 - 3.f * y2);		// P = cos(3A)cos3(E) = X(X2-3Y2)
+				*ws++ = y * (y2 - 3.f * x2);		// Q = sin(3A)cos3(E) = Y(3X2-Y2)
+			}
+		}
+		
+		if(dim == 3){
+			*ws++ = z;								// Z = sin(E)
+			
+			if(order > 1){
+				float z2 = z*z;
+		
+				*ws++ = 2.f * z * x;				// S = cos(A)sin(2E) = 2zx
+				*ws++ = 2.f * z * y;				// T = sin(A)sin(2E) = 2yz
+				*ws++ = (1.5f * z2) - 0.5f;			// R = 1.5sin2(E)-0.5 = 1.5zz-0.5
+				
+				if(order > 2){
+					float pre = c40_11 * z2 - c8_11;
+					
+					*ws++ = z * (x2-y2) * 0.5f;		// N = cos(2A)sin(E)cos2(E) = Z(X2-Y2)/2
+					*ws++ = x * y * z;				// O = sin(2A)sin(E)cos2(E) = XYZ
+					*ws++ = pre * x;				// L = 8cos(A)cos(E)(5sin2(E) - 1)/11 = 8X(5Z2-1)/11
+					*ws++ = pre * y;				// M = 8sin(A)cos(E)(5sin2(E) - 1)/11 = 8Y(5Z2-1)/11
+					*ws   = z * (2.5f * z2 - 1.5f);	// K = sin(E)(5sin2(E) - 3)/2 = Z(5Z2-3)/2
+				}
+			}
+		}
+	}
+}
+
+
+void AmbiBase::encodeWeightsFuMa(float * ws, int dim, int order, float az, float el){
+	WRAP(az);
+	WRAP(el);
+	float cosel = COS(el);
+	float x = COS(az) * cosel;
+	float y = SIN(az) * cosel;
+	float z = dim>=3 ? SIN(el) : 0;
+	encodeWeightsFuMa(ws, dim, x,y,z);
+}
+
+// [x, y, z] is the normalized direction vector
+void AmbiBase::encodeWeightsFuMa16(float * ws, float x, float y, float z){
+	float x2 = x * x;
+	float y2 = y * y;
+	float z2 = z * z;
+	float pre = c40_11 * z2 - c8_11;
+
+	ws[ 0] = c1_sqrt2;					// W channel, shouldn't it be already defined?
+	ws[ 1] = x;							// X = cos(A)cos(E)	
+	ws[ 2] = y;							// Y = sin(A)cos(E)
+	ws[ 3] = z;							// Z = sin(E)
+	ws[ 4] = x2 - y2;					// U = cos(2A)cos2(E) = xx-yy
+	ws[ 5] = 2.f * x * y;				// V = sin(2A)cos2(E) = 2xy
+	ws[ 6] = 2.f * z * x;				// S = cos(A)sin(2E) = 2zx
+	ws[ 7] = 2.f * z * y;				// T = sin(A)sin(2E) = 2yz
+	ws[ 8] = 1.5f * z2 - 0.5f;			// R = 1.5sin2(E)-0.5 = 1.5zz-0.5
+	ws[ 9] = x * (x2 - 3.f * y2);		// P = cos(3A)cos3(E) = X(X2-3Y2)
+	ws[10] = y * (y2 - 3.f * x2);		// Q = sin(3A)cos3(E) = Y(3X2-Y2)
+	ws[11] = z * (x2 - y2) * 0.5f;		// N = cos(2A)sin(E)cos2(E) = Z(X2-Y2)/2
+	ws[12] = x * y * z;					// O = sin(2A)sin(E)cos2(E) = XYZ
+	ws[13] = pre * x;					// L = 8cos(A)cos(E)(5sin2(E) - 1)/11 = 8X(5Z2-1)/11
+	ws[14] = pre * y;					// M = 8sin(A)cos(E)(5sin2(E) - 1)/11 = 8Y(5Z2-1)/11
+	ws[15] = z * (2.5f * z2 - 1.5f);	// K = sin(E)(5sin2(E) - 3)/2 = Z(5Z2-3)/2	
+}
+
+
+void AmbiBase::encodeWeightsFuMa16(float * ws, float az, float el){
+	WRAP(az);
+	WRAP(el);
+	float cosel = COS(el);
+	float x = COS(az) * cosel;
+	float y = SIN(az) * cosel;
+	float z = SIN(el);
+	encodeWeightsFuMa16(ws, x,y,z);
+}
+
+
+
+
+// AmbiDecode
+
+float AmbiDecode::flavorWeights[4][5][5] = {
+	{	// none:
+		{1,		1,		1,		1,		1    }, // n = 0, M = 0, 1, 2, 3, 4
+		{0,		1,		1,		1,		1    }, // n = 1, M = 0, 1, 2, 3, 4
+		{0,		0,		1,		1,		1    }, // n = 2, M = 0, 1, 2, 3, 4
+		{0,		0,		0,		1,		1    }, // n = 3, M = 0, 1, 2, 3, 4
+		{0,		0,		0,		0,		1    }  // n = 4, M = 0, 1, 2, 3, 4
+	},{	// default:
+		{1,		0.707,	0.707,	0.707,	0.707},	// n = 0, M = 0, 1, 2, 3, 4
+		{0,		1    ,	0.75 ,	0.75 ,	0.75 },	// n = 1, M = 0, 1, 2, 3, 4
+		{0,		0    ,	0.5  ,	0.5  ,	0.5  },	// n = 2, M = 0, 1, 2, 3, 4
+		{0,		0    ,	0    ,	0.3  ,	0.3  },	// n = 3, M = 0, 1, 2, 3, 4
+		{0,		0    ,	0    ,	0    ,	0.1  } 	// n = 4, M = 0, 1, 2, 3, 4
+	},{	// in phase
+		{1,		1,		1,		1,		1    },	// n = 0, M = 0, 1, 2, 3, 4
+		{0,		0.333,	0.5,	0.6,	0.667},	// n = 1, M = 0, 1, 2, 3, 4
+		{0,		0,		0.1,	0.2,	0.286},	// n = 2, M = 0, 1, 2, 3, 4
+		{0,		0,		0,		0.029,	0.071},	// n = 3, M = 0, 1, 2, 3, 4
+		{0,		0,		0,		0,		0.008}	// n = 4, M = 0, 1, 2, 3, 4	
+	},{	// max-rE
+		{1,		1,		1,		1,		1    },	// n = 0, M = 0, 1, 2, 3, 4
+		{0,		0.577,	0.775,	0.861,	0.906},	// n = 1, M = 0, 1, 2, 3, 4
+		{0,		0,		0.4,	0.612,	0.732},	// n = 2, M = 0, 1, 2, 3, 4
+		{0,		0,		0,		0.305,	0.501},	// n = 3, M = 0, 1, 2, 3, 4
+		{0,		0,		0,		0,		0.246}	// n = 4, M = 0, 1, 2, 3, 4
+	}
+};
+
+AmbiDecode::AmbiDecode(int dim, int order, int numSpeakers, int flavor)
+	: AmbiBase(dim, order),
+	mNumSpeakers(0), mDecodeMatrix(0), mPositions(0), mFrame(0)
+{
+	resizeArrays(channels(), numSpeakers);
+	this->flavor(flavor);
+}
+
+AmbiDecode::~AmbiDecode(){
+	delete[] mDecodeMatrix;
+	delete[] mPositions;
+}
+
+void AmbiDecode::decode(float ** dec, const float ** enc, int numDecFrames){
+		
+	// iterate speakers
+	for(int s=0; s<numSpeakers(); ++s){
+		float * out = dec[s];
+		
+		// iterate ambi channels
+		for(int c=0; c<channels(); c++){
+			const float * in = enc[c];
+			float w = decodeWeight(s, c);
+			for(int i=0; i<numDecFrames; ++i) out[i] += in[i] * w;		
+		}		
+	}
+}
+
+void AmbiDecode::flavor(int type){
+	if(type < 4){
+		mFlavor = type;
+		for(int i=0; i<4 ; i++) mWOrder[i] = flavorWeights[type][i][mOrder];
+		updateChanWeights();
+	} 
+}
+
+void AmbiDecode::numSpeakers(int num){
+	resizeArrays(channels(), num);
+}
+
+void AmbiDecode::zero(){ memset(mFrame, 0, channels()*sizeof(float)); }
+
+void AmbiDecode::setSpeaker(int index, float az, float el){
+	if(index < numSpeakers()){		// verify speaker index
+		azimuths()  [index] = az;	// update speaker location	
+		elevations()[index] = el;
+		
+		// update encoding weights
+		//mDecodeMatrix[index][0] *= AmbiBase::c1_sqrt2;
+		encodeWeightsFuMa(mDecodeMatrix + index * channels(), mDim, mOrder, az, el);
+	}	
+}
+
+void AmbiDecode::setSpeakerDegrees(int index, float az, float el){
+	setSpeaker(index, az * float(0.01745329252), el * float(0.01745329252));
+}
+
+void AmbiDecode::updateChanWeights(){
+	float * wc = mWeights;
+	*wc++ = mWOrder[0];	
+	
+	if (mOrder > 0) {
+		*wc++ = mWOrder[1];				// X
+		*wc++ = mWOrder[1];				// Y
+		if (mOrder > 1) {
+			*wc++ = mWOrder[2];			// U
+			*wc++ = mWOrder[2];			// V
+			if (mOrder > 2) {
+				*wc++ = mWOrder[3];		// P
+				*wc++ = mWOrder[3];		// Q
+			}
+		}
+		
+		if(3 == mDim){
+			*wc++ = mWOrder[1];			// Z
+			if (mOrder > 1) {
+				*wc++ = mWOrder[2];		// S
+				*wc++ = mWOrder[2];		// T
+				*wc++ = mWOrder[2];		// R
+				if (mOrder > 2) {
+					*wc++ = mWOrder[3];	// N
+					*wc++ = mWOrder[3];	// O
+					*wc++ = mWOrder[3];	// L
+					*wc++ = mWOrder[3];	// M
+					*wc   = mWOrder[3];	// K
+				}
+			}
+		}
+	}
+}
+
+void AmbiDecode::resizeArrays(int numChannels, int numSpeakers){
+
+	int oldSize = channels() * mNumSpeakers;
+	int newSize = numChannels * numSpeakers;
+	
+	if(oldSize != newSize){
+
+		resize(mDecodeMatrix, newSize);
+		resize(mFrame, newSize);
+		
+		// resize number of speakers (?)
+		if(numSpeakers != mNumSpeakers){
+			mNumSpeakers = numSpeakers;
+			resize(mPositions, numSpeakers * 2);
+		}
+		
+		// recompute decode matrix weights
+		for(int i=0; i<numSpeakers; i++){
+			setSpeaker(i, azimuths()[i], elevations()[i]);
+		}
+		
+		updateChanWeights();
+	}
+
+	mChannels = numChannels;
+}
+
+void AmbiDecode::onChannelsChange(){
+	resizeArrays(channels(), mNumSpeakers);
+}
+
+void AmbiDecode::print(FILE * fp, const char * append){
+//	AmbiBase::print(stdout, ", ");
+//	fprintf(fp, "s:%3d%s", mNumSpeakers, append);
+}
+
+} // al::
+
+#undef WRAP
+#undef COS
+#undef SIN
