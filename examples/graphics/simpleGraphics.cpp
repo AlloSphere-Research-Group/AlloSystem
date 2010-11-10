f9a75b25fec0f58d9f47123bf1e63d532786073d
l.putnam <l.putnam@bd62deb4-2030-0410-8420-fdd75d726ec8>
2010-11-10 01:01:55 +0000

    update simpleGraphics

--- examples/graphics/simpleGraphics.cpp
+++ examples/graphics/simpleGraphics.cpp
@@ -6,39 +6,35 @@ Graphics gl(new GraphicsBackendOpenGL);
 struct MyWindow : Window{
 
 	bool onFrame(){
+
 		gl.clearColor(0,0,0,0);
 		gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-		gl.viewport(0,0, width(), height());
-		
-		gl.data().resetBuffers();
-		gl.begin(gl.TRIANGLES);
-		
-		//printf("%d\n", gl.DEPTH_BUFFER_BIT);
-		
-		for(int i=0; i<2000; ++i){
-			//printf("%d ", i);
-			float x = rnd::uniformS();
-			float y = rnd::uniformS();
-			float d = 0.01;
 
+		gl.viewport(0,0, width(), height());
+
+		gl.matrixMode(gl.PROJECTION);
+		gl.loadMatrix(Matrix4d::perspective(45, aspect(), 0.1, 100));
+
+		gl.matrixMode(gl.MODELVIEW);
+		gl.loadMatrix(Matrix4d::lookAt(Vec3d(0,0,-4), Vec3d(0,0,0), Vec3d(0,1,0)));
+
+		gl.begin(gl.QUADS);
 			gl.color(1,0,0);
-			gl.vertex(x-d, y-d);
+			gl.vertex(-1,-1);
 			gl.color(0,1,0);
-			gl.vertex(x+d, y-d);
-			gl.vertex(x  , y+d);
-		}
-		
+			gl.vertex( 1,-1);
+			gl.color(0,0,1);
+			gl.vertex( 1, 1);
+			gl.color(1,1,1);
+			gl.vertex(-1, 1);		
 		gl.end();
 		return true;
 	}
 };
 
 int main(){
-
 	MyWindow win1;
-
 	win1.add(new StandardWindowKeyControls);
-
 	win1.create(Window::Dim(800, 600));
 
 	MainLoop::start();
