4c8891dffbc9f5d5f2d43f401d11f2b30cbdf859
wakefield <wakefield@bd62deb4-2030-0410-8420-fdd75d726ec8>
2010-12-13 21:31:51 +0000

    adding al_Asset

--- experimental/shadertest/modelShader.cpp
+++ experimental/shadertest/modelShader.cpp
@@ -1,4 +1,5 @@
 #include "allocore/al_Allocore.hpp"
+#include "al_Asset.hpp"
 
 // @see http://assimp.svn.sourceforge.net/viewvc/assimp/trunk/samples/SimpleOpenGL/Sample_SimpleOpenGL.c?revision=827&view=markup
 #include "assimp/assimp.h"
@@ -19,219 +20,12 @@ ShaderProgram shaderprogram;
 Texture tex(&gl);
 Image img;
 
-// the global Assimp scene object
-const struct aiScene* scene = NULL;
+Scene * ascene = 0;
+Vec3f scene_min, scene_max, scene_center;
 GLuint scene_list = 0;
-struct aiVector3D scene_min, scene_max, scene_center;
 
+float a = 0.f; // current rotation angle
 
-// current rotation angle
-static float a = 0.f;
-
-// ----------------------------------------------------------------------------
-void get_bounding_box_for_node(const struct aiNode* nd, 
-		struct aiVector3D* min, struct aiVector3D* max, struct aiMatrix4x4* trafo){
-	struct aiMatrix4x4 prev;
-	unsigned int n = 0, t;
-	prev = *trafo;
-	aiMultiplyMatrix4(trafo,&nd->mTransformation);
-	for (; n < nd->mNumMeshes; ++n) {
-		const struct aiMesh* mesh = scene->mMeshes[nd->mMeshes[n]];
-			for (t = 0; t < mesh->mNumVertices; ++t) {
-			struct aiVector3D tmp = mesh->mVertices[t];
-			aiTransformVecByMatrix4(&tmp,trafo);
-			min->x = MIN(min->x,tmp.x);
-			min->y = MIN(min->y,tmp.y);
-			min->z = MIN(min->z,tmp.z);
-			max->x = MAX(max->x,tmp.x);
-			max->y = MAX(max->y,tmp.y);
-			max->z = MAX(max->z,tmp.z);
-		}
-	}
-	for (n = 0; n < nd->mNumChildren; ++n) {
-		get_bounding_box_for_node(nd->mChildren[n],min,max,trafo);
-	}
-	*trafo = prev;
-}
-
-void get_bounding_box(struct aiVector3D* min, struct aiVector3D* max)
-{
-	struct aiMatrix4x4 trafo;
-	aiIdentityMatrix4(&trafo);
-	min->x = min->y = min->z =  1e10f;
-	max->x = max->y = max->z = -1e10f;
-	get_bounding_box_for_node(scene->mRootNode,min,max,&trafo);
-}
-
-int loadasset(std::string path)
-{
-	printf("loading model %s\n", path.c_str());
-	// we are taking one of the postprocessing presets to avoid
-	// writing 20 single postprocessing flags here.
-	
-	int flags = aiProcessPreset_TargetRealtime_MaxQuality;
-	scene = aiImportFile(path.c_str(), flags);
-	if (scene) {
-		get_bounding_box(&scene_min,&scene_max);
-		scene_center.x = (scene_min.x + scene_max.x) / 2.0f;
-		scene_center.y = (scene_min.y + scene_max.y) / 2.0f;
-		scene_center.z = (scene_min.z + scene_max.z) / 2.0f;
-		printf("scene center %f %f %f\n", scene_center.x, scene_center.y, scene_center.z);
-		return 0;
-	}
-	return 1;
-}
-
-// ----------------------------------------------------------------------------
-void color4_to_float4(const struct aiColor4D *c, float f[4])
-{
-	f[0] = c->r;
-	f[1] = c->g;
-	f[2] = c->b;
-	f[3] = c->a;
-}
-
-// ----------------------------------------------------------------------------
-
-void set_float4(float f[4], float a, float b, float c, float d)
-{
-	f[0] = a;
-	f[1] = b;
-	f[2] = c;
-	f[3] = d;
-}
-// ----------------------------------------------------------------------------
-void apply_material(const struct aiMaterial *mtl)
-{
-	float c[4];
-
-	GLenum fill_mode;
-	unsigned int ret1, ret2;
-	struct aiColor4D diffuse;
-	struct aiColor4D specular;
-	struct aiColor4D ambient;
-	struct aiColor4D emission;
-	float shininess, strength;
-	int two_sided;
-	int wireframe;
-	unsigned int max;
-
-	set_float4(c, 0.8f, 0.8f, 0.8f, 1.0f);
-	if(AI_SUCCESS == aiGetMaterialColor(mtl, AI_MATKEY_COLOR_DIFFUSE, &diffuse))
-		color4_to_float4(&diffuse, c);
-	glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, c);
-
-	set_float4(c, 0.0f, 0.0f, 0.0f, 1.0f);
-	if(AI_SUCCESS == aiGetMaterialColor(mtl, AI_MATKEY_COLOR_SPECULAR, &specular))
-		color4_to_float4(&specular, c);
-	glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, c);
-
-	set_float4(c, 0.2f, 0.2f, 0.2f, 1.0f);
-	if(AI_SUCCESS == aiGetMaterialColor(mtl, AI_MATKEY_COLOR_AMBIENT, &ambient))
-		color4_to_float4(&ambient, c);
-	glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, c);
-
-	set_float4(c, 0.0f, 0.0f, 0.0f, 1.0f);
-	if(AI_SUCCESS == aiGetMaterialColor(mtl, AI_MATKEY_COLOR_EMISSIVE, &emission))
-		color4_to_float4(&emission, c);
-	glMaterialfv(GL_FRONT_AND_BACK, GL_EMISSION, c);
-
-	max = 1;
-	ret1 = aiGetMaterialFloatArray(mtl, AI_MATKEY_SHININESS, &shininess, &max);
-	max = 1;
-	ret2 = aiGetMaterialFloatArray(mtl, AI_MATKEY_SHININESS_STRENGTH, &strength, &max);
-	if((ret1 == AI_SUCCESS) && (ret2 == AI_SUCCESS))
-		glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, shininess * strength);
-	else {
-		glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, 0.0f);
-		set_float4(c, 0.0f, 0.0f, 0.0f, 0.0f);
-		glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, c);
-	}
-
-	max = 1;
-	if(AI_SUCCESS == aiGetMaterialIntegerArray(mtl, AI_MATKEY_ENABLE_WIREFRAME, &wireframe, &max))
-		fill_mode = wireframe ? GL_LINE : GL_FILL;
-	else
-		fill_mode = GL_FILL;
-	glPolygonMode(GL_FRONT_AND_BACK, fill_mode);
-
-	max = 1;
-	if((AI_SUCCESS == aiGetMaterialIntegerArray(mtl, AI_MATKEY_TWOSIDED, &two_sided, &max)) && two_sided)
-		glEnable(GL_CULL_FACE);
-	else 
-		glDisable(GL_CULL_FACE);
-}
-
-// Can't send color down as a pointer to aiColor4D because AI colors are ABGR.
-void Color4f(const struct aiColor4D *color)
-{
-	glColor4f(color->r, color->g, color->b, color->a);
-}
-void recursive_render (const struct aiScene *sc, const struct aiNode* nd)
-{
-	int i;
-	unsigned int n = 0, t;
-	struct aiMatrix4x4 m = nd->mTransformation;
-
-	// update transform
-	aiTransposeMatrix4(&m);
-	glPushMatrix();
-	glMultMatrixf((float*)&m);
-
-	// draw all meshes assigned to this node
-	for (; n < nd->mNumMeshes; ++n) {
-		const struct aiMesh* mesh = scene->mMeshes[nd->mMeshes[n]];
-
-		//apply_material(sc->mMaterials[mesh->mMaterialIndex]);
-
-		if(mesh->mNormals == NULL) {
-			glDisable(GL_LIGHTING);
-		} else {
-			glEnable(GL_LIGHTING);
-		}
-
-		if(mesh->mColors[0] != NULL) {
-			glEnable(GL_COLOR_MATERIAL);
-		} else {
-			glDisable(GL_COLOR_MATERIAL);
-		}
-
-		for (t = 0; t < mesh->mNumFaces; ++t) {
-			const struct aiFace* face = &mesh->mFaces[t];
-			GLenum face_mode;
-
-			switch(face->mNumIndices) {
-				case 1: face_mode = GL_POINTS; break;
-				case 2: face_mode = GL_LINES; break;
-				case 3: face_mode = GL_TRIANGLES; break;
-				default: face_mode = GL_POLYGON; break;
-			}
-
-			glBegin(face_mode);
-
-			for(i = 0; i < face->mNumIndices; i++) {
-				int index = face->mIndices[i];
-				if(mesh->mColors[0] != NULL)
-					Color4f(&mesh->mColors[0][index]);
-				if(mesh->mNormals != NULL) 
-					glNormal3fv(&mesh->mNormals[index].x);
-				if(mesh->mTextureCoords[0] != NULL) 
-					glTexCoord3fv(&mesh->mTextureCoords[0][index].x);
-				glVertex3fv(&mesh->mVertices[index].x);
-			}
-
-			glEnd();
-		}
-
-	}
-
-	// draw all children
-	for (n = 0; n < nd->mNumChildren; ++n) {
-		recursive_render(sc, nd->mChildren[n]);
-	}
-
-	glPopMatrix();
-}
 
 struct MyWindow : Window{
 
@@ -298,14 +92,14 @@ struct MyWindow : Window{
 		a += 0.5;
 		
 		// scale the whole asset to fit into our view frustum 
-		float tmp = scene_max.x-scene_min.x;
-		tmp = MAX(scene_max.y - scene_min.y,tmp);
-		tmp = MAX(scene_max.z - scene_min.z,tmp);
+		float tmp = scene_max[0]-scene_min[0];
+		tmp = MAX(scene_max[1] - scene_min[1],tmp);
+		tmp = MAX(scene_max[2] - scene_min[2],tmp);
 		tmp = 2.f / tmp;
 		glScalef(tmp, tmp, tmp);
-		
+	
 		// center the model
-		glTranslatef( -scene_center.x, -scene_center.y, -scene_center.z );
+		gl.translate( -scene_center );
 		
 		
 		// if the display list has not been made yet, create a new one and
@@ -313,7 +107,14 @@ struct MyWindow : Window{
 		if(scene_list == 0) {
 			scene_list = glGenLists(1);
 			glNewList(scene_list, GL_COMPILE);
-			recursive_render(scene, scene->mRootNode);
+			
+			Mesh mesh;
+			for (int i=0; i<ascene->meshes(); i++) {
+				ascene->mesh(i, mesh);
+				gl.draw(mesh);
+			}	
+			
+			//recursive_render(scene, scene->mRootNode);
 			glEndList();
 		}
 		
@@ -335,21 +136,21 @@ int main (int argc, char * const argv[]) {
 	searchpaths.addAppPaths(argc, argv);
 	searchpaths.addSearchPath(searchpaths.appPath() + "../../resources");
 	
-	
-	// get a handle to the predefined STDOUT log stream and attach
-	// it to the logging system. It will be active for all further
-	// calls to aiImportFile(Ex) and aiApplyPostProcessing.
-	struct aiLogStream stream = aiGetPredefinedLogStream(aiDefaultLogStream_STDOUT,NULL);
-	aiAttachLogStream(&stream);
-	
 	// load in a "scene"
 	//FilePath path = searchpaths.find("magnolia.obj");
 	FilePath path = searchpaths.find("ducky.obj");
 	//FilePath path = searchpaths.find("dodecahedron.obj");
 	//FilePath path = searchpaths.find("body_cylind.obj");
-	if (loadasset(path.filepath())) {
+	
+	//ascene = Scene::import(path.filepath());
+	ascene = Scene::import("/Users/grahamwakefield/Source/allosphere/nanomed/trunk/models/uvExportFromMaya/arteries.obj");
+	if (ascene==0) {
 		printf("error reading %s\n", path.filepath().c_str());
 		return -1;
+	} else {
+		ascene->getBounds(scene_min,scene_max);
+		scene_center = (scene_min + scene_max) / 2.f;
+		ascene->dump();
 	}
 	File frag_file(searchpaths.find("basicFragment.glsl"), "r", true);
 	File vert_file(searchpaths.find("basicVertex.glsl"), "r", true);
@@ -363,17 +164,165 @@ int main (int argc, char * const argv[]) {
 	path = searchpaths.find("hubble.jpg");
 	img.load(path.filepath());
 	tex.fromArray(&img.array());
-	//tex.target(Texture::TEXTURE_2D);
 	
 	MyWindow win1;
 	win1.add(new StandardWindowKeyControls);
-	win1.create(Window::Dim(800, 600));
+	win1.create(Window::Dim(640, 480));
 	
 	MainLoop::start();
 	
-	// cleanup
-	aiReleaseImport(scene);
-	aiDetachAllLogStreams();
-	
 	return 0;
 }
+
+
+
+// ----------------------------------------------------------------------------
+//void color4_to_float4(const struct aiColor4D *c, float f[4])
+//{
+//	f[0] = c->r;
+//	f[1] = c->g;
+//	f[2] = c->b;
+//	f[3] = c->a;
+//}
+//
+// ----------------------------------------------------------------------------
+//
+//void set_float4(float f[4], float a, float b, float c, float d)
+//{
+//	f[0] = a;
+//	f[1] = b;
+//	f[2] = c;
+//	f[3] = d;
+//}
+// ----------------------------------------------------------------------------
+//void apply_material(const struct aiMaterial *mtl)
+//{
+//	float c[4];
+//
+//	GLenum fill_mode;
+//	unsigned int ret1, ret2;
+//	struct aiColor4D diffuse;
+//	struct aiColor4D specular;
+//	struct aiColor4D ambient;
+//	struct aiColor4D emission;
+//	float shininess, strength;
+//	int two_sided;
+//	int wireframe;
+//	unsigned int max;
+//
+//	set_float4(c, 0.8f, 0.8f, 0.8f, 1.0f);
+//	if(AI_SUCCESS == aiGetMaterialColor(mtl, AI_MATKEY_COLOR_DIFFUSE, &diffuse))
+//		color4_to_float4(&diffuse, c);
+//	glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, c);
+//
+//	set_float4(c, 0.0f, 0.0f, 0.0f, 1.0f);
+//	if(AI_SUCCESS == aiGetMaterialColor(mtl, AI_MATKEY_COLOR_SPECULAR, &specular))
+//		color4_to_float4(&specular, c);
+//	glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, c);
+//
+//	set_float4(c, 0.2f, 0.2f, 0.2f, 1.0f);
+//	if(AI_SUCCESS == aiGetMaterialColor(mtl, AI_MATKEY_COLOR_AMBIENT, &ambient))
+//		color4_to_float4(&ambient, c);
+//	glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, c);
+//
+//	set_float4(c, 0.0f, 0.0f, 0.0f, 1.0f);
+//	if(AI_SUCCESS == aiGetMaterialColor(mtl, AI_MATKEY_COLOR_EMISSIVE, &emission))
+//		color4_to_float4(&emission, c);
+//	glMaterialfv(GL_FRONT_AND_BACK, GL_EMISSION, c);
+//
+//	max = 1;
+//	ret1 = aiGetMaterialFloatArray(mtl, AI_MATKEY_SHININESS, &shininess, &max);
+//	max = 1;
+//	ret2 = aiGetMaterialFloatArray(mtl, AI_MATKEY_SHININESS_STRENGTH, &strength, &max);
+//	if((ret1 == AI_SUCCESS) && (ret2 == AI_SUCCESS))
+//		glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, shininess * strength);
+//	else {
+//		glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, 0.0f);
+//		set_float4(c, 0.0f, 0.0f, 0.0f, 0.0f);
+//		glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, c);
+//	}
+//
+//	max = 1;
+//	if(AI_SUCCESS == aiGetMaterialIntegerArray(mtl, AI_MATKEY_ENABLE_WIREFRAME, &wireframe, &max))
+//		fill_mode = wireframe ? GL_LINE : GL_FILL;
+//	else
+//		fill_mode = GL_FILL;
+//	glPolygonMode(GL_FRONT_AND_BACK, fill_mode);
+//
+//	max = 1;
+//	if((AI_SUCCESS == aiGetMaterialIntegerArray(mtl, AI_MATKEY_TWOSIDED, &two_sided, &max)) && two_sided)
+//		glEnable(GL_CULL_FACE);
+//	else 
+//		glDisable(GL_CULL_FACE);
+//}
+//
+// Can't send color down as a pointer to aiColor4D because AI colors are ABGR.
+//void Color4f(const struct aiColor4D *color)
+//{
+//	glColor4f(color->r, color->g, color->b, color->a);
+//}
+//void recursive_render (const struct aiScene *sc, const struct aiNode* nd)
+//{
+//	int i;
+//	unsigned int n = 0, t;
+//	struct aiMatrix4x4 m = nd->mTransformation;
+//
+//	// update transform
+//	aiTransposeMatrix4(&m);
+//	glPushMatrix();
+//	glMultMatrixf((float*)&m);
+//
+//	// draw all meshes assigned to this node
+//	for (; n < nd->mNumMeshes; ++n) {
+//		const struct aiMesh* mesh = scene->mMeshes[nd->mMeshes[n]];
+//
+//		//apply_material(sc->mMaterials[mesh->mMaterialIndex]);
+//
+//		if(mesh->mNormals == NULL) {
+//			glDisable(GL_LIGHTING);
+//		} else {
+//			glEnable(GL_LIGHTING);
+//		}
+//
+//		if(mesh->mColors[0] != NULL) {
+//			glEnable(GL_COLOR_MATERIAL);
+//		} else {
+//			glDisable(GL_COLOR_MATERIAL);
+//		}
+//
+//		for (t = 0; t < mesh->mNumFaces; ++t) {
+//			const struct aiFace* face = &mesh->mFaces[t];
+//			GLenum face_mode;
+//
+//			switch(face->mNumIndices) {
+//				case 1: face_mode = GL_POINTS; break;
+//				case 2: face_mode = GL_LINES; break;
+//				case 3: face_mode = GL_TRIANGLES; break;
+//				default: face_mode = GL_POLYGON; break;
+//			}
+//
+//			glBegin(face_mode);
+//
+//			for(i = 0; i < face->mNumIndices; i++) {
+//				int index = face->mIndices[i];
+//				if(mesh->mColors[0] != NULL)
+//					Color4f(&mesh->mColors[0][index]);
+//				if(mesh->mNormals != NULL) 
+//					glNormal3fv(&mesh->mNormals[index].x);
+//				if(mesh->mTextureCoords[0] != NULL) 
+//					glTexCoord3fv(&mesh->mTextureCoords[0][index].x);
+//				glVertex3fv(&mesh->mVertices[index].x);
+//			}
+//
+//			glEnd();
+//		}
+//
+//	}
+//
+//	// draw all children
+//	for (n = 0; n < nd->mNumChildren; ++n) {
+//		recursive_render(sc, nd->mChildren[n]);
+//	}
+//
+//	glPopMatrix();
+//}
---
+++ include/allocore/graphics/al_Asset.hpp
@@ -0,0 +1,149 @@
+#ifndef INCLUDE_AL_GRAPHICS_ASSET_HPP
+#define INCLUDE_AL_GRAPHICS_ASSET_HPP
+
+/*
+ *  A collection of functions and classes related to application mainloops
+ *  AlloSphere Research Group / Media Arts & Technology, UCSB, 2009
+ */
+
+/*
+	Copyright (C) 2006-2008. The Regents of the University of California (REGENTS). 
+	All Rights Reserved.
+
+	Permission to use, copy, modify, distribute, and distribute modified versions
+	of this software and its documentation without fee and without a signed
+	licensing agreement, is hereby granted, provided that the above copyright
+	notice, the list of contributors, this paragraph and the following two paragraphs 
+	appear in all copies, modifications, and distributions.
+
+	IN NO EVENT SHALL REGENTS BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
+	SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS, ARISING
+	OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF REGENTS HAS
+	BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+	REGENTS SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+	THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+	PURPOSE. THE SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF ANY, PROVIDED
+	HEREUNDER IS PROVIDED "AS IS". REGENTS HAS  NO OBLIGATION TO PROVIDE
+	MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+*/
+
+/*!
+	Asset manages the loading and parsing of 3D asset files
+	It wraps the AssImp library.
+	
+	A scene contains
+		- array of meshes
+		- array of materials
+		- array of textures
+		- also: array of animations, array of lights, array of cameras
+		- tree of nodes, starting from a root node
+		
+	A mesh contains
+		- name
+		- material (index into scene)
+		- array of vertices
+		- array of normals
+		- array of colors
+		- array of texcoords
+		- arrays of tangents, bitangents
+		- bones
+	
+	A material contains
+		- array of properties
+		- helper methods for retrieving textures
+	
+	A node contains 
+		- name
+		- a 4x4 matrix transform relative to its parent
+		- a list of children
+		- a number of meshes (indexing scene arrays)
+	
+	
+*/
+
+#include "allocore/graphics/al_Graphics.hpp"
+#include "allocore/types/al_Color.hpp"
+
+#include <string>
+
+namespace al{
+
+class Scene {
+public:
+	enum ImportPreset {
+		FAST,
+		QUALITY,
+		MAX_QUALITY
+	};
+	
+	struct Node {
+		class Impl;
+		
+		Node();
+		~Node();
+	
+		std::string name() const;
+		Impl * mImpl;
+	};
+	
+	struct Material {
+		struct TextureProperty {
+			bool useTexture;
+			std::string texture;
+			
+			TextureProperty() : useTexture(false) {}
+		};
+		
+		Material();
+		
+		int shading_model, two_sided, wireframe, blend_func;
+		float shininess, shininess_strength, opacity, reflectivity, refracti, bump_scaling;
+		Color diffuse, ambient, specular, emissive, transparent, reflective;
+		TextureProperty diffusemap, ambientmap, specularmap, opacitymap, emissivemap, shininessmap, lightmap, normalmap, heightmap, displacementmap, reflectionmap; 
+		std::string background;	
+	};
+	
+	static Scene * import(std::string path, ImportPreset preset = MAX_QUALITY);
+	~Scene();
+	
+	/// return number of meshes in scene
+	unsigned int meshes() const;
+	/// read a mesh from the Scene:
+	void mesh(unsigned int i, Mesh& mesh) const;
+	/// get the material index for a given mesh:
+	unsigned int meshMaterial(unsigned int i) const;
+	/// get the name of a given mesh
+	std::string meshName(unsigned int i) const;
+	
+	/// return number of materials in scene
+	unsigned int materials() const;
+	
+	/// return number of materials in scene
+	unsigned int textures() const;
+	
+	/// return number of nodes in scene
+	unsigned int nodes() const;
+	/// read a node in the scene:
+	Node& node(unsigned int i) const;
+	
+	
+	/// get scene extents
+	void getBounds(Vec3f& min, Vec3f& max) const;
+	
+	/// print out information about the Scene
+	void dump() const;
+	
+protected:
+
+	std::vector<Material> mMaterials;
+
+	class Impl;
+	Impl * mImpl;
+	
+	Scene(Impl * impl);
+};
+
+} // al::
+
+#endif /* include guard */
