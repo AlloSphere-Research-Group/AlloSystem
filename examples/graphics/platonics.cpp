0db7f78ac938049f548d8e5f36c419180fe9dfe6
l.putnam <l.putnam@bd62deb4-2030-0410-8420-fdd75d726ec8>
2010-12-14 20:55:52 +0000

    added shapes

--- examples/graphics/platonics.cpp
+++ examples/graphics/platonics.cpp
@@ -51,27 +51,12 @@ struct MyWindow : Window{
 	double angle1, angle2;
 };
 
+
 int main(){
 
-	// Platonic solid data taken from "Platonic Solids (Regular Polytopes In 3D)"
-	// http://local.wasp.uwa.edu.au/~pbourke/geometry/platonic/
-	const double phi = (1 + sqrt(5))/2; // the golden ratio
-
 	{
-		float l = sqrt(1./3);
-		float vertices[] = {
-			 l, l, l,
-			-l, l,-l,
-			 l,-l,-l,
-			-l,-l, l
-		};
+		int Nv = addTetrahedron(solids[0]);
 
-		int indices[] = {0,2,1, 0,1,3, 1,2,3, 2,0,3};
-
-		int Nv = sizeof(vertices)/sizeof(*vertices)/3;
-
-		solids[0].vertex(vertices, Nv);
-		solids[0].index(indices, sizeof(indices)/sizeof(*indices));
 		solids[0].primitive(Graphics::TRIANGLES);
 
 		for(int i=0; i<Nv; ++i){
@@ -84,23 +69,8 @@ int main(){
 	}
 
 	{
-		const float l = sqrt(1./3);
-		float vertices[] = {
-			-l, l,-l,	 l, l,-l,	// 0  1
-			-l,-l,-l,	 l,-l,-l,	// 2  3
-				-l, l, l,	 l, l, l,	// 4  5
-				-l,-l, l,	 l,-l, l,	// 6  7
-		};
+		int Nv = addCube(solids[1]);
 
-		int indices[] = {
-			6,5,4, 6,7,5, 7,1,5, 7,3,1, 3,0,1, 3,2,0, 2,4,0, 2,6,4,
-			4,1,0, 4,5,1, 2,3,6, 3,7,6
-		};
-		
-		int Nv = sizeof(vertices)/sizeof(*vertices)/3;
-
-		solids[1].vertex(vertices, Nv);
-		solids[1].index(indices, sizeof(indices)/sizeof(*indices));
 		solids[1].primitive(Graphics::TRIANGLES);
 
 		for(int i=0; i<Nv; ++i){
@@ -113,20 +83,7 @@ int main(){
 	}
 
 	{
-		float vertices[] = {
-			 1,0,0, 0, 1,0, 0,0, 1,	// 0 1 2
-			-1,0,0, 0,-1,0, 0,0,-1	// 3 4 5
-		};
-
-		int indices[] = {
-			0,1,2, 1,3,2, 3,4,2, 4,0,2,
-			1,0,5, 3,1,5, 4,3,5, 0,4,5
-		};
-		
-		int Nv = sizeof(vertices)/sizeof(*vertices)/3;
-
-		solids[2].vertex(vertices, Nv);
-		solids[2].index(indices, sizeof(indices)/sizeof(*indices));
+		int Nv = addOctahedron(solids[2]);
 		solids[2].primitive(Graphics::TRIANGLES);
 
 		for(int i=0; i<Nv; ++i){
@@ -139,27 +96,7 @@ int main(){
 	}
 
 	{
-		float a = 1.6 * 0.5;
-		float b = 1.6 / (2 * phi);
-		float vertices[] = {
-			 0, b,-a,	 b, a, 0,	-b, a, 0,	//  0  1  2
-			 0, b, a,	 0,-b, a,	-a, 0, b,	//  3  4  5
-			 a, 0, b,	 0,-b,-a,	 a, 0,-b,	//  6  7  8
-			-a, 0,-b,	 b,-a, 0,	-b,-a, 0	//  9 10 11
-		};
-
-		int indices[] = {
-			 1, 0, 2,	 2, 3, 1,	 4, 3, 5,	 6, 3, 4,
-			 7, 0, 8,	 9, 0, 7,	10, 4,11,	11, 7,10,
-			 5, 2, 9,	 9,11, 5,	 8, 1, 6,	 6,10, 8,
-			 5, 3, 2,	 1, 3, 6,	 2, 0, 9,	 8, 0, 1,
-			 9, 7,11,	10, 7, 8,	11, 4, 5,	 6, 4,10
-		};
-		
-		int Nv = sizeof(vertices)/sizeof(*vertices)/3;
-
-		solids[3].vertex(vertices, Nv);
-		solids[3].index(indices, sizeof(indices)/sizeof(*indices));
+		int Nv = addDodecahedron(solids[3]);
 		solids[3].primitive(Graphics::TRIANGLES);
 
 		for(int i=0; i<Nv; ++i){
@@ -172,73 +109,7 @@ int main(){
 	}
 
 	{
-//		float b = 1. / phi;
-//		float c = 2. - phi;
-//		float vertices[] = {
-//			 c,  0,  1,   -c,  0,  1,   -b,  b,  b,    0,  1,  c,    b,  b,  b,
-//			-c,  0,  1,    c,  0,  1,    b, -b,  b,    0, -1,  c,   -b, -b,  b,
-//			 c,  0, -1,   -c,  0, -1,   -b, -b, -b,    0, -1, -c,    b, -b, -b,
-//			-c,  0, -1,    c,  0, -1,    b,  b, -b,    0,  1, -c,   -b,  b, -b,
-//			 0,  1, -c,    0,  1,  c,    b,  b,  b,    1,  c,  0,    b,  b, -b,
-//			 0,  1,  c,    0,  1, -c,   -b,  b, -b,   -1,  c,  0,   -b,  b,  b,
-//			 0, -1, -c,    0, -1,  c,   -b, -b,  b,   -1, -c,  0,   -b, -b, -b,
-//			 0, -1,  c,    0, -1, -c,    b, -b, -b,    1, -c,  0,    b, -b,  b,
-//			 1,  c,  0,    1, -c,  0,    b, -b,  b,    c,  0,  1,    b,  b,  b,
-//			 1, -c,  0,    1,  c,  0,    b,  b, -b,    c,  0, -1,    b, -b, -b,
-//			-1,  c,  0,   -1, -c,  0,   -b, -b, -b,   -c,  0, -1,   -b,  b, -b,
-//			-1, -c,  0,   -1,  c,  0,   -b,  b,  b,   -c,  0,  1,   -b, -b,  b
-//		};
-//
-//		for(int i=0; i<Nv; i+=5){
-//			Vec3f v1(vertices[3*i+ 0], vertices[3*i+ 1], vertices[3*i+ 2]);
-//			Vec3f v2(vertices[3*i+ 3], vertices[3*i+ 4], vertices[3*i+ 5]);
-//			Vec3f v3(vertices[3*i+ 6], vertices[3*i+ 7], vertices[3*i+ 8]);
-//			Vec3f v4(vertices[3*i+ 9], vertices[3*i+10], vertices[3*i+11]);
-//			Vec3f v5(vertices[3*i+12], vertices[3*i+13], vertices[3*i+14]);
-//			
-//			Vec3f vc = (v1+v2+v3+v4+v5)/5;
-//			
-//			plato5.vertex(v1);
-//		}
-
-		float vertices[] = {
-			-0.57735, -0.57735, 0.57735,
-			0.934172,  0.356822, 0,
-			0.934172, -0.356822, 0,
-			-0.934172, 0.356822, 0,
-			-0.934172, -0.356822, 0,
-			0,  0.934172,  0.356822,
-			0,  0.934172,  -0.356822,
-			0.356822,  0,  -0.934172,
-			-0.356822,  0,  -0.934172,
-			0,  -0.934172,  -0.356822,
-			0,  -0.934172,  0.356822,
-			0.356822,  0,  0.934172,
-			-0.356822,  0,  0.934172,
-			0.57735,  0.57735,  -0.57735,
-			0.57735,  0.57735, 0.57735,
-			-0.57735,  0.57735,  -0.57735,
-			-0.57735,  0.57735,  0.57735,
-			0.57735,  -0.57735,  -0.57735,
-			0.57735,  -0.57735,  0.57735,
-			-0.57735,  -0.57735,  -0.57735
-		};
-
-		int indices[] = {
-			18, 2, 1,	11,18, 1,	14,11, 1,	 7,13, 1,	17, 7, 1,
-			 2,17, 1,	19, 4, 3,	 8,19, 3,	15, 8, 3,	12,16, 3,
-			 0,12, 3,	 4, 0, 3,	 6,15, 3,	 5, 6, 3,	16, 5, 3,
-			 5,14, 1,	 6, 5, 1,	13, 6, 1,	 9,17, 2,	10, 9, 2,
-			18,10, 2,	10, 0, 4,	 9,10, 4,	19, 9, 4,	19, 8, 7,
-			 9,19, 7,	17, 9, 7,	 8,15, 6,	 7, 8, 6,	13, 7, 6,
-			11,14, 5,	12,11, 5,	16,12, 5,	12, 0,10,	11,12,10,
-			18,11,10
-		};
-
-		int Nv = sizeof(vertices)/sizeof(*vertices)/3;
-
-		solids[4].vertex(vertices, Nv);
-		solids[4].index(indices, sizeof(indices)/sizeof(*indices));
+		int Nv = addIcosahedron(solids[4]);
 		solids[4].primitive(Graphics::TRIANGLES);
 
 		for(int i=0; i<Nv; ++i){
---
+++ include/allocore/graphics/al_Shapes.hpp
@@ -0,0 +1,52 @@
+#ifndef INCLUDE_AL_GRAPHICS_SHAPES_HPP
+#define INCLUDE_AL_GRAPHICS_SHAPES_HPP
+
+/*
+	Copyright (C) 2006-2008. The Regents of the University of California (REGENTS).
+	All Rights Reserved.
+
+	Permission to use, copy, modify, distribute, and distribute modified versions
+	of this software and its documentation without fee and without a signed
+	licensing agreement, is hereby granted, provided that the above copyright
+	notice, the list of contributors, this paragraph and the following two paragraphs
+	appear in all copies, modifications, and distributions.
+
+	IN NO EVENT SHALL REGENTS BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
+	SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS, ARISING
+	OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF REGENTS HAS
+	BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+	REGENTS SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+	THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+	PURPOSE. THE SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF ANY, PROVIDED
+	HEREUNDER IS PROVIDED "AS IS". REGENTS HAS  NO OBLIGATION TO PROVIDE
+	MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+*/
+
+#include "allocore/graphics/al_Graphics.hpp"
+#include "allocore/graphics/al_Mesh.hpp"
+
+namespace al{
+
+// Platonic solids
+// Platonic solid data taken from "Platonic Solids (Regular Polytopes In 3D)"
+// http://local.wasp.uwa.edu.au/~pbourke/geometry/platonic/
+
+/// Add tetrahedron as triangle vertices and indices
+int addTetrahedron(Mesh& m);
+
+/// Add cube as triangle vertices and indices
+int addCube(Mesh& m);
+
+/// Add octahedron as triangle vertices and indices
+int addOctahedron(Mesh& m);
+
+/// Add dodecahedron as triangle vertices and indices
+int addDodecahedron(Mesh& m);
+
+/// Add icosahedron as triangle vertices and indices
+int addIcosahedron(Mesh& m);
+
+} // al::
+
+#endif
