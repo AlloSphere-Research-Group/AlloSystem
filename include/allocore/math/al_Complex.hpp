82addfd68d384a3b11cea342aa24e35384e0a4c4
l.putnam <l.putnam@bd62deb4-2030-0410-8420-fdd75d726ec8>
2011-02-18 22:12:33 +0000

    Complex: added unit arg

--- include/allocore/math/al_Complex.hpp
+++ include/allocore/math/al_Complex.hpp
@@ -118,7 +118,7 @@ public:
 	C operator / (const T& v) const { return C(*this) /= v; }
 	
 	T arg() const { return atan2(i, r); }					///< Returns argument (angle)
-	T argUnit() const { T r=arg()/M_PI; return r>0 ? r : r+1; }	///< Return argument in unit interval [0, 1)
+	T argUnit() const { T r=arg()/M_2PI; return r>0 ? r : r+1; }	///< Return argument in unit interval [0, 1)
 	C conj() const { return C(r,-i); }						///< Returns conjugate, z*
 	T dot(const C& v) const { return r*v.r + i*v.i; }		///< Returns vector dot product
 	C exp() const { return Polar<T>(::exp(r), i); }			///< Returns e^z
