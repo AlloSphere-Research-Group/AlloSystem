44669692b507575069d2835ec4423d72e84e3d3c
l.putnam <l.putnam@bd62deb4-2030-0410-8420-fdd75d726ec8>
2011-02-26 23:23:04 +0000

    BufferObject: fixed to compile

--- include/allocore/graphics/al_BufferObject.hpp
+++ include/allocore/graphics/al_BufferObject.hpp
@@ -2,6 +2,7 @@
 #define INCLUDE_AL_GRAPHICS_BUFFEROBJECT_HPP
 
 #include "allocore/graphics/al_GPUObject.hpp"
+#include "allocore/graphics/al_Graphics.hpp"
 
 namespace al{
 
@@ -27,15 +28,76 @@ identifier.
 */
 class BufferObject : public GPUObject {
 public:
-	BufferObject(BufferType::t bufType, BufferUsage::t bufUsage)
-	:	mMapMode(AccessMode::ReadWrite), mType(bufType), mUsage(bufUsage),
-		mDataType(DataType::Unknown), mNumComps(0), mNumElems(0), mData(0)
+
+	enum AccessMode{
+		READ_ONLY				= GL_READ_ONLY,
+		WRITE_ONLY				= GL_WRITE_ONLY, 
+		READ_WRITE				= GL_READ_WRITE
+	};
+
+	enum ArrayType{
+		VERTEX_ARRAY			= GL_VERTEX_ARRAY,
+		NORMAL_ARRAY			= GL_NORMAL_ARRAY,
+		COLOR_ARRAY				= GL_COLOR_ARRAY,
+		INDEX_ARRAY				= GL_INDEX_ARRAY,
+		TEXTURE_COORD_ARRAY		= GL_TEXTURE_COORD_ARRAY,
+		EDGE_FLAG_ARRAY			= GL_EDGE_FLAG_ARRAY
+	};
+
+	enum BufferType{
+		ARRAY_BUFFER			= GL_ARRAY_BUFFER,
+		ELEMENT_ARRAY_BUFFER	= GL_ELEMENT_ARRAY_BUFFER,
+		PIXEL_PACK_BUFFER		= GL_PIXEL_PACK_BUFFER,			/**< Transfer to PBO */
+		PIXEL_UNPACK_BUFFER		= GL_PIXEL_UNPACK_BUFFER		/**< Transfer from PBO */
+	};
+
+
+	/*
+	"Static" means the data in VBO will not be changed, "Dynamic" means the data
+	will be changed frequently, and "Stream" means the data will be changed every
+	frame. "Draw" means the data will be sent to the GPU from the application,
+	"Read" means the data will be read by the application from the GPU, and "Copy"
+	means the data will be copied internally on the GPU.
+	*/
+	enum BufferUsage{
+		STREAM_DRAW		= GL_STREAM_DRAW,
+//		STREAM_READ		= GL_STREAM_READ,
+//		STREAM_COPY		= GL_STREAM_COPY,
+		STATIC_DRAW		= GL_STATIC_DRAW,
+//		STATIC_READ		= GL_STATIC_READ,
+//		STATIC_COPY		= GL_STATIC_COPY,
+		DYNAMIC_DRAW	= GL_DYNAMIC_DRAW,
+//		DYNAMIC_READ	= GL_DYNAMIC_READ,
+//		DYNAMIC_COPY	= GL_DYNAMIC_COPY
+	};
+
+	enum DataType{
+		BYTE			= GL_BYTE,
+		UNSIGNED_BYTE	= GL_UNSIGNED_BYTE,
+		SHORT			= GL_SHORT,
+		UNSIGNED_SHORT	= GL_UNSIGNED_SHORT,
+		INT				= GL_INT,
+		UNSIGNED_INT	= GL_UNSIGNED_INT,
+		FLOAT			= GL_FLOAT,
+		BYTES_2			= GL_2_BYTES,                  
+		BYTES_3			= GL_3_BYTES,
+		BYTES_4			= GL_4_BYTES,
+		DOUBLE			= GL_DOUBLE,
+		UNKNOWN
+	};
+
+	template<class T> DataType asDataType();	///< Get DataType associated with built in type
+	int numBytes(DataType type);				///< Get size of DataType, in bytes
+
+	BufferObject(BufferType bufType, BufferUsage bufUsage)
+	:	mMapMode(READ_WRITE), mType(bufType), mUsage(bufUsage),
+		mDataType(UNKNOWN), mNumComps(0), mNumElems(0), mData(0)
 	{}
 	
 	virtual ~BufferObject(){ destroy(); }
 
-	void bufferType(BufferType::t v){ mType=v; }
-	void mapMode(AccessMode::t v){ mMapMode=v; }
+	void bufferType(BufferType v){ mType=v; }
+	void mapMode(AccessMode v){ mMapMode=v; }
 	
 	void operator()(){
 		//data(); onPointerFunc(); unbind();
@@ -43,7 +105,7 @@ public:
 	}
 	void send() { (*this)(); }
 	
-	void bind() const { validate(); glBindBuffer(mType, id()); }
+	void bind(){ validate(); glBindBuffer(mType, id()); }
 	void unbind() const { glBindBuffer(mType, 0); }
 
 #ifdef AL_GRAPHICS_USE_OPENGL
@@ -63,7 +125,7 @@ public:
 	/// After using the pointer, call unmap() as soon as possible
 	template <class T>
 	bool map(T *& buf){
-		if(asType<T>() == mDataType){
+		if(asDataType<T>() == mDataType){
 			void * r = map();
 			if(r){ buf=(T *)r; return true; }
 		}
@@ -76,7 +138,7 @@ public:
 #endif
 
 	/// Set buffer data store and copy over client data
-	void data(const void * src, DataType::t dataType, int numElems, int numComps=1){
+	void data(const void * src, DataType dataType, int numElems, int numComps=1){
 		mData = (void *)src;
 		mDataType = dataType;
 		mNumElems = numElems;
@@ -87,16 +149,16 @@ public:
 	/// Set buffer data store and copy over client data
 	template <class T>
 	void data(const T * src, int numElems, int numComps=1){
-		data(src, asType<T>(), numElems, numComps);
+		data(src, asDataType<T>(), numElems, numComps);
 	}
 	
 	/// Set buffer data store without copying client data
-	void data(DataType::t dataType, int numElems, int numComps=1){
+	void data(DataType dataType, int numElems, int numComps=1){
 		data(0, dataType, numElems, numComps);
 	}
 
 	/// Set buffer data store using cached values
-	void data() const {
+	void data(){
 		bind();
 		glBufferData(mType, numBytes(mDataType)*mNumElems*mNumComps, mData, mUsage);
 		unbind();
@@ -104,16 +166,16 @@ public:
 
 
 protected:
-	AccessMode::t mMapMode;
-	BufferType::t mType;
-	BufferUsage::t mUsage;
-	DataType::t mDataType;
+	AccessMode mMapMode;
+	BufferType mType;
+	BufferUsage mUsage;
+	DataType mDataType;
 	int mNumComps;
 	int mNumElems;
 	void * mData;
 
-	virtual void onCreate(){ glGenBuffers(1, &mID); }
-	virtual void onDestroy(){ glDeleteBuffers(1, &mID); }
+	virtual void onCreate(){ glGenBuffers(1, (GLuint*)&mID); }
+	virtual void onDestroy(){ glDeleteBuffers(1, (GLuint*)&mID); }
 	virtual void onPointerFunc() = 0;
 };
 
@@ -122,11 +184,11 @@ protected:
 /// Vertex buffer object
 class VBO : public BufferObject {
 public:
-	VBO(BufferUsage::t usage=BufferUsage::StreamDraw)
-	:	BufferObject(BufferType::ArrayBuffer, usage){}
+	VBO(BufferUsage usage=STREAM_DRAW)
+	:	BufferObject(ARRAY_BUFFER, usage){}
 	
-	static void enable(){ glEnableClientState(ArrayType::VertexArray); }
-	static void disable(){ glDisableClientState(ArrayType::VertexArray); }
+	static void enable(){ glEnableClientState(VERTEX_ARRAY); }
+	static void disable(){ glDisableClientState(VERTEX_ARRAY); }
 
 protected:
 	virtual void onPointerFunc(){ glVertexPointer(mNumComps, mDataType, 0, 0); }
@@ -136,11 +198,11 @@ protected:
 /// Color buffer object
 class CBO : public BufferObject {
 public:
-	CBO(BufferUsage::t usage=BufferUsage::StreamDraw)
-	:	BufferObject(BufferType::ArrayBuffer, usage){}
+	CBO(BufferUsage usage=STREAM_DRAW)
+	:	BufferObject(ARRAY_BUFFER, usage){}
 
-	static void enable(){ glEnableClientState(ArrayType::ColorArray); }
-	static void disable(){ glDisableClientState(ArrayType::ColorArray); }
+	static void enable(){ glEnableClientState(COLOR_ARRAY); }
+	static void disable(){ glDisableClientState(COLOR_ARRAY); }
 
 protected:
 	virtual void onPointerFunc(){ glColorPointer(mNumComps, mDataType, 0, 0); }
@@ -151,8 +213,8 @@ protected:
 /// Pixel buffer object
 class PBO : public BufferObject {
 public:
-	PBO(bool packMode, BufferUsage::t usage=BufferUsage::StreamDraw)
-	:	BufferObject(packMode ? BufferType::PixelPack : BufferType::PixelUnpack, usage){}
+	PBO(bool packMode, BufferUsage usage=STREAM_DRAW)
+	:	BufferObject(packMode ? PIXEL_PACK_BUFFER : PIXEL_UNPACK_BUFFER, usage){}
 	
 //	static void enable(){ glEnableClientState(ArrayType::Vertex); }
 //	static void disable(){ glDisableClientState(ArrayType::Vertex); }
@@ -166,15 +228,15 @@ protected:
 /// Element object buffer
 class EBO : public BufferObject {
 public:
-	EBO(Prim::t prim=Prim::Points, BufferUsage::t usage=BufferUsage::StaticDraw)
-	:	BufferObject(BufferType::ElementArray, usage), mPrim(prim), mStart(0), mEnd(0)
+	EBO(Graphics::Primitive prim=Graphics::POINTS, BufferUsage usage=STATIC_DRAW)
+	:	BufferObject(ELEMENT_ARRAY_BUFFER, usage), mPrim(prim), mStart(0), mEnd(0)
 	{}
 
-	EBO& prim(Prim::t v){ mPrim=v; return *this; }
+	EBO& prim(Graphics::Primitive v){ mPrim=v; return *this; }
 	EBO& range(int start, int end){ mStart=start; mEnd=end; return *this; }
 
 protected:
-	Prim::t mPrim;
+	Graphics::Primitive mPrim;
 	int mStart, mEnd;
 	
 	virtual void onPointerFunc(){
@@ -206,6 +268,33 @@ protected:
 	EBO mEBO;
 };
 */
+
+
+template<class T> inline BufferObject::DataType BufferObject::asDataType(){ return UNKNOWN; }
+template<> inline BufferObject::DataType BufferObject::asDataType<char>(){ return BYTE; }
+template<> inline BufferObject::DataType BufferObject::asDataType<unsigned char>(){ return UNSIGNED_BYTE; }
+template<> inline BufferObject::DataType BufferObject::asDataType<short>(){ return SHORT; }
+template<> inline BufferObject::DataType BufferObject::asDataType<unsigned short>(){ return UNSIGNED_SHORT; }
+template<> inline BufferObject::DataType BufferObject::asDataType<int>(){ return INT; }
+template<> inline BufferObject::DataType BufferObject::asDataType<unsigned int>(){ return UNSIGNED_INT; }
+template<> inline BufferObject::DataType BufferObject::asDataType<float>(){ return FLOAT; }
+template<> inline BufferObject::DataType BufferObject::asDataType<double>(){ return DOUBLE; }
+
+inline int BufferObject::numBytes(BufferObject::DataType type){
+	#define CS(a,b) case a: return sizeof(b); 
+	switch(type){
+		CS(BYTE, char)
+		CS(UNSIGNED_BYTE, unsigned char)
+		CS(SHORT, short)
+		CS(UNSIGNED_SHORT, unsigned short)
+		CS(INT, int)
+		CS(UNSIGNED_INT, unsigned int)
+		CS(FLOAT, float)
+		CS(DOUBLE, double)
+		default: return 0;
+	};
+	#undef CS
+}
 
 } // al::
 #endif
