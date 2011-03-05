107e2805dc3751cba4e1140be69e200f47d77941
l.putnam <l.putnam@bd62deb4-2030-0410-8420-fdd75d726ec8>
2011-03-05 20:29:07 +0000

    unit test: added Mesh test

---
+++ unitTests/utGraphicsMesh.cpp
@@ -0,0 +1,34 @@
+#include "utAllocore.h"
+
+int utGraphicsMesh(){
+
+	{
+		const int N = 3;
+		Vec3f verts[N] = { Vec3f(1,0,0), Vec3f(0,1,0), Vec3f(0,0,1)};
+		Color colors[N] ={ Color(1,0,0), Color(0,1,0), Color(0,0,1)};
+		
+		assert(Color(0) == Color(0)); // ensure we can compare colors
+		
+		{
+			Mesh m;
+			for(int i=0; i<N; ++i){
+				m.vertex(verts[i]);
+				m.color(colors[i]);
+			}
+
+//			for(int i=0; i<N; ++i){
+//				printf("truth:   "); al::print(&verts[i][0], 3, "; "); al::println(&colors[i][0], 4);
+//				printf("in mesh: "); al::print(&m.vertices()[i][0], 3, "; "); al::println(&m.colors()[i][0], 4);
+//				printf("\n");
+//			}
+			
+			for(int i=0; i<N; ++i){
+				assert(m.vertices()[i] == verts[i]);
+				assert(m.colors()[i] == colors[i]);
+			}
+
+		}
+	}
+
+	return 0;
+}
