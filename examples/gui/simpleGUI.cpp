17a762143bbdb77180dd264ca3a6eab316cecc45
l.putnam <l.putnam@bd62deb4-2030-0410-8420-fdd75d726ec8>
2011-03-15 22:42:46 +0000

    Added simple GUI example

---
+++ examples/gui/simpleGUI.cpp
@@ -0,0 +1,26 @@
+#include "allocore/al_Allocore.hpp"
+#include "alloutil/al_ControlGLV.hpp"
+#include "GLV/glv.h"
+
+using namespace al;
+
+int main(){
+
+	glv::GLV topView(0,0);
+	glv::Button btn(glv::Rect(100)); 
+
+	btn.pos(glv::Place::CC).anchor(0.5, 0.5);
+
+	topView << btn;
+
+	Window win;
+	win.add(new StandardWindowKeyControls);
+	win.add(new GLVInputControl(&topView));
+	win.add(new GLVWindowControl(&topView));
+
+	win.create(Window::Dim(600,400), "Simple GUI");
+
+	MainLoop::start();
+
+	return 0;
+}
