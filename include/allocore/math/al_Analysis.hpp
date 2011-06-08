332323f4e7d499de9288ae14434c617313736e03
wakefield <wakefield@bd62deb4-2030-0410-8420-fdd75d726ec8>
2011-06-08 19:59:05 +0000

    linux edits

--- include/allocore/math/al_Analysis.hpp
+++ include/allocore/math/al_Analysis.hpp
@@ -6,13 +6,13 @@
  */
 
 /*
-	Copyright (C) 2006-2008. The Regents of the University of California (REGENTS). 
+	Copyright (C) 2006-2008. The Regents of the University of California (REGENTS).
 	All Rights Reserved.
 
 	Permission to use, copy, modify, distribute, and distribute modified versions
 	of this software and its documentation without fee and without a signed
 	licensing agreement, is hereby granted, provided that the above copyright
-	notice, the list of contributors, this paragraph and the following two paragraphs 
+	notice, the list of contributors, this paragraph and the following two paragraphs
 	appear in all copies, modifications, and distributions.
 
 	IN NO EVENT SHALL REGENTS BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
@@ -29,6 +29,8 @@
 
 #include "allocore/math/al_Functions.hpp"
 
+#include<limits>
+
 namespace al {
 
 /// A way to analyse data acquired gradually:
@@ -36,28 +38,28 @@ template<typename T=double>
 class MinMeanMax {
 public:
 	MinMeanMax() { clear(); }
-	
+
 	void clear() {
 		minimum = std::numeric_limits<T>::infinity();
 		maximum = -std::numeric_limits<T>::infinity();
 		sum = T(0);
 		count = 0;
 	}
-	
+
 	// add another analysis point:
 	void operator()(T val) {
 		minimum = al::min(val, minimum);
-		maximum = al::max(val, maximum); 
+		maximum = al::max(val, maximum);
 		sum += val;
 		count++;
 	}
-	
+
 	// read analyses:
 	T min() const { return minimum; }
 	T max() const { return maximum; }
 	T mean() const { return sum/count; }
 
-protected:	
+protected:
 	T minimum, maximum, sum;
 	unsigned count;
 };
--- linux/install_dependencies.sh
+++ linux/install_dependencies.sh
@@ -1,2 +1,2 @@
 sudo apt-get update
-sudo apt-get install libapr1-dev libglew-dev libglut3-dev portaudio19-dev libjack-dev libfreeimage-dev libfreetype6-dev libsndfile1-dev libcv-dev  libusb-1.0-0-dev
+sudo apt-get install libapr1-dev libglew-dev libglut3-dev portaudio19-dev libjack-dev libfreeimage-dev libfreetype6-dev libsndfile1-dev libcv-dev  libusb-1.0-0-dev liblua5.1-0-dev
