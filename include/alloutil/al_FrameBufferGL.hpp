79be34bb1629cdc7e2ae18fa3d6004c03bb52da8
wakefield <wakefield@bd62deb4-2030-0410-8420-fdd75d726ec8>
2011-06-09 03:11:21 +0000

    very rough draft of FBO

---
+++ include/alloutil/al_FrameBufferGL.hpp
@@ -0,0 +1,341 @@
+#ifndef INC_AL_UTIL_FRAMEBUFFERGL_HPP
+#define INC_AL_UTIL_FRAMEBUFFERGL_HPP
+
+#include "allocore/graphics/al_GraphicsOpenGL.hpp"
+
+/*
+ Dealing with Framebuffers in OpenGL can be tricky. This object tries to make it easier.
+ @see http://www.opengl.org/wiki/Framebuffer_Object
+ 
+ Image: a 2D array of pixels with specific format (e.g. array, texture)
+ Layered image: sequence of images of same size
+ Texture: object with several images with same format (but not necessarily same size, e.g. mip-maps)
+ Renderbuffer: object contains a single image. Can only be attached to FBOs.
+ Framebuffer-attachable image: Any image that can be attached to a framebuffer object.
+ Framebuffer-attachable layered image: Any layered image that can be attached to a framebuffer object.
+ Attachment point: A named location within a framebuffer object that a framebuffer-attachable image or layered image can be attached to. Attachment points can have limitations on the format of the images attached there.
+ Attach: To connect one object to another. This is not limited to FBOs, but attaching is a big part of them. Attachment is different from binding. Objects are bound to the context; they are attached to each other.
+ */
+
+namespace al {
+	
+	class SimpleFBO {
+	public:
+		SimpleFBO(unsigned width=512, unsigned height=512)
+		:	width(width),
+			height(height)
+		{
+			//fbo = 0; depthbuffer = 0;
+			//tex.target(Texture::TEXTURE_2D);
+			
+			textureId = rboId = fboId = 0;
+		}
+		
+		GLuint textureId;
+		GLuint rboId;
+		GLuint fboId;
+		unsigned width, height;
+		Color bg;
+		
+		void reset(int w, int h) {
+			onDestroy();
+			width = w; height = h;
+			onCreate();
+		}
+		
+		void onCreate() {
+			
+//			glInfo glInfo;
+//			glInfo.getInfo();
+//			//glInfo.printSelf();
+//			
+//			if(!glInfo.isExtensionSupported("GL_EXT_framebuffer_object")) {
+//				printf("FBO not supported on this GPU\n");
+//				exit(0);
+//			}
+			
+			// create a texture object
+			glGenTextures(1, &textureId);
+			glBindTexture(GL_TEXTURE_2D, textureId);
+			glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+			glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+			glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+			glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+			glTexParameteri(GL_TEXTURE_2D, GL_GENERATE_MIPMAP, GL_TRUE); // automatic mipmap
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, 0);
+			glBindTexture(GL_TEXTURE_2D, 0);
+			
+			// create a framebuffer object, you need to delete them when program exits.
+			glGenFramebuffersEXT(1, &fboId);
+			glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, fboId);
+			
+			// create a renderbuffer object to store depth info
+			// NOTE: A depth renderable image should be attached the FBO for depth test.
+			// If we don't attach a depth renderable image to the FBO, then
+			// the rendering output will be corrupted because of missing depth test.
+			// If you also need stencil test for your rendering, then you must
+			// attach additional image to the stencil attachement point, too.
+			glGenRenderbuffersEXT(1, &rboId);
+			glBindRenderbufferEXT(GL_RENDERBUFFER_EXT, rboId);
+			// or GL_DEPTH_COMPONENT24
+			glRenderbufferStorageEXT(GL_RENDERBUFFER_EXT, GL_DEPTH_COMPONENT24, width, height);
+			//glRenderbufferStorageEXT(GL_RENDERBUFFER_EXT, GL_DEPTH_COMPONENT, width, height);
+			glBindRenderbufferEXT(GL_RENDERBUFFER_EXT, 0);
+			
+			
+			// attach a texture to FBO color attachement point
+			glFramebufferTexture2DEXT(GL_FRAMEBUFFER_EXT, GL_COLOR_ATTACHMENT0_EXT, GL_TEXTURE_2D, textureId, 0);
+			
+			// attach a renderbuffer to depth attachment point
+			glFramebufferRenderbufferEXT(GL_FRAMEBUFFER_EXT, GL_DEPTH_ATTACHMENT_EXT, GL_RENDERBUFFER_EXT, rboId);
+			
+			
+			// check FBO status
+			//printFramebufferInfo();
+			GLenum status = glCheckFramebufferStatusEXT(GL_FRAMEBUFFER_EXT);
+			if(status != GL_FRAMEBUFFER_COMPLETE_EXT) {
+				printf("error creating FBO (%d)\n", status);
+			}
+			
+			// switch back to window-system-provided framebuffer
+			glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, 0);
+			
+			if (GraphicsGL::gl_error("SimpleFBO onCreate")) exit(0);
+		}
+		
+		void onDestroy() {
+			if (fboId) {
+				glDeleteFramebuffersEXT(1, &textureId);
+				fboId = 0;
+			}
+			if (textureId) {
+				glDeleteTextures(1, &textureId);
+				textureId = 0;
+			}
+			if (rboId) {
+				glDeleteRenderbuffersEXT(1, &textureId);
+				rboId = 0;
+			}
+		}
+		
+		void onEnter() {
+			
+			if (GraphicsGL::gl_error("SimpleFBO onEnter (pre)")) exit(0);
+			
+			glPushAttrib(GL_VIEWPORT_BIT);
+			glViewport(0, 0, width, height);
+			
+			// set rendering destination to FBO
+			glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, fboId);
+			
+			//glBindFramebuffer(GL_FRAMEBUFFER, fbo);
+			//glPushAttrib(GL_VIEWPORT_BIT);
+			//glViewport(0, 0, width, height);
+			
+//			// clear buffers
+//			glClearColor(bg.r, bg.g, bg.b, 1.);
+//			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+			
+			if (GraphicsGL::gl_error("SimpleFBO onEnter (post)")) exit(0);
+			
+		}
+		
+		void onLeave() {
+			//glBindFramebuffer(GL_FRAMEBUFFER, 0);
+			//glBindRenderbuffer(GL_RENDERBUFFER, 0);
+			
+			// unbind FBO
+			glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, 0);
+			
+			// GL_VIEWPORT_BIT
+			glPopAttrib();
+			
+			// trigger mipmaps generation explicitly
+			// NOTE: If GL_GENERATE_MIPMAP is set to GL_TRUE, then glCopyTexSubImage2D()
+			// triggers mipmap generation automatically. However, the texture attached
+			// onto a FBO should generate mipmaps manually via glGenerateMipmapEXT().
+			glEnable(GL_TEXTURE_2D);
+			glBindTexture(GL_TEXTURE_2D, textureId);
+			glGenerateMipmapEXT(GL_TEXTURE_2D);
+			glBindTexture(GL_TEXTURE_2D, 0);
+			glDisable(GL_TEXTURE_2D);
+			
+			if (GraphicsGL::gl_error("SimpleFBO onLeave")) exit(0);
+			
+		}
+		
+		void draw(Graphics& gl) {
+			gl.matrixMode(gl.PROJECTION);
+			gl.loadMatrix(Matrix4d::ortho(0, 1, 0, 1, -100, 100));
+			
+			gl.matrixMode(gl.MODELVIEW);
+			gl.loadIdentity();
+			
+			gl.lighting(false);
+			gl.blending(false);
+			
+			//tex.bind();
+			glEnable(GL_TEXTURE_2D);
+			glBindTexture(GL_TEXTURE_2D, textureId);
+			gl.color(1, 1, 1, 1);
+			gl.begin(gl.QUADS);
+			gl.texCoord	( 0, 0);
+			gl.vertex	( 0, 0, 0);
+			gl.texCoord	( 0, 1);
+			gl.vertex	( 0, 1, 0);
+			gl.texCoord	( 1, 1);
+			gl.vertex	( 1, 1, 0);
+			gl.texCoord	( 1, 0);
+			gl.vertex	( 1, 0, 0);
+			gl.end();
+			//tex.unbind();
+			glBindTexture(GL_TEXTURE_2D, 0);
+			glDisable(GL_TEXTURE_2D);
+		}
+		
+//		void printFramebufferInfo()
+//		{
+//			using namespace std;
+//			
+//			cout << "\n***** FBO STATUS *****\n";
+//			
+//			// print max # of colorbuffers supported by FBO
+//			int colorBufferCount = 0;
+//			glGetIntegerv(GL_MAX_COLOR_ATTACHMENTS_EXT, &colorBufferCount);
+//			cout << "Max Number of Color Buffer Attachment Points: " << colorBufferCount << endl;
+//			
+//			int objectType;
+//			int objectId;
+//			
+//			// print info of the colorbuffer attachable image
+//			for(int i = 0; i < colorBufferCount; ++i)
+//			{
+//				glGetFramebufferAttachmentParameterivEXT(GL_FRAMEBUFFER_EXT,
+//														 GL_COLOR_ATTACHMENT0_EXT+i,
+//														 GL_FRAMEBUFFER_ATTACHMENT_OBJECT_TYPE_EXT,
+//														 &objectType);
+//				if(objectType != GL_NONE)
+//				{
+//					glGetFramebufferAttachmentParameterivEXT(GL_FRAMEBUFFER_EXT,
+//															 GL_COLOR_ATTACHMENT0_EXT+i,
+//															 GL_FRAMEBUFFER_ATTACHMENT_OBJECT_NAME_EXT,
+//															 &objectId);
+//					
+//					std::string formatName;
+//					
+//					cout << "Color Attachment " << i << ": ";
+//					if(objectType == GL_TEXTURE)
+//						cout << "GL_TEXTURE, " << getTextureParameters(objectId) << endl;
+//					else if(objectType == GL_RENDERBUFFER_EXT)
+//						cout << "GL_RENDERBUFFER_EXT, " << getRenderbufferParameters(objectId) << endl;
+//				}
+//			}
+//			
+//			// print info of the depthbuffer attachable image
+//			glGetFramebufferAttachmentParameterivEXT(GL_FRAMEBUFFER_EXT,
+//													 GL_DEPTH_ATTACHMENT_EXT,
+//													 GL_FRAMEBUFFER_ATTACHMENT_OBJECT_TYPE_EXT,
+//													 &objectType);
+//			if(objectType != GL_NONE)
+//			{
+//				glGetFramebufferAttachmentParameterivEXT(GL_FRAMEBUFFER_EXT,
+//														 GL_DEPTH_ATTACHMENT_EXT,
+//														 GL_FRAMEBUFFER_ATTACHMENT_OBJECT_NAME_EXT,
+//														 &objectId);
+//				
+//				cout << "Depth Attachment: ";
+//				switch(objectType)
+//				{
+//					case GL_TEXTURE:
+//						cout << "GL_TEXTURE, " << getTextureParameters(objectId) << endl;
+//						break;
+//					case GL_RENDERBUFFER_EXT:
+//						cout << "GL_RENDERBUFFER_EXT, " << getRenderbufferParameters(objectId) << endl;
+//						break;
+//				}
+//			}
+//			
+//			// print info of the stencilbuffer attachable image
+//			glGetFramebufferAttachmentParameterivEXT(GL_FRAMEBUFFER_EXT,
+//													 GL_STENCIL_ATTACHMENT_EXT,
+//													 GL_FRAMEBUFFER_ATTACHMENT_OBJECT_TYPE_EXT,
+//													 &objectType);
+//			if(objectType != GL_NONE)
+//			{
+//				glGetFramebufferAttachmentParameterivEXT(GL_FRAMEBUFFER_EXT,
+//														 GL_STENCIL_ATTACHMENT_EXT,
+//														 GL_FRAMEBUFFER_ATTACHMENT_OBJECT_NAME_EXT,
+//														 &objectId);
+//				
+//				cout << "Stencil Attachment: ";
+//				switch(objectType)
+//				{
+//					case GL_TEXTURE:
+//						cout << "GL_TEXTURE, " << getTextureParameters(objectId) << endl;
+//						break;
+//					case GL_RENDERBUFFER_EXT:
+//						cout << "GL_RENDERBUFFER_EXT, " << getRenderbufferParameters(objectId) << endl;
+//						break;
+//				}
+//			}
+//			
+//			cout << endl;
+//		}
+		
+	};
+	
+	
+//class RenderBuffer : public GPUObject {
+//public:
+//	
+//protected: 	
+//	virtual void onCreate() {
+//		glGenRenderbuffers(1, (GLuint *)&mID);
+//	};
+//	virtual void onDestroy() {
+//		printf("destroy fbo\n");
+//		glDeleteRenderbuffers(1, (GLuint *)&mID);
+//	};
+//};
+//
+//class FrameBuffer : public GPUObject {
+//public:
+//	FrameBuffer() : GPUObject() {}
+//	
+//	/*
+//	 When an FBO is bound to a target, the available surfaces change. 
+//	 FBOs do not have buffers like GL_FRONT, GL_BACK, GL_AUXi, GL_ACCUM etc.
+//	 */
+//	void bind() {
+//		// set read & write to same FBO using GL_FRAMEBUFFER
+//		// alternatives could be GL_READ_FRAMEBUFFER or GL_DRAW_FRAMEBUFFER
+//		// to allow reading from one FBO (glCopyPixels) and writing to another (glDraw*)
+//		glBindFramebuffer(GL_FRAMEBUFFER, mID);
+//	}
+//	
+//	void unbind() {
+//		glBindFramebuffer(GL_FRAMEBUFFER, 0);
+//	}
+//	
+//	/*
+//	 Bound FBOs have several possible attachment points, including GL_COLOR_ATTACHMENT0 and GL_DEPTH_ATTACHMENT
+//	 */
+//	void attach(Texture& tex) {
+//		
+//	}
+//	
+//protected: 	
+//	virtual void onCreate() {
+//		printf("create fbo\n");
+//		glGenFramebuffers(1, (GLuint *)&mID);
+//		bind();
+//	};
+//	virtual void onDestroy() {
+//		printf("destroy fbo\n");
+//		glDeleteFramebuffers(1, (GLuint *)&mID);
+//	};
+//};
+	
+} // al::
+
+#endif
--- include/alloutil/al_OpenGL.hpp
+++ include/alloutil/al_OpenGL.hpp
@@ -252,6 +252,155 @@ protected:
 	bool mRebuild, mSubmit;
 };
 
+	class Lighting {
+	public:
+		
+		static void globalAmbient(Color c) {
+			glLightModelfv(GL_LIGHT_MODEL_AMBIENT, c.components);
+		}
+		
+		static void shadeModel(bool b) {
+			glShadeModel(b ? GL_SMOOTH : GL_FLAT);
+		}
+		
+		static void wireFrame(bool b=true) {
+			glPolygonMode(GL_FRONT_AND_BACK, b ? GL_LINE : GL_FILL);
+		}
+		
+		static void cullFace(bool b=true) {
+			if (b) {
+				glEnable(GL_CULL_FACE);
+			} else {
+				glDisable(GL_CULL_FACE);
+			}
+		}
+		
+		class Material {
+		public:
+			
+			Material()
+			:	ccw(true),
+			useGLColor(true),
+			twoSided(true),
+			wireFrame(false),
+			shininess(0.5),
+			ambient(0.5),
+			diffuse(ambient),
+			specular(1),
+			emission(0)
+			{}
+			
+			void begin() {
+				glFrontFace(ccw ? GL_CCW : GL_CW);
+				GLenum face = twoSided ? GL_FRONT_AND_BACK : GL_FRONT;
+				
+				glPolygonMode(face, wireFrame ? GL_LINE : GL_FILL);
+				
+				glMaterialfv(face, GL_SPECULAR, specular.components);
+				glMaterialfv(face, GL_EMISSION, emission.components);
+				glMateriali(face, GL_SHININESS, 128.f*shininess);
+				
+				if (useGLColor) {
+					glEnable(GL_COLOR_MATERIAL);
+					glColorMaterial(face, GL_AMBIENT_AND_DIFFUSE);
+				} else {
+					glDisable(GL_COLOR_MATERIAL);
+					glMaterialfv(face, GL_AMBIENT, ambient.components);
+					glMaterialfv(face, GL_DIFFUSE, diffuse.components);
+				}
+			}
+			
+			void end() {
+				
+			}
+			
+			void ambientAndDiffuse(Color c) {
+				ambient = c;
+				diffuse = c;
+			}
+			
+			// set winding (which way is front)
+			void ccwWinding(bool b=true) {
+				ccw = b;
+			}
+			
+			bool ccw;
+			bool useGLColor;
+			bool twoSided;
+			bool wireFrame;
+			float shininess;
+			Color ambient, diffuse, specular, emission;
+			
+		};
+		
+		class Light {
+		public:
+			
+			Light()
+			:	id(0),
+			diffuse(1),
+			specular(1),
+			ambient(0),
+			position(Vec3f(0, 0, 1)),
+			positionMode(1.f),
+			spotDirection(Vec3f(0, 0, -1)),
+			spotExponent(0),
+			spotCutoff(180),
+			attenuationConstant(1.f),
+			attenuationLinear(0.f),
+			attenuationQuadratic(0.f)
+			{}
+			
+			void color(const Color& c) {
+				diffuse = c;
+				specular = c;
+			}
+			
+			void direction(Vec3f d) {
+				directional();
+				position.set(d);
+			}
+			
+			void directional() { positionMode=0.f; }
+			void positional() { positionMode=1.f; }
+			
+			void begin(int i=0) {
+				id = GL_LIGHT0 + (i&7);
+				glEnable(id);
+				glLightfv(id, GL_AMBIENT, ambient.components);
+				glLightfv(id, GL_DIFFUSE, diffuse.components);
+				glLightfv(id, GL_SPECULAR, specular.components);
+				float p[] = {position[0], position[1], position[2], positionMode};
+				glLightfv(id, GL_POSITION, p);
+				
+				glLightf (id, GL_SPOT_CUTOFF, spotCutoff);
+				glLightf (id, GL_SPOT_EXPONENT, spotExponent);
+				if (spotCutoff != 180.f) {
+					glLightfv(id, GL_SPOT_DIRECTION, spotDirection.elems);
+				}
+				
+				if (positionMode != 0.f) {
+					glLightf(id, GL_CONSTANT_ATTENUATION, attenuationConstant);
+					glLightf(id, GL_LINEAR_ATTENUATION, attenuationLinear);
+					glLightf(id, GL_QUADRATIC_ATTENUATION, attenuationQuadratic);
+				}
+			}
+			
+			void end() {
+				glDisable(id);
+			}
+			
+			int id;
+			Color diffuse, specular, ambient;
+			Vec3f position;
+			float positionMode;	// 0==directional, 1==positional
+			Vec3f spotDirection;// valid only when spotCutoff != 180
+			int spotExponent;	// 0..128, increases focussing of light in spotDirection
+			float spotCutoff;	// 0..90 (cone) or 180 (omni)
+			float attenuationConstant, attenuationLinear, attenuationQuadratic;
+			
+		};
+	};
 
 } // al::
 
