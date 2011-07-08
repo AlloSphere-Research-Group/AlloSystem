9979b072456fb725bb6a386b477df7b92a3555df
wakefield <wakefield@bd62deb4-2030-0410-8420-fdd75d726ec8>
2011-07-08 21:31:12 +0000

    added print() methods to Vec and Quat

--- unitTests/utSpatial.cpp
+++ unitTests/utSpatial.cpp
@@ -3,23 +3,6 @@
 int utSpatial(){
 
 	{
-		Quatd q0(2.4e-14, 0, 1, 0), q1(1, 0, -0.0004, 0);
-		
-		for (int i=0; i<10; i++) {
-			Quatd s = Quatd::slerp(q0, q1, i*0.1);
-			
-			Vec3d x = s.toVectorX();
-			Vec3d y = s.toVectorY();
-			Vec3d z = s.toVectorZ();
-			
-			s.print();
-			x.print();
-			y.print();
-			z.print();
-		}
-	}
-
-	{
 		Pose a;
 	}
 
