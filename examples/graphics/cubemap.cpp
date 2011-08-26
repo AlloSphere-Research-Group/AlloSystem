59717326f87620ee376078dd7d52cb3d9e9db014
wakefield <wakefield@bd62deb4-2030-0410-8420-fdd75d726ec8>
2011-08-26 16:31:21 +0000

    cleaned up cubemap example

--- examples/graphics/cubemap.cpp
+++ examples/graphics/cubemap.cpp
@@ -3,6 +3,8 @@
 #include "alloutil/al_OpenGL.hpp"
 //#include "alloutil/al_FrameBufferGL.hpp"
 
+#define TEST
+
 using namespace al;
 
 static GraphicsGL gl;
@@ -10,6 +12,223 @@ static Mesh mesh, grid;
 static Stereographic stereo;
 static Camera cam;
 Nav nav;
+
+class CubeMapTexture : public GPUObject {
+public:
+	CubeMapTexture(int resolution=1024) 
+	:	GPUObject(),
+		mResolution(resolution) 
+	{
+		// create mesh for drawing map:
+		mMapMesh.primitive(Graphics::QUADS);
+		mMapMesh.color(1,1,1,1);
+		int mapSteps =100;
+		double step = 1./mapSteps;
+		for (double x=0; x<=1.; x+=step) {
+			for (double y=0; y<=1.; y+=step) {
+				drawMapVertex(x,		y);
+				drawMapVertex(x,		y+step);
+				drawMapVertex(x+step,	y+step);
+				drawMapVertex(x+step,	y);
+			}
+		}
+	}
+	
+	virtual ~CubeMapTexture() {}
+	
+	virtual void onCreate() {
+		// create cubemap texture:
+		// RGBA8 Cubemap texture, 24 bit depth texture, mResolution x mResolution
+		glGenTextures(1, (GLuint *)&mID);
+		glBindTexture(GL_TEXTURE_CUBE_MAP, mID);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		
+		// NULL means reserve texture memory, but texels are undefined
+		for (int i=0; i<6; i++) {
+			glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X+i, 0, GL_RGBA8, mResolution, mResolution, 0, GL_BGRA, GL_UNSIGNED_BYTE, NULL);
+		}
+				
+		// clean up:
+		glBindTexture(GL_TEXTURE_CUBE_MAP, 0);
+		
+		//printf("created CubeMapTexture %dx%d\n", mResolution, mResolution);
+	}
+	
+	virtual void onDestroy() {
+		glDeleteTextures(1, (GLuint *)&mID);
+		mID=0;
+	}
+	
+	void bind() const {
+		glEnable(GL_TEXTURE_CUBE_MAP);
+		glBindTexture(GL_TEXTURE_CUBE_MAP, mID);
+	}
+	
+	void unbind() const {
+		glBindTexture(GL_TEXTURE_CUBE_MAP, 0);
+		glDisable(GL_TEXTURE_CUBE_MAP);
+	}	
+	
+	unsigned resolution() const { return mResolution; }
+	
+	// useful for debugging
+	// draws full cubemap in a cylindrical projection
+	void drawMap(Graphics& gl, double x0=0., double y0=0., double x1=1., double y1=1.) {
+		bind();
+		gl.color(1, 1, 1, 1);
+		gl.pushMatrix();
+		gl.translate(x0, y0, 0);
+		gl.scale((x1-x0), (y1-y0), 1.);
+		gl.draw(mMapMesh);
+		gl.popMatrix();
+		unbind();
+	}
+
+protected:
+	inline void drawMapVertex(double x, double y) {
+		// x runs 0..1, convert to angle -PI..PI:
+		double az = M_PI * (x*2.-1.);
+		// y runs 0..1, convert to angle -PI_2..PI_2:
+		double el = M_PI * 0.5 * (y*2.-1.);
+		// convert polar to normal:
+		double x1 = sin(az);
+		double y1 = sin(el);
+		double z1 = cos(az);
+		Vec3d v(x1, y1, z1);
+		v.normalize();
+		mMapMesh.texCoord	( v );
+		mMapMesh.vertex	( x, y, 0);
+	}
+	
+	unsigned mResolution;
+	Mesh mMapMesh;
+};
+
+class CubeMapFBO : public CubeMapTexture {
+public:
+	CubeMapFBO(int resolution=1024) 
+	:	CubeMapTexture(resolution),
+		mFboId(0),
+		mRboId(0),
+		mClearColor(0)
+	{}
+	
+	virtual ~CubeMapFBO() {}
+	
+	virtual void onCreate() {
+		CubeMapTexture::onCreate();
+		
+		//-------------------------
+		glGenFramebuffersEXT(1, &mFboId);
+		glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, mFboId);
+		//Attach one of the faces of the Cubemap texture to this FBO
+		glFramebufferTexture2DEXT(GL_FRAMEBUFFER_EXT, GL_COLOR_ATTACHMENT0_EXT, GL_TEXTURE_CUBE_MAP_POSITIVE_X, id(), 0);
+
+		
+		glGenRenderbuffersEXT(1, &mRboId);
+		glBindRenderbufferEXT(GL_RENDERBUFFER_EXT, mRboId);
+		glRenderbufferStorageEXT(GL_RENDERBUFFER_EXT, GL_DEPTH_COMPONENT24, mResolution, mResolution);
+		//-------------------------
+		//Attach depth buffer to FBO
+		glFramebufferRenderbufferEXT(GL_FRAMEBUFFER_EXT, GL_DEPTH_ATTACHMENT_EXT, GL_RENDERBUFFER_EXT, mRboId);
+		
+		//-------------------------
+		//Does the GPU support current FBO configuration?
+		GLenum status;
+		status = glCheckFramebufferStatusEXT(GL_FRAMEBUFFER_EXT);
+		if (status != GL_FRAMEBUFFER_COMPLETE_EXT) {
+			printf("GPU does not support required FBO configuration\n");
+			exit(0);
+		}
+		
+		// cleanup:
+		glBindRenderbufferEXT(GL_RENDERBUFFER_EXT, 0);
+		glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, 0);
+	}
+	
+	virtual void onDestroy() {
+		glDeleteRenderbuffersEXT(1, &mRboId);
+		glDeleteFramebuffersEXT(1, &mFboId);
+		mRboId = mFboId = 0;
+		
+		CubeMapTexture::onDestroy();
+	}
+	
+	void capture(Graphics& gl, const Camera& cam, const Pose& pose, Drawable& draw) {
+		
+		Vec3d pos = pose.pos();
+		Vec3d ux, uy, uz; 
+		pose.unitVectors(ux, uy, uz);
+		
+		mProjection = Matrix4d::perspective(90, 1, cam.near(), cam.far());
+		
+		glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, mFboId);
+		for (int i=0; i<6; i++) {
+			glFramebufferTexture2DEXT(GL_FRAMEBUFFER_EXT, GL_COLOR_ATTACHMENT0_EXT, GL_TEXTURE_CUBE_MAP_POSITIVE_X+i, id(), 0);
+			
+			gl.viewport(0, 0, resolution(), resolution());
+			gl.clearColor(clearColor());
+			gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+			
+			switch (i) {
+				case 0:
+					// GL_TEXTURE_CUBE_MAP_POSITIVE_X   
+					mModelView = Matrix4d::lookAt(uz, uy, -ux, pos);
+					break;
+				case 1:
+					// GL_TEXTURE_CUBE_MAP_NEGATIVE_X   
+					mModelView = Matrix4d::lookAt(-uz, uy, ux, pos);
+					break;
+				case 2:
+					// GL_TEXTURE_CUBE_MAP_POSITIVE_Y   
+					mModelView = Matrix4d::lookAt(ux, -uz, uy, pos);
+					break;
+				case 3:
+					// GL_TEXTURE_CUBE_MAP_NEGATIVE_Y   
+					mModelView = Matrix4d::lookAt(ux, uz, -uy, pos);
+					break;
+				case 4:
+					// GL_TEXTURE_CUBE_MAP_POSITIVE_Z   
+					mModelView = Matrix4d::lookAt(ux, uy, uz, pos);
+					break;
+				default:
+					// GL_TEXTURE_CUBE_MAP_NEGATIVE_Z   
+					mModelView = Matrix4d::lookAt(-ux, uy, -uz, pos);
+					break;
+			}
+			
+			// apply camera transform:
+			gl.pushMatrix(gl.PROJECTION);
+			gl.loadMatrix(mProjection);
+			gl.pushMatrix(gl.MODELVIEW);
+			gl.loadMatrix(mModelView);
+			
+			draw.onDraw(gl);
+			
+			gl.popMatrix(gl.PROJECTION);
+			gl.popMatrix(gl.MODELVIEW);
+		}
+		glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, 0);
+	}
+	
+	Color clearColor() const { return mClearColor; }
+	Matrix4d projection() const { return mProjection; }
+	Matrix4d modelView() const { return mModelView; }
+	GLuint fbo() { return mFboId; }
+	GLuint rbo() { return mRboId; }
+
+	CubeMapFBO& clearColor(const Color& c) { mClearColor = c; return *this; }
+	
+protected:
+	GLuint mFboId, mRboId;
+	Color mClearColor;
+	Matrix4d mProjection;
+	Matrix4d mModelView;
+};
 
 void viewOrtho(double w, double h)                            // Set Up An Ortho View
 {
@@ -22,58 +241,9 @@ void viewOrtho(double w, double h)                            // Set Up An Ortho
     glLoadIdentity();                       // Reset The Matrix
 }
 
-void drawQuad() {
-	gl.texCoord	( sin(-0.5*M_PI), sin(0.5*M_PI), 1);
-	gl.vertex	( 0, 0, 0);
-	gl.texCoord	( sin(-0.5*M_PI), sin(-0.5*M_PI), 1);
-	gl.vertex	( 0, 1, 0);
-	gl.texCoord	( sin(0.5*M_PI), sin(-0.5*M_PI), 1);
-	gl.vertex	( 1, 1, 0);
-	gl.texCoord	( sin(0.5*M_PI), sin(0.5*M_PI), 1);
-	gl.vertex	( 1, 0, 0);
-}
-
-void drawVertex(double x, double y) {
-
-	Vec3d v(1.-x*2., 1.-y*2., 1.);
-	v.normalize();
-
-	gl.texCoord	( v );
-	gl.vertex	( x, y, 0);
-}
-
-void drawVertex1(double x, double y) {
-
-	// x runs 0..1, convert to angle -PI..PI:
-	double az = M_PI * (x*2.-1.);
-	// y runs 0..1, convert to angle -PI_2..PI_2:
-	double el = M_PI * 0.5 * (y*2.-1.);
-	
-	// convert polar to normal:
-	double x1 = sin(az);
-	double y1 = sin(el);
-	double z1 = cos(az);
-
-	Vec3d v(x1, y1, z1);
-	v.normalize();
-
-	gl.texCoord	( v );
-	gl.vertex	( x, y, 0);
-}
-
-void drawQuad1() {
-	double step = 1./30.;
-	for (double x=0; x<=1.; x+=step) {
-		for (double y=0; y<=1.; y+=step) {
-			drawVertex1(x, y);
-			drawVertex1(x, y+step);
-			drawVertex1(x+step, y+step);
-			drawVertex1(x+step, y);
-		}
-	}
-}
-
 struct MyWindow : Window, public Drawable{
+
+	CubeMapFBO mCubeFBO;
 	
 	bool onKeyDown(const Keyboard& k){	
 		
@@ -90,178 +260,39 @@ struct MyWindow : Window, public Drawable{
 	}
 	
 	bool onCreate() {
-		return true;
-	}
-	
-	bool onDestroy() {
+		mCubeFBO.validate();
 		return true;
 	}
 
 	bool onFrame(){
+		
+		nav.step();
 	
 		int w = width();
 		int h = height();
 		
-		int cubelen = 1024;
-	
-		unsigned int color_tex, fb, depth_rb;
-		
-		nav.step();
-		
-		
 		Viewport vp(width(), height());
-		Pose& pose = nav;
-		double near = cam.near();
-		double far = cam.far();
-		const Vec3d& pos = pose.pos();
-		double fovy = cam.fovy();
-		double aspect = vp.aspect();
-		Vec3d ux, uy, uz; pose.unitVectors(ux, uy, uz);
-		Matrix4d mProjection = Matrix4d::perspective(90, 1, near, far);
-		Matrix4d mModelViews[6];
-		// GL_TEXTURE_CUBE_MAP_POSITIVE_X   
-		mModelViews[0] = Matrix4d::lookAt(uz, uy, -ux, pos);
-		// GL_TEXTURE_CUBE_MAP_NEGATIVE_X   
-		mModelViews[1] = Matrix4d::lookAt(-uz, uy, ux, pos);
-		// GL_TEXTURE_CUBE_MAP_POSITIVE_Y   
-		mModelViews[2] = Matrix4d::lookAt(ux, -uz, uy, pos);
-		// GL_TEXTURE_CUBE_MAP_NEGATIVE_Y   
-		mModelViews[3] = Matrix4d::lookAt(ux, uz, -uy, pos);
-		// GL_TEXTURE_CUBE_MAP_POSITIVE_Z   
-		mModelViews[4] = Matrix4d::lookAt(ux, uy, uz, pos);
-		// GL_TEXTURE_CUBE_MAP_NEGATIVE_Z   
-		mModelViews[5] = Matrix4d::lookAt(-ux, uy, -uz, pos);
 		
-		stereo.clearColor(Color(0));
-		
-		//RGBA8 Cubemap texture, 24 bit depth texture, 256x256
-		glGenTextures(1, &color_tex);
-		glBindTexture(GL_TEXTURE_CUBE_MAP, color_tex);
-		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
-		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-		//NULL means reserve texture memory, but texels are undefined
-		for (int i=0; i<6; i++) {
-			glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X+i, 0, GL_RGBA8, cubelen, cubelen, 0, GL_BGRA, GL_UNSIGNED_BYTE, NULL);
-		}
-		//-------------------------
-		glGenFramebuffersEXT(1, &fb);
-		glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, fb);
-		//Attach one of the faces of the Cubemap texture to this FBO
-		glFramebufferTexture2DEXT(GL_FRAMEBUFFER_EXT, GL_COLOR_ATTACHMENT0_EXT, GL_TEXTURE_CUBE_MAP_POSITIVE_X, color_tex, 0);
-		//-------------------------
-		glGenRenderbuffersEXT(1, &depth_rb);
-		glBindRenderbufferEXT(GL_RENDERBUFFER_EXT, depth_rb);
-		glRenderbufferStorageEXT(GL_RENDERBUFFER_EXT, GL_DEPTH_COMPONENT24, cubelen, cubelen);
-		//-------------------------
-		//Attach depth buffer to FBO
-		glFramebufferRenderbufferEXT(GL_FRAMEBUFFER_EXT, GL_DEPTH_ATTACHMENT_EXT, GL_RENDERBUFFER_EXT, depth_rb);
-		//-------------------------
-		//Does the GPU support current FBO configuration?
-		GLenum status;
-		status = glCheckFramebufferStatusEXT(GL_FRAMEBUFFER_EXT);
-		if (status != GL_FRAMEBUFFER_COMPLETE_EXT) {
-			printf("GPU does not support required FBO configuration\n");
-			exit(0);
-		}
+		// capture the scene:
+		mCubeFBO.capture(gl, cam, nav, *this);
 
-		glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, fb);
-		
-		for (int i=0; i<6; i++) {
-		
-			glFramebufferTexture2DEXT(GL_FRAMEBUFFER_EXT, GL_COLOR_ATTACHMENT0_EXT, GL_TEXTURE_CUBE_MAP_POSITIVE_X+i, color_tex, 0);
-		
-			gl.viewport(0, 0, cubelen, cubelen);
-			gl.clearColor(stereo.clearColor());
-			gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-			// apply camera transform:
-			gl.pushMatrix(gl.PROJECTION);
-			gl.loadMatrix(mProjection);
-			gl.pushMatrix(gl.MODELVIEW);
-			gl.loadMatrix(mModelViews[i]);
-			
-			onDraw(gl);
-		
-		}
-		//Bind 0, which means render to back buffer, as a result, fb is unbound
-		glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, 0);
-		
-		// now draw texture:		
+		// now draw the captured texture:		
 		gl.viewport(vp.l, vp.b, vp.w, vp.h);
-		gl.clearColor(stereo.clearColor());
+		gl.clearColor(mCubeFBO.clearColor());
 		gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-		viewOrtho(1, 1);
 		
+		// ortho for 2D, ranging from 0..1 in each axis:
+		gl.projection(Matrix4d::ortho(0, 1, 1, 0, -1, 1));
+		gl.modelView(Matrix4d::identity());
 		gl.lighting(false);
 		gl.blending(false);
+		mCubeFBO.drawMap(gl);
 		
-//		//tex.bind();
-		glEnable(GL_TEXTURE_CUBE_MAP);
-		glBindTexture(GL_TEXTURE_CUBE_MAP, color_tex);
-		gl.color(1, 1, 1, 1);
-
-		gl.begin(gl.QUADS);
-		drawQuad1();
-		gl.end();
-		
-		//tex.unbind();
-		glBindTexture(GL_TEXTURE_CUBE_MAP, 0);
-		glDisable(GL_TEXTURE_CUBE_MAP);
-		
-		//Delete resources
-		glDeleteTextures(1, &color_tex);
-		glDeleteRenderbuffersEXT(1, &depth_rb);
-		glDeleteFramebuffersEXT(1, &fb);
-		
-		
-		
-/*		
-		
-		// draw 6 times to FBO, capture as texture
-		fbo.onEnter();
-		//stereo.draw(gl, cam, nav, vp, *this);
-		
-		Viewport vp(width(), height());
-		Pose& pose = nav;
-		double near = cam.near();
-		double far = cam.far();
-		const Vec3d& pos = pose.pos();
-		
-//		glPushAttrib(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_ENABLE_BIT | GL_VIEWPORT_BIT);
-//		gl.clearColor(stereo.clearColor());
-		
-		//glDrawBuffer(GL_BACK);
-		gl.viewport(vp.l, vp.b, vp.w, vp.h);
-		gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-		
-		
-		double fovy = cam.fovy();
-		double aspect = vp.aspect();
-		Vec3d ux, uy, uz; pose.unitVectors(ux, uy, uz);
-		Matrix4d mProjection = Matrix4d::perspective(fovy, aspect, near, far);
-		Matrix4d mModelView = Matrix4d::lookAt(ux, uy, uz, pos);
-
-		// apply camera transform:
-		gl.pushMatrix(gl.PROJECTION);
-		gl.loadMatrix(mProjection);
-		gl.pushMatrix(gl.MODELVIEW);
-		gl.loadMatrix(mModelView);
-		
-		onDraw(gl);
-
-		gl.popMatrix(gl.PROJECTION);
-		gl.popMatrix(gl.MODELVIEW);
-		
-//		glPopAttrib();
-		
-		fbo.onLeave();
-		
-		// draw textures
-		fbo.draw(gl);
-*/		
-		
+		return true;
+	}
+	
+	bool onDestroy() {
+		mCubeFBO.destroy();
 		return true;
 	}
 
@@ -320,7 +351,7 @@ int main(){
 	grid.scale(world_radius);
 	
 	MyWindow win;
-	win.create(Window::Dim(100, 0, 640, 480), "Omnigraphic Example", 60);
+	win.create(Window::Dim(100, 0, 640, 480), "Cube Map FBO Example", 60);
 	
 	win.displayMode(win.displayMode() | DisplayMode::StereoBuf);
 
