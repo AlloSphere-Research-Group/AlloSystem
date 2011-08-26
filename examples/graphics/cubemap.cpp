70008d2a7fc50fbc09c3f110bbc89d19d032dd9b
wakefield <wakefield@bd62deb4-2030-0410-8420-fdd75d726ec8>
2011-08-26 17:55:49 +0000

    more cubemap example

--- examples/graphics/cubemap.cpp
+++ examples/graphics/cubemap.cpp
@@ -1,17 +1,18 @@
 #include "allocore/al_Allocore.hpp"
 #include "alloutil/al_ControlNav.hpp"
 #include "alloutil/al_OpenGL.hpp"
-//#include "alloutil/al_FrameBufferGL.hpp"
 
 #define TEST
 
 using namespace al;
 
 static GraphicsGL gl;
-static Mesh mesh, grid;
+static Mesh mesh, grid, cube;
 static Camera cam;
 static Nav nav;
 static CubeMapFBO cubeFBO;
+
+unsigned drawMode = 1;
 
 struct MyWindow : Window, public Drawable{
 
@@ -25,18 +26,55 @@ struct MyWindow : Window, public Drawable{
 		// capture the scene:
 		cubeFBO.capture(gl, cam, nav, *this);
 
-		// now draw the captured texture:		
+		// now use the captured texture:		
 		gl.viewport(0, 0, width(), height());
-		gl.clearColor(cubeFBO.clearColor());
+		gl.clearColor(0.2, 0.2, 0.2, 0);
 		gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 		
+		// first, draw it as a cylindrical map in the background:
 		// ortho for 2D, ranging from 0..1 in each axis:
 		gl.projection(Matrix4d::ortho(0, 1, 1, 0, -1, 1));
 		gl.modelView(Matrix4d::identity());
-		
+		gl.depthTesting(false);
 		gl.lighting(false);
 		gl.blending(false);
 		cubeFBO.drawMap(gl);
+					
+					
+		// second, use it to texture a rotating object:
+		gl.projection(Matrix4d::perspective(70, width()/(double)height(), cam.near(), cam.far()));
+		gl.modelView(Matrix4d::lookAt(Vec3d(0, 0, 4), Vec3d(0, 0, 2), Vec3d(0, 1, 0)));
+		
+		// rotate over time:
+		gl.rotate(MainLoop::now()*30., 0.707, 0.707, 0.);
+		
+		gl.lighting(false);
+		gl.blending(false);
+		gl.depthTesting(true);
+		
+		// use cubemap texture
+		cubeFBO.bind();
+		
+		// generate texture coordinates from the object:
+		GLenum map = GL_OBJECT_LINEAR;
+		//GLenum map = GL_REFLECTION_MAP;
+		//GLenum map = GL_NORMAL_MAP;
+		//GLenum map = GL_EYE_LINEAR;
+		//GLenum map = GL_SPHERE_MAP;		
+		glTexGeni(GL_S, GL_TEXTURE_GEN_MODE, map); 
+		glTexGeni(GL_T, GL_TEXTURE_GEN_MODE, map); 
+		glTexGeni(GL_R, GL_TEXTURE_GEN_MODE, map); 
+		glEnable(GL_TEXTURE_GEN_S); 
+		glEnable(GL_TEXTURE_GEN_T); 
+		glEnable(GL_TEXTURE_GEN_R);
+		
+		gl.draw(cube);
+		
+		glDisable(GL_TEXTURE_GEN_S); 
+		glDisable(GL_TEXTURE_GEN_T); 
+		glDisable(GL_TEXTURE_GEN_R);
+		
+		cubeFBO.unbind();
 		
 		return true;
 	}
@@ -54,8 +92,6 @@ int main(){
 	double world_radius = 50;
 	
 	nav.smooth(0.8);
-	nav.pos(0, 0, -20);
-
 	cam.near(1).far(world_radius);
 	
 	// set up mesh:
@@ -72,6 +108,7 @@ int main(){
 		}
 	}
 	
+	// set up grid:
 	grid.primitive(Graphics::LINES);
 	double stepsize = 1./2;
 	for (double x=-1; x<=1; x+= stepsize) {
@@ -91,11 +128,15 @@ int main(){
 	}}
 	grid.scale(world_radius);
 	
+	// set up cube:
+	cube.color(1,1,1,1);
+	cube.primitive(Graphics::TRIANGLES);
+	addCube(cube);
+	cube.generateNormals();
+	
 	MyWindow win;
 	win.create(Window::Dim(100, 0, 640, 480), "Cube Map FBO Example", 60);
-	
 	win.displayMode(win.displayMode() | DisplayMode::StereoBuf);
-
 	win.add(new StandardWindowKeyControls);
 	win.add(new NavInputControl(nav));
 
