68d6c72bd6071e08aa7a5f7df44c7de633a2ba5a
l.putnam <l.putnam@bd62deb4-2030-0410-8420-fdd75d726ec8>
2011-08-30 22:52:47 +0000

    Header info update

--- include/allocore/spatial/al_DistAtten.hpp
+++ include/allocore/spatial/al_DistAtten.hpp
@@ -1,11 +1,10 @@
 #ifndef INCLUDE_AL_DIST_ATTEN_HPP
 #define INCLUDE_AL_DIST_ATTEN_HPP
 
-/*
- *  AlloSphere Research Group / Media Arts & Technology, UCSB, 2009
- */
+/*	Allocore --
+	Multimedia / virtual environment application class library
 
-/*
+	Copyright (C) 2009. AlloSphere Research Group, Media Arts & Technology, UCSB.
 	Copyright (C) 2006-2008. The Regents of the University of California (REGENTS). 
 	All Rights Reserved.
 
@@ -25,6 +24,14 @@
 	PURPOSE. THE SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF ANY, PROVIDED
 	HEREUNDER IS PROVIDED "AS IS". REGENTS HAS  NO OBLIGATION TO PROVIDE
 	MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+
+
+	File description:
+	Distance attentuation functions useful for calculating sound pressure
+	levels, fog density, etc. within a space.
+
+	File author(s):
+	Lance Putnam, 2010, putnam.lance@gmail.com
 */
 
 #include <math.h>
