8bec96ac9351ffe96c5518914155e8a34d21a9bd
l.putnam <l.putnam@bd62deb4-2030-0410-8420-fdd75d726ec8>
2011-08-30 22:58:16 +0000

    Header info update

--- include/allocore/types/al_Color.hpp
+++ include/allocore/types/al_Color.hpp
@@ -1,18 +1,17 @@
 #ifndef INCLUDE_AL_COLOR_HPP
 #define INCLUDE_AL_COLOR_HPP
 
-/*
- *  AlloSphere Research Group / Media Arts & Technology, UCSB, 2009
- */
+/*	Allocore --
+	Multimedia / virtual environment application class library
 
-/*
-	Copyright (C) 2006-2008. The Regents of the University of California (REGENTS).
+	Copyright (C) 2009. AlloSphere Research Group, Media Arts & Technology, UCSB.
+	Copyright (C) 2006-2008. The Regents of the University of California (REGENTS). 
 	All Rights Reserved.
 
 	Permission to use, copy, modify, distribute, and distribute modified versions
 	of this software and its documentation without fee and without a signed
 	licensing agreement, is hereby granted, provided that the above copyright
-	notice, the list of contributors, this paragraph and the following two paragraphs
+	notice, the list of contributors, this paragraph and the following two paragraphs 
 	appear in all copies, modifications, and distributions.
 
 	IN NO EVENT SHALL REGENTS BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
@@ -25,6 +24,13 @@
 	PURPOSE. THE SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF ANY, PROVIDED
 	HEREUNDER IS PROVIDED "AS IS". REGENTS HAS  NO OBLIGATION TO PROVIDE
 	MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+
+
+	File description:
+	RGBA and HSV color classes
+
+	File author(s):
+	Lance Putnam, 2010, putnam.lance@gmail.com
 */
 
 #include "allocore/system/pstdint.h"
