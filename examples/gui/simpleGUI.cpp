f2b6ba52bdf54d531a11d93dbb9de14460f887f7
l.putnam <l.putnam@bd62deb4-2030-0410-8420-fdd75d726ec8>
2011-09-14 20:31:47 +0000

    example: add comments to simpleGUI

--- examples/gui/simpleGUI.cpp
+++ examples/gui/simpleGUI.cpp
@@ -1,3 +1,13 @@
+/*
+Allocore Example: Simple GUI
+
+Description:
+The example demonstrates how to add a GUI (using GLV) to your application.
+
+Author:
+Lance Putnam, 4/25/2011
+*/
+
 #include "allocore/al_Allocore.hpp"
 #include "alloutil/al_ControlGLV.hpp"
 #include "GLV/glv.h"
@@ -6,21 +16,31 @@ using namespace al;
 
 int main(){
 
-	glv::GLV topView(0,0);
+	// Create the top-level GUI view
+	glv::GLV topView;
+	
+	// Create a button
 	glv::Button btn(glv::Rect(100)); 
 
+	// Tell our button to keep itself centered in the GUI
 	btn.pos(glv::Place::CC).anchor(0.5, 0.5);
 
+	// Add the button to the GUI
 	topView << btn;
 
+
 	Window win;
-	win.add(*new StandardWindowKeyControls);
-	win.add(*new GLVInputControl(topView));
+
+	// Prepend GUI input handler so it receives events first
+	win.prepend(*new GLVInputControl(topView));
+	
+	// Append GUI window control so the GUI is drawn last
 	win.add(*new GLVWindowControl(topView));
+
+	win.add(*new StandardWindowKeyControls);
 
 	win.create(Window::Dim(600,400), "Simple GUI");
 
 	MainLoop::start();
-
 	return 0;
 }
