479b4bb71e79c359b93feaf9ab9afb15b008bd88
matt <matt@bd62deb4-2030-0410-8420-fdd75d726ec8>
2011-09-21 18:17:33 +0000

    Simple panning example.

---
+++ examples/sound/simplePan.cpp
@@ -0,0 +1,54 @@
+/*
+ By Matt Wright 9/21/11
+ Very simple equal-amplitude panner using App and gamma.
+
+*/
+
+
+#include "alloutil/al_App.hpp"
+#include "Gamma/Oscillator.h"
+using namespace al;
+
+
+struct PanApp : App {
+  gam::TableSine<> sine;
+  gam::TableSine<> pansine;
+  
+
+  PanApp():
+    sine(440.0f),
+    pansine(1.0f)
+  {}
+
+  virtual void onSound(AudioIOData& io) {
+    gam::Sync::master().spu(audioIO().fps());
+
+    while(io()){
+      float p = (pansine() + 1.)*0.5; // 0=left, 1=right
+      float s = sine();
+
+      io.out(0) = s * (1-p);
+      io.out(1) = s * p;
+    }
+  }
+};
+
+int main() {
+  PanApp app;
+
+  app.initWindow(
+    Window::Dim(600,400), // window dimensions
+    "Dot", // name on window
+    30.0f, // frames per second
+    DisplayMode::DefaultBuf | DisplayMode::Multisample // makes things look nice
+  );
+
+  app.initAudio(
+    44100, // sample rate, in Hz
+    128, // block size
+    2, // number of output channels to open
+    0 // number of input channels to open
+  );
+
+  app.start();
+}
