12c74a8f141b2b05a1d85f302afad32dc41efaa2
l.putnam <l.putnam@bd62deb4-2030-0410-8420-fdd75d726ec8>
2011-10-20 02:02:42 +0000

    add more Array unit tests

--- unitTests/utTypes.cpp
+++ unitTests/utTypes.cpp
@@ -4,26 +4,193 @@ typedef double data_t;
 
 int utTypes(){
 	
+
+	// Array
+	{	// test the basic C functionality
+		const int comps = 1;	//
+		const int dims = 2;		// no. dimensions
+		const int size0 = 2;	// no. elements in dimension 1
+		const int size1 = 64;	// no. elements in dimension 2
+		const int stride0 = comps * sizeof(data_t);	// byte offset at dim 0
+		const int stride1 = stride0*size0;
+
+		data_t data[comps*size0*size1];
+	
+		AlloArrayHeader hdr;
+		hdr.type = AlloFloat64Ty;
+		hdr.components = comps;
+		hdr.dimcount = dims;
+		hdr.dim[0] = size0;
+		hdr.dim[1] = size1;
+		hdr.stride[0] = stride0;
+		hdr.stride[1] = stride1;
+		
+		AlloArray lat;
+		lat.data.ptr = (char *)&data;
+		
+		allo_array_setheader(&lat, &hdr);
+
+		assert(allo_array_elements(&lat) == size0*size1);
+		assert(allo_array_size(&lat) == sizeof(data));
+	}
 	
 	{
-		// Basic Array usage
+		AlloArrayHeader hdr;
+		hdr.type = AlloSInt8Ty;
+		hdr.components = 1;
+		hdr.dim[0] = 7;
+		hdr.dim[1] = 7;
+
+		// 2D test
+		hdr.dimcount = 2;
 		
-		// a 1D array of float-pairs (e.g. interleaved audio buffer)
-		Array buf(2, AlloFloat32Ty, 64);
+		allo_array_setstride(&hdr, 1);
+		assert(hdr.stride[0] == 1);
+		assert(hdr.stride[1] == 7);
+
+		allo_array_setstride(&hdr, 4);
+		assert(hdr.stride[1] == 8);
 		
+		allo_array_setstride(&hdr, 8);
+		assert(hdr.stride[1] == 8);
 		
-		// a 2D array of char[4] data (e.g. ARGB image matrix with color values as 0-255) 
-		Array img(4, AlloUInt8Ty, 720, 480);
-		
-		// a 3D array of float triplets (e.g. vector field)
-		Array field(3, Array::type<float>(), 16, 16, 16);
-		
-		// check the type of 
-		assert(buf.isType<float>());
-		assert(buf.isType(AlloFloat32Ty));
-		
-		
+		// 3D test
+		hdr.dimcount = 3;
+		allo_array_setstride(&hdr, 4);
+		assert(hdr.stride[2] == 8*7);
 	}
+
+	{	// Basic Array usage
+		
+		// TODO: move this to an example
+//		// a 1D array of float-pairs (e.g. interleaved audio buffer)
+//		Array buf(2, AlloFloat32Ty, 64);
+//
+//		// a 2D array of char[4] data (e.g. ARGB image matrix with color values as 0-255) 
+//		Array img(4, AlloUInt8Ty, 720, 480);
+//		
+//		// a 3D array of float triplets (e.g. vector field)
+//		Array field(3, Array::type<float>(), 16, 16, 16);
+
+		{	Array a;
+			assert(a.size() == 0);
+			assert(!a.hasData());
+			assert(a.type() == AlloVoidTy);
+			assert(a.isType(AlloVoidTy));
+			assert(a.isFormat(a));
+		}
+
+		// Run tests on Arrays with various sizes and dimensions
+		const int Nc= 3;
+		const int Ns[] = {1, 4, 5, 6, 7, 64, 128, 129};
+
+		for(unsigned IN=0; IN<sizeof(Ns)/sizeof(*Ns); ++IN){
+
+			int N = Ns[IN]; // number of cells along each dimension
+
+			{	// Constructors
+				{	Array a(Nc, AlloFloat32Ty, N);
+					assert(a.hasData());
+					assert(a.type() == AlloFloat32Ty);
+					assert(a.components() == Nc);
+					assert(a.dimcount() == 1);
+				}
+				{	Array a(Nc, AlloFloat32Ty, N,N);
+					assert(a.hasData());
+					assert(a.type() == AlloFloat32Ty);
+					assert(a.components() == Nc);
+					assert(a.dimcount() == 2);
+				}
+				{	Array a(Nc, AlloFloat32Ty, N,N,N);
+					assert(a.hasData());
+					assert(a.type() == AlloFloat32Ty);
+					assert(a.components() == Nc);
+					assert(a.dimcount() == 3);
+				}
+			}
+
+			{	// Memory allocation
+				Array a;
+				
+				a.formatAligned(Nc, AlloFloat32Ty, N, 1);
+				assert((int)a.size() == Nc*N*4);
+				assert(a.hasData());
+				assert(a.isType(AlloFloat32Ty));
+				assert(a.isType<float>());
+				
+				a.formatAligned(Nc, AlloSInt8Ty, N, N, 1);
+				assert((int)a.size() == Nc*N*N*1);
+				assert(a.hasData());
+				assert(a.isType(AlloSInt8Ty));
+				assert(a.isType<int8_t>());
+
+				a.formatAligned(Nc, AlloSInt16Ty, N, N, N, 1);
+				assert((int)a.size() == Nc*N*N*N*2);
+				assert(a.hasData());
+				assert(a.isType(AlloSInt16Ty));
+				assert(a.isType<int16_t>());
+			}
+
+			{	// 1-D element access
+				Array a(Nc, AlloSInt8Ty, N);
+				for(int i=0,t=0; i<N; ++i){
+				
+					int8_t x[Nc] = {i, i+1, i+2};
+					int8_t y[Nc] = {-1,-1,-1};
+					a.write(x, i);
+					a.read(y, i);
+					for(int c=0; c<Nc; ++c) assert(y[c] == x[c]);
+				
+					for(int c=0; c<Nc; ++c){
+						a.at<int8_t>(c,i) = t+1;
+						assert(a.at<int8_t>(c,i) == int8_t(t+1));
+						++t;
+					}
+				}
+			}
+
+			{	// 2-D element access
+				Array a(Nc, AlloSInt8Ty, N,N);
+				for(int j=0,t=0; j<N; ++j){
+				for(int i=0; i<N; ++i){
+				
+					int8_t x[Nc] = {j, j+1, j+2};
+					int8_t y[Nc] = {-1,-1,-1};
+					a.write(x, i,j);
+					a.read(y, i,j);
+					for(int c=0; c<Nc; ++c) assert(y[c] == x[c]);
+				
+					for(int c=0; c<Nc; ++c){
+						a.at<int8_t>(c,i,j) = t+1;
+						assert(a.at<int8_t>(c,i,j) == int8_t(t+1));
+						++t;
+					}
+				}}
+			}
+
+			{	// 3-D element access
+				Array a(Nc, AlloSInt8Ty, N,N,N);
+				for(int k=0,t=0; k<N; ++k){
+				for(int j=0; j<N; ++j){
+				for(int i=0; i<N; ++i){
+				
+					int8_t x[Nc] = {k, k+1, k+2};
+					int8_t y[Nc] = {-1,-1,-1};
+					a.write(x, i,j,k);
+					a.read(y, i,j,k);
+					for(int c=0; c<Nc; ++c) assert(y[c] == x[c]);
+				
+					for(int c=0; c<Nc; ++c){
+						a.at<int8_t>(c,i,j,k) = t+1;
+						assert(a.at<int8_t>(c,i,j,k) == int8_t(t+1));
+						++t;
+					}
+				}}}
+			}
+
+		}	// end size loop		
+	}
+
 
 	{
 		Buffer<int> a(0,2);
@@ -112,72 +279,6 @@ int utTypes(){
 		assert(a.atRel(3) == 2);
 	}
 
-
-	// test the basic C functionality
-	{
-		const int comps = 1;	//
-		const int dims = 2;		// no. dimensions
-		const int size0 = 2;	// no. elements in dimension 1
-		const int size1 = 64;	// no. elements in dimension 2
-		const int stride0 = comps * sizeof(data_t);	// byte offset at dim 0
-		const int stride1 = stride0*size0;
-
-		data_t data[comps*size0*size1];
-	
-		AlloArrayHeader hdr;
-		hdr.type = AlloFloat64Ty;
-		hdr.components = comps;
-		hdr.dimcount = dims;
-		hdr.dim[0] = size0;
-		hdr.dim[1] = size1;
-		hdr.stride[0] = stride0;
-		hdr.stride[1] = stride1;
-		
-		AlloArray lat;
-		lat.data.ptr = (char *)&data;
-		
-		allo_array_setheader(&lat, &hdr);
-
-		assert(allo_array_elements(&lat) == size0*size1);
-		assert(allo_array_size(&lat) == sizeof(data));
-	}
-	
-	{
-		AlloArrayHeader hdr;
-		hdr.type = AlloSInt8Ty;
-		hdr.components = 1;
-		hdr.dim[0] = 7;
-		hdr.dim[1] = 7;
-
-		// 2D test
-		hdr.dimcount = 2;
-		
-		allo_array_setstride(&hdr, 1);
-		assert(hdr.stride[0] == 1);
-		assert(hdr.stride[1] == 7);
-
-		allo_array_setstride(&hdr, 4);
-		assert(hdr.stride[1] == 8);
-		
-		allo_array_setstride(&hdr, 8);
-		assert(hdr.stride[1] == 8);
-		
-		// 3D test
-		hdr.dimcount = 3;
-		allo_array_setstride(&hdr, 4);
-		assert(hdr.stride[2] == 8*7);
-	}
-	
-	// not implemented...
-//	{
-//		al::Array lat;
-//		lat.setpacked3d<double>(2, 8, 8, 8);
-//		lat.data.ptr = (char *)calloc(1, lat.size());
-//
-//		double vals[2];
-//		lat.interp(vals, 1, 2, 3);
-//		printf("value at 1, 2, 3: %f %f\n", vals[0], vals[1]);
-//	}
 	return 0;
 }
 
