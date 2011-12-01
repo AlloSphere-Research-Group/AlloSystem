6659c64b64d7373f0d85f79d76f9a4a3cc5ff97e
allolab <allolab@bd62deb4-2030-0410-8420-fdd75d726ec8>
2011-12-01 04:35:39 +0000

    resource manager linux fix

--- include/alloutil/al_ResourceManager.hpp
+++ include/alloutil/al_ResourceManager.hpp
@@ -3,15 +3,15 @@
 
 /*	Allocore --
 	Multimedia / virtual environment application class library
-	
+
 	Copyright (C) 2009. AlloSphere Research Group, Media Arts & Technology, UCSB.
-	Copyright (C) 2006-2008. The Regents of the University of California (REGENTS). 
+	Copyright (C) 2006-2008. The Regents of the University of California (REGENTS).
 	All Rights Reserved.
 
 	Permission to use, copy, modify, distribute, and distribute modified versions
 	of this software and its documentation without fee and without a signed
 	licensing agreement, is hereby granted, provided that the above copyright
-	notice, the list of contributors, this paragraph and the following two paragraphs 
+	notice, the list of contributors, this paragraph and the following two paragraphs
 	appear in all copies, modifications, and distributions.
 
 	IN NO EVENT SHALL REGENTS BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
@@ -31,8 +31,8 @@
 
 	File author(s):
 	Graham Wakefield, 2010, grrrwaaa@gmail.com
-	
-	
+
+
 */
 
 #include "allocore/io/al_File.hpp"
@@ -53,34 +53,34 @@ public:
 		std::string path;
 		std::string data;
 		al_sec modified;
-		
+
 		FileInfo() : modified(0) {}
 		FileInfo(const FileInfo& cpy) : path(cpy.path), modified(cpy.modified) {}
 	};
-	
+
 	///! returns NULL if the file cannot be found
 	const char * find(std::string filename);
-	
+
 	///! adds a file to the file map (assuming it can be found
 	/// with immediate==true also calls read() directly
 	bool add(std::string filename, bool immediate=true);
-	
+
 	/// get the FileInfo for a given fileanme
 	/// (calls addFile(fileanme) if the file isn't currently in the file map)
 	FileInfo& get(std::string filename);
 	FileInfo& operator[](std::string filename) { return get(filename); }
-	
+
 	/// get the data for a given filename
 	/// (calls addFile(fileanme) if the file isn't currently in the file map)
 	std::string data(std::string filename);
-	
+
 	///! updates the modified/changed flags of all files in the filemap:
 	void poll();
-	
-	
+
+
 	///! list of paths to search for files:
 	SearchPaths paths;
-	
+
 protected:
 	bool read(std::string filename);
 
@@ -93,22 +93,22 @@ protected:
 /// Example use-case with shaders:
 class ManagedShader : public ShaderProgram {
 public:
-	
-	ManagedShader(ResourceManager& rm, std::string vert="", std::string frag="") 
-	:	ShaderProgram(), 
-		rm(rm), 
+
+	ManagedShader(ResourceManager& rm, std::string vert="", std::string frag="")
+	:	ShaderProgram(),
+		rm(rm),
 		relink(0)
 	{
 		vertex(vert);
 		fragment(frag);
 	}
-	
+
 	virtual ~ManagedShader() {}
-	
+
 	ManagedShader& vertex(std::string name) { return add(name, Shader::VERTEX); }
 	ManagedShader& fragment(std::string name) { return add(name, Shader::FRAGMENT); }
 	ManagedShader& geometry(std::string name) { return add(name, Shader::GEOMETRY); }
-	
+
 	ManagedShader& add(std::string name, Shader::Type type) {
 		if (name != "") {
 			shaders.push_back(ShaderFile());
@@ -120,7 +120,7 @@ public:
 		}
 		return *this;
 	}
-	
+
 	// overrides ShaderProgram::begin():
 	void begin() {
 		// check all shaders:
@@ -131,7 +131,7 @@ public:
 				// mark as read:
 				sf.modified = info.modified;
 				// remove existing:
-				if (linked()) detach(sf.shader);
+				if (sf.shader.compiled()) detach(sf.shader);
 				// recompile & link:
 				sf.shader.source(info.data, sf.type).compile();
 				attach(sf.shader);
@@ -142,30 +142,40 @@ public:
 		}
 		if (relink) {
 			link();
-			printLog();	
-			relink = false;	
+			printLog();
+			relink = false;
 		}
 		ShaderProgram::begin();
 	}
-	
+
 protected:
+
+	virtual void onCreate() {
+		ShaderProgram::onCreate();
+
+		for (unsigned i=0; i<shaders.size(); i++) {
+			ShaderFile& sf = shaders[i];
+			sf.modified = 0;    // trigger reload
+		}
+
+	}
 
 	virtual void onDestroy() {
 		ShaderProgram::onDestroy();
-	
+
 		for (unsigned i=0; i<shaders.size(); i++) {
 			ShaderFile& sf = shaders[i];
 			sf.shader.invalidate();
 		}
 	}
-	
+
 	struct ShaderFile {
 		std::string name;
 		Shader shader;
 		Shader::Type type;
 		al_sec modified;
 	};
-	
+
 	ResourceManager& rm;
 	std::vector<ShaderFile> shaders;
 	bool relink;
