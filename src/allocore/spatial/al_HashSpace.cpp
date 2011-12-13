1863618291a22aa7a0ba873bf0cde73338a7911d
wakefield <wakefield@bd62deb4-2030-0410-8420-fdd75d726ec8>
2011-12-13 00:18:36 +0000

    hashspace update

--- src/allocore/spatial/al_HashSpace.cpp
+++ src/allocore/spatial/al_HashSpace.cpp
@@ -23,6 +23,9 @@ HashSpace :: HashSpace(uint32_t resolution, uint32_t numObjects)
 	
 	mVoxels.resize(mDim3);
 	mObjects.resize(numObjects);
+	for (int i=0; i<mObjects.size(); i++) {
+		mObjects[i].id = i;
+	}
 	
 	// each voxel has a particular distance from the origin.
 	// this can be used to create a reverse lookup table
