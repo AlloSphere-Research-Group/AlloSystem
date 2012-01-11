7763511765a77a1c8723be99a9bb1e4a6500e290
wakefield <wakefield@bd62deb4-2030-0410-8420-fdd75d726ec8>
2012-01-11 21:44:22 +0000

    universalized libfreenect

--- include/alloutil/al_Lua.hpp
+++ include/alloutil/al_Lua.hpp
@@ -80,6 +80,10 @@ public:
 		return res;
 	}
 	
+	int dostring(const std::string& code, int nargs=0) {
+		return luaL_loadstring(L, code.c_str()) || pcall(L, nargs);
+	}
+	
 	int dofile(const std::string& path, int nargs=0) {
 		return luaL_loadfile(L, path.c_str()) || pcall(L, nargs);
 	}
