9515953f441d10058729badf13b2807c443e47a1
l.putnam <l.putnam@bd62deb4-2030-0410-8420-fdd75d726ec8>
2012-02-21 18:30:02 +0000

    remove GL types from header

--- include/allocore/graphics/al_FBO.hpp
+++ include/allocore/graphics/al_FBO.hpp
@@ -40,6 +40,9 @@ namespace al{
 
 
 /// Render buffer object
+
+/// Render buffer objects contain a single image of a renderable internal
+/// format, namely depth and stencil. They are used for offscreen rendering.
 class RBO : public GPUObject{
 public:
 
@@ -51,19 +54,25 @@ public:
 	///
 	RBO(PixelFormat format=DEPTH);
 
-	static void bind(GLuint id);
-	static void storage(GLenum internalformat, GLsizei width, GLsizei height);
-
-	GLenum format() const { return mFormat; }
+	/// Get pixel format
+	PixelFormat format() const { return mFormat; }
 
 	void bind(){ validate(); bind(id()); }
 	void begin(){ bind(); }
 	static void end() { bind(0); }
 
-	void storage(GLsizei width, GLsizei height);
+	/// Set dimensions, in pixels
+	
+	/// \returns whether the resize was successful
+	///
+	bool resize(unsigned width, unsigned height);
+
+	static unsigned maxSize();
+	static void bind(unsigned id);
+	static bool resize(PixelFormat format, unsigned width, unsigned height);
 
 protected:
-	GLenum mFormat;
+	PixelFormat mFormat;
 	
 	virtual void onCreate();
 	virtual void onDestroy();
@@ -73,22 +82,30 @@ protected:
 
 /// Frame buffer object
 
-/// All attachments must have the same dimensions. This is a standard
-/// requirement of an FBO, not an implementation imposed limitation.
-/// You can only render to RGB, RGBA, and depth textures.
+/// A frame buffer object is an application-created frame buffer that is
+/// non-displayable. A single FBO can have multiple rendering destinations
+/// (attachments) for color, depth, and stencil information. If the attachment
+/// is a texture, then the FBO does "render-to-texture". If the attachment is
+/// a render buffer object, then the FBO does "offscreen rendering".
+/// A single FBO can have multiple color attachments, but only one depth and
+/// one stencil attachment. Switching the attachments (attach/detach calls) is
+/// much faster than using multiple FBOs.
+/// All attachments must have the same dimensions and all color attachments
+/// must have the same pixel format. These are standard requirements of an FBO,
+/// not an implementation-imposed limitation.
 class FBO : public GPUObject {
 public:
 
 	/// Attachment type
 	enum Attachment{
 		COLOR_ATTACHMENT0			= GL_COLOR_ATTACHMENT0_EXT,
+		COLOR_ATTACHMENT1			= GL_COLOR_ATTACHMENT1_EXT,
+		COLOR_ATTACHMENT2			= GL_COLOR_ATTACHMENT2_EXT,
+		COLOR_ATTACHMENT3			= GL_COLOR_ATTACHMENT3_EXT,
 		DEPTH_ATTACHMENT			= GL_DEPTH_ATTACHMENT_EXT,
 		STENCIL_ATTACHMENT			= GL_STENCIL_ATTACHMENT_EXT
 	};
 
-	static void bind(GLuint id);
-	static void renderBuffer(const RBO& rbo);
-	static void texture2D(GLuint texID, Attachment attach=COLOR_ATTACHMENT0, int level=0);
 	
 	FBO& attach(const RBO& rbo);
 	
@@ -97,7 +114,7 @@ public:
 	/// @param[in] texID	texture ID
 	/// @param[in] attach	Attachment type
 	/// @param[in] level	mipmap level of texture
-	FBO& attachTexture2D(GLuint texID, Attachment attach=COLOR_ATTACHMENT0, int level=0);
+	FBO& attachTexture2D(unsigned texID, Attachment attach=COLOR_ATTACHMENT0, int level=0);
 
 	/// Detach texture at a specific attachament point
 	FBO& detachTexture2D(Attachment attach, int level=0);
@@ -111,6 +128,11 @@ public:
 	GLenum status();
 	const char * statusString();
 	const char * statusString(GLenum stat);
+
+
+	static void bind(unsigned rboID);
+	static void renderBuffer(const RBO& rbo);
+	static void texture2D(unsigned texID, Attachment attach=COLOR_ATTACHMENT0, int level=0);
 
 protected:
 
--- src/allocore/graphics/al_FBO.cpp
+++ src/allocore/graphics/al_FBO.cpp
@@ -4,51 +4,46 @@ namespace al{
 
 RBO::RBO(PixelFormat format): mFormat(format){}
 
-void RBO::bind(GLuint id){ glBindRenderbufferEXT(GL_RENDERBUFFER_EXT, id); }
-
-void RBO::storage(GLenum internalformat, GLsizei width, GLsizei height){
-	glRenderbufferStorageEXT(GL_RENDERBUFFER_EXT, internalformat, width, height);
-}
-
 void RBO::onCreate(){ GLuint i; glGenRenderbuffersEXT(1,&i); mID=i; }
-void RBO::onDestroy(){ GLuint i=id(); glDeleteRenderbuffersEXT(1, &i); }
+void RBO::onDestroy(){ GLuint i=id(); glDeleteRenderbuffersEXT(1,&i); }
 
-void RBO::storage(GLsizei width, GLsizei height){
-	begin(); storage(format(), width, height);
+bool RBO::resize(unsigned w, unsigned h){
+	begin(); return resize(format(), w, h);
+}
+
+// static functions
+unsigned RBO::maxSize(){
+	int s;
+	glGetIntegerv(GL_MAX_RENDERBUFFER_SIZE_EXT, &s);
+	return s;
+}
+
+void RBO::bind(unsigned id){ glBindRenderbufferEXT(GL_RENDERBUFFER_EXT, id); }
+
+bool RBO::resize(PixelFormat format, unsigned w, unsigned h){
+	unsigned mx = maxSize();
+	if(w > mx || h > mx) return false;
+	glRenderbufferStorageEXT(GL_RENDERBUFFER_EXT, format, w, h);
+	return true;
 }
 
 
-//	static void renderBufferDepth(GLuint rbID, int attach=0, int level=0){
-//		glFramebufferRenderbufferEXT(GL_FRAMEBUFFER_EXT, GL_DEPTH_ATTACHMENT_EXT, GL_RENDERBUFFER_EXT, rbID);
-//	}
 
 
-FBO& FBO::attachTexture2D(GLuint texID, Attachment att, int level){
-	begin(); texture2D(texID, att, level); end(); return *this;
-}
+void FBO::onCreate(){ GLuint i; glGenFramebuffersEXT(1,&i); mID=i; }
+void FBO::onDestroy(){ GLuint i=id(); glDeleteFramebuffersEXT(1,&i); }
 
 FBO& FBO::attach(const RBO& rbo){
 	begin(); renderBuffer(rbo); end(); return *this;
 }
 
-void FBO::bind(GLuint id){ glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, id); }
+FBO& FBO::attachTexture2D(unsigned texID, Attachment att, int level){
+	begin(); texture2D(texID, att, level); end(); return *this;
+}
 
 FBO& FBO::detachTexture2D(Attachment att, int level){
 	attachTexture2D(0,att,level);
 	return *this;		
-}
-
-void FBO::onCreate(){ GLuint i; glGenFramebuffersEXT(1,&i); mID=i; }
-void FBO::onDestroy(){ GLuint i=id(); glDeleteFramebuffersEXT(1, &i); }
-
-
-void FBO::renderBuffer(const RBO& rbo){
-	GLenum a=0;
-	switch(rbo.format()){
-		case RBO::DEPTH: a=GL_DEPTH_ATTACHMENT_EXT; break;
-		default:;
-	}
-	glFramebufferRenderbufferEXT(GL_FRAMEBUFFER_EXT, a, GL_RENDERBUFFER_EXT, rbo.id());
 }
 
 GLenum FBO::status(){
@@ -69,6 +64,18 @@ const char * FBO::statusString(GLenum stat){
 	CS(GL_FRAMEBUFFER_INCOMPLETE_READ_BUFFER_EXT)			CS(GL_FRAMEBUFFER_UNSUPPORTED_EXT)
 	default: return "GL_FRAMEBUFFER_UNKNOWN";
 	};
+}
+
+// static functions
+void FBO::bind(unsigned rboID){ glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, rboID); }
+
+void FBO::renderBuffer(const RBO& rbo){
+	GLenum a=0;
+	switch(rbo.format()){
+		case RBO::DEPTH: a=GL_DEPTH_ATTACHMENT_EXT; break;
+		default:;
+	}
+	glFramebufferRenderbufferEXT(GL_FRAMEBUFFER_EXT, a, GL_RENDERBUFFER_EXT, rbo.id());
 }
 
 void FBO::texture2D(GLuint texID, Attachment att, int level){
