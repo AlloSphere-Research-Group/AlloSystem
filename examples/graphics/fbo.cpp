82ad42643fd53085ad66362a36f8625148998f88
wakefield <wakefield@bd62deb4-2030-0410-8420-fdd75d726ec8>
2012-05-16 18:44:09 +0000

    fbo example with comments

--- examples/graphics/fbo.cpp
+++ examples/graphics/fbo.cpp
@@ -12,19 +12,26 @@ Graham Wakefield, 2012
 
 using namespace al;
 
+int w=256, h=256;
+
 Graphics gl;
-Texture fbotex(512, 512);
+Texture fbotex(w, h);
 RBO rbo;
 FBO fbo;
+
+rnd::Random<> rng;
 
 struct MyWindow : Window{
 
 	bool onCreate(){
-		rbo.resize(512, 512);
+		
+		// both depth and color attachees must be valid on the GPU before use:
+		rbo.resize(w, h);
 		fbotex.validate();
 		
 		fbo.attachRBO(rbo, FBO::DEPTH_ATTACHMENT);
 		fbo.attachTexture2D(fbotex.id(), FBO::COLOR_ATTACHMENT0);
+		printf("fbo status %s\n", fbo.statusString());
 
 		return true;
 	}
@@ -32,20 +39,38 @@ struct MyWindow : Window{
 	bool onFrame(){
 		
 		fbo.begin();
-		
-			gl.viewport(0, 0, width(), height());
-			gl.clearColor(1, 1, 0, 0);
+			
+			// capture green-world to texture:
+			gl.viewport(0, 0, fbotex.width(), fbotex.height());
+			gl.clearColor(0, 0.5, 0, 0);
 			gl.clear(Graphics::COLOR_BUFFER_BIT | Graphics::DEPTH_BUFFER_BIT);
+			
+			gl.projection(Matrix4d::perspective(45, 1, 0.1, 10));
+			gl.modelView(Matrix4d::identity());
+			
+			gl.begin(gl.TRIANGLES);
+			gl.color(1, 1, 0);
+				gl.vertex(rng.uniformS()*0.5, rng.uniformS()*0.5, -5);
+				gl.vertex(rng.uniformS()*0.5, rng.uniformS()*0.5, -5);
+				gl.vertex(rng.uniformS()*0.5, rng.uniformS()*0.5, -5);
+			gl.end();
 		
 		fbo.end();
 		
+		// generation of Mipmaps must be done manually for FBO-bound textures:
+		// (to see why, comment this code out and then make the window very small).
+		fbotex.bind();
+		glGenerateMipmapEXT(GL_TEXTURE_2D);
+		fbotex.unbind();
+		
+		// show in blue-world:
 		gl.viewport(0, 0, width(), height());
-		gl.clearColor(0, 1, 1, 0);
+		gl.clearColor(0, 0, 0.5, 0);
 		gl.clear(Graphics::COLOR_BUFFER_BIT | Graphics::DEPTH_BUFFER_BIT);
 		
 		gl.projection(Matrix4d::ortho(0, 1, 1, 0, -1, 1));
 		gl.modelView(Matrix4d::identity());
-		fbotex.quad(gl);
+		fbotex.quad(gl, 0.8, 0.8, 0.1, 0.1);
 
 		return true;
 	}
