6db44999cf2cc3affdf7887183971bd301519aa6
l.putnam <l.putnam@bd62deb4-2030-0410-8420-fdd75d726ec8>
2012-05-25 18:40:14 +0000

    license text update

--- include/allocore/math/al_Functions.hpp
+++ include/allocore/math/al_Functions.hpp
@@ -5,25 +5,34 @@
 	Multimedia / virtual environment application class library
 
 	Copyright (C) 2009. AlloSphere Research Group, Media Arts & Technology, UCSB.
-	Copyright (C) 2006-2008. The Regents of the University of California (REGENTS). 
-	All Rights Reserved.
+	Copyright (C) 2012. The Regents of the University of California.
+	All rights reserved.
 
-	Permission to use, copy, modify, distribute, and distribute modified versions
-	of this software and its documentation without fee and without a signed
-	licensing agreement, is hereby granted, provided that the above copyright
-	notice, the list of contributors, this paragraph and the following two paragraphs 
-	appear in all copies, modifications, and distributions.
+	Redistribution and use in source and binary forms, with or without 
+	modification, are permitted provided that the following conditions are met:
 
-	IN NO EVENT SHALL REGENTS BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
-	SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS, ARISING
-	OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF REGENTS HAS
-	BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+		Redistributions of source code must retain the above copyright notice, 
+		this list of conditions and the following disclaimer.
 
-	REGENTS SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
-	THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-	PURPOSE. THE SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF ANY, PROVIDED
-	HEREUNDER IS PROVIDED "AS IS". REGENTS HAS  NO OBLIGATION TO PROVIDE
-	MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+		Redistributions in binary form must reproduce the above copyright 
+		notice, this list of conditions and the following disclaimer in the 
+		documentation and/or other materials provided with the distribution.
+
+		Neither the name of the University of California nor the names of its 
+		contributors may be used to endorse or promote products derived from 
+		this software without specific prior written permission.
+
+	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+	AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+	IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+	ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE 
+	LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+	CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+	SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+	INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+	CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+	ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+	POSSIBILITY OF SUCH DAMAGE.
 
 
 	File description:
