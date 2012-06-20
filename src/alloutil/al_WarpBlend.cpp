79fba913843177bfb84e8a564cb3332bef6f2eb8
wakefield <wakefield@bd62deb4-2030-0410-8420-fdd75d726ec8>
2012-06-20 00:49:59 +0000

    latest

--- src/alloutil/al_WarpBlend.cpp
+++ src/alloutil/al_WarpBlend.cpp
@@ -43,8 +43,8 @@ static const char * geomFS3D = AL_STRINGIFY(
 		vec3 v = texture2D(pixelMap, texcoord0).rgb;
 		float a = texture2D(alphaMap, vec2(texcoord0.x, 1.-texcoord0.y)).r;
 		v = normalize(v);
-		v = mod(v * 8., 1.);
-		gl_FragColor = vec4(v, a);
+		v = mod(v * 8., 1.) * a;
+		gl_FragColor = vec4(v, 1.);
 	}
 );
 
