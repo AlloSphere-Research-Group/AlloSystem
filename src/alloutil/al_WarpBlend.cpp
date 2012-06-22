84ad6d5c8680473a11a38f6ed085fe70f8d2a337
wakefield <wakefield@bd62deb4-2030-0410-8420-fdd75d726ec8>
2012-06-22 21:12:36 +0000

    updated warper

--- src/alloutil/al_WarpBlend.cpp
+++ src/alloutil/al_WarpBlend.cpp
@@ -222,7 +222,7 @@ static const char * demoFS = AL_STRINGIFY(
 		// should reduce to zero as the nv becomes close to (0, 1, 0)
 		// take the vector of nv in the XZ plane
 		// and rotate it 90' around Y:
-		vec3 nvx = cross(vn, vec3(0, 1, 0)); //vec3(nv.z, 0., nv.x);
+		vec3 nvx = cross(nv, vec3(0, 1, 0)); //vec3(nv.z, 0., nv.x);
 		vec3 eye = -nvx * eyesep * 0.005;
 		
 		// ray direction (world space)
