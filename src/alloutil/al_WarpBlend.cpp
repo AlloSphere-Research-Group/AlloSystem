88fa383ee4248c8ffcaef700caa7e6993eaa62c7
wakefield <wakefield@bd62deb4-2030-0410-8420-fdd75d726ec8>
2012-06-22 22:49:18 +0000

    warp blend updates

--- src/alloutil/al_WarpBlend.cpp
+++ src/alloutil/al_WarpBlend.cpp
@@ -207,6 +207,13 @@ static const char * demoFS = AL_STRINGIFY(
 		return res;
 	}
 	
+	// pn is the normal to the plane
+	vec3 projection_on_plane(vec3 v, vec3 pn) {
+		vec3 c = cross(v, pn);
+		vec3 d = cross(c, pn);
+		return d;
+	}
+	
 	void main(){
 	
 		vec3 light1 = pos + vec3(3, 2., 1);
@@ -227,10 +234,10 @@ static const char * demoFS = AL_STRINGIFY(
 		// take the vector of nv in the XZ plane
 		// and rotate it 90' around Y:
 		vec3 up = vec3(0, 1, 0);
-		vec3 nvx = cross(nv, up);
-		nvx.y = 0.;
-		nvx = normalize(nvx); //vec3(nv.z, 0., nv.x);
-		float amount = 1.-abs(dot(nv, up));
+		vec3 nvx = projection_on_plane(nv, up);
+		//nvx.y = 0.;
+		//nvx = normalize(nvx); //vec3(nv.z, 0., nv.x);
+		//float amount = 1.-abs(dot(nv, up));
 		//vec3 nvx = vec3(0, nv.r, nv.b);
 		vec3 eye = nvx * amount * eyesep * 0.005;
 		
