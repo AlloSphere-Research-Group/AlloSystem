69d2b7aea5d89f1ac5b7812eaff59286d932da1a
wakefield <wakefield@bd62deb4-2030-0410-8420-fdd75d726ec8>
2012-06-22 22:58:43 +0000

    warp blend updates

--- src/alloutil/al_WarpBlend.cpp
+++ src/alloutil/al_WarpBlend.cpp
@@ -237,7 +237,7 @@ static const char * demoFS = AL_STRINGIFY(
 		//nvx = normalize(nvx); //vec3(nv.z, 0., nv.x);
 		//float amount = 1.-abs(dot(nv, up));
 		//vec3 nvx = vec3(0, nv.r, nv.b);
-		vec3 eye = nvx.zxy * eyesep * 0.005;
+		vec3 eye = nvx.zxy * eyesep * 0.01;
 		
 		// ray direction (world space)
 		//vec3 nev = normalize(v - pos);
