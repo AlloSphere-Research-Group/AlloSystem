2e735adfd727e5102fb18c75b93c6f13547ff727
Lance Putnam <putnam.lance@gmail.com>
2012-06-27 13:45:10 -0700

    update dependency install for apt-get

--- allocore/install_dependencies.sh
+++ allocore/install_dependencies.sh
@@ -2,19 +2,23 @@
 
 if [ `which apt-get` ]; then
 	echo "Found apt-get"
+	sudo apt-get update
+	sudo apt-get install libapr1-dev libaprutil1-dev
+	sudo apt-get install portaudio19-dev libsndfile1-dev
+	sudo apt-get install libglew-dev freeglut3-dev libfreeimage-dev libfreetype6-dev
 
 elif [ `which port` ]; then
 	echo "Found MacPorts"
-#	sudo port selfupdate
-#	port install assimp freeimage 
-#	port install portaudio libsndfile
+	sudo port selfupdate
+	port install assimp freeimage 
+	port install portaudio libsndfile
 
 elif [ `which brew` ]; then
 	echo "Found Homebrew"
-  cd /usr/local/Library/Formula
-  curl --insecure -O "https://raw.github.com/OpenKinect/libfreenect/master/platform/osx/homebrew/libfreenect.rb"
-  curl --insecure -O "https://raw.github.com/OpenKinect/libfreenect/master/platform/osx/homebrew/libusb-freenect.rb"
-  brew install portaudio libsndfile libfreenect assimp freeimage
+	cd /usr/local/Library/Formula
+	curl --insecure -O "https://raw.github.com/OpenKinect/libfreenect/master/platform/osx/homebrew/libfreenect.rb"
+	curl --insecure -O "https://raw.github.com/OpenKinect/libfreenect/master/platform/osx/homebrew/libusb-freenect.rb"
+	brew install portaudio libsndfile libfreenect assimp freeimage
 
 else
 	echo "Error: No suitable package manager found."
