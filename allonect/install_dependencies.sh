478bb58151d5fa845bef49beac453a787940f4f0
Lance Putnam <putnam.lance@gmail.com>
2012-06-28 12:48:02 -0700

    update dependency install scripts

--- allonect/install_dependencies.sh
+++ allonect/install_dependencies.sh
@@ -1,28 +1,23 @@
 #!/bin/bash
-# AlloCore dependencies install script
 
 if [ `which apt-get` ]; then
 	echo "Found apt-get"
 	sudo apt-get update
-	sudo apt-get install libapr1-dev libaprutil1-dev
-	sudo apt-get install portaudio19-dev libsndfile1-dev
-	sudo apt-get install libglew-dev freeglut3-dev libassimp-dev libfreeimage-dev libfreetype6-dev
+#	sudo apt-get install freenect
 
 elif [ `which port` ]; then
 	echo "Found MacPorts"
 	sudo port selfupdate
-	port install assimp freeimage 
-	port install portaudio libsndfile
+#	port install freenect
 
 elif [ `which brew` ]; then
 	echo "Found Homebrew"
 	cd /usr/local/Library/Formula
 	curl --insecure -O "https://raw.github.com/OpenKinect/libfreenect/master/platform/osx/homebrew/libfreenect.rb"
 	curl --insecure -O "https://raw.github.com/OpenKinect/libfreenect/master/platform/osx/homebrew/libusb-freenect.rb"
-	brew install portaudio libsndfile libfreenect assimp freeimage
+	brew install libfreenect
 
 else
 	echo "Error: No suitable package manager found."
 	echo "Error: Install apt-get, MacPorts, or Homebrew and try again."
 fi
-
---
+++ alloutil/install_dependencies.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# AlloUtil dependencies install script
+
+if [ `which apt-get` ]; then
+	echo "Found apt-get"
+	sudo apt-get update
+	sudo apt-get install liblua5.1-0-dev
+
+elif [ `which port` ]; then
+	echo "Found MacPorts"
+	sudo port selfupdate
+#	port install lua
+
+elif [ `which brew` ]; then
+	echo "Found Homebrew"
+#	brew install lua
+
+else
+	echo "Error: No suitable package manager found."
+	echo "Error: Install apt-get, MacPorts, or Homebrew and try again."
+fi
+
