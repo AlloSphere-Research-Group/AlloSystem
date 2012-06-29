63ae31d99d96223623d4a6d96e3e0c694078abe7
Lance Putnam <putnam.lance@gmail.com>
2012-06-29 15:40:18 -0700

    include fix

--- alloGLV/examples/detachableGUI.cpp
+++ alloGLV/examples/detachableGUI.cpp
@@ -9,7 +9,7 @@ Lance Putnam, 2011, putnam.lance@gmail.com
 */
 
 #include "allocore/al_Allocore.hpp"
-#include "alloutil/al_ControlGLV.hpp"
+#include "alloGLV/al_ControlGLV.hpp"
 #include "GLV/glv.h"
 
 using namespace al;
