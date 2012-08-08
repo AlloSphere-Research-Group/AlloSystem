6969ea128a6af85192f9533aa3f0bed1e5205530
Lance Putnam <putnam.lance@gmail.com>
2012-08-08 10:42:54 +0200

    remove freenect; updates to MacPorts installs

--- allocore/install_dependencies.sh
+++ allocore/install_dependencies.sh
@@ -13,15 +13,15 @@ if [ `which apt-get` ]; then
 elif [ `which port` ]; then
 	echo "Found MacPorts"
 	sudo port selfupdate
-	port install assimp freeimage 
 	port install portaudio libsndfile
+	port install glew
+	port install assimp freeimage
 
 elif [ `which brew` ]; then
 	echo "Found Homebrew"
-	cd /usr/local/Library/Formula
-	curl --insecure -O "https://raw.github.com/OpenKinect/libfreenect/master/platform/osx/homebrew/libfreenect.rb"
-	curl --insecure -O "https://raw.github.com/OpenKinect/libfreenect/master/platform/osx/homebrew/libusb-freenect.rb"
-	brew install portaudio libsndfile libfreenect assimp freeimage
+	brew install portaudio libsndfile
+	brew install glew
+	brew install assimp freeimage
 
 else
 	echo "Error: No suitable package manager found."
