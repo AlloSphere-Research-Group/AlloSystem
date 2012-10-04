dc60b2203ac9d8ced77a70604c4ce14914bc9845
Karl Yerkes <karl.yerkes@gmail.com>
2012-10-04 12:55:39 -0700

    fixed to actually install assimp

--- allocore/install_dependencies.sh
+++ allocore/install_dependencies.sh
@@ -18,7 +18,7 @@ elif [ `which port` ]; then
 
 	# Since we only support v.2, we need to use an old Portfile
 	#sudo port install assimp
-	pushd pkg/port/assimp2/; sudo port install; popd
+	pushd pkg/port/assimp2/; sudo port install assimp; popd
 
 	sudo port install freeimage +universal
 	sudo port install freetype +universal
