34e48c67671fb4e4829c5e4cda39756418acafca
Pablo Colapinto <wolftype@gmail.com>
2013-02-27 11:13:33 -0800

    new vsr omni example

---
+++ allovsr/osx/allovsr/allovsr/omni.cpp
@@ -0,0 +1,71 @@
+//
+//  omni.cpp
+//  allovsr
+//
+//  Created by Pablo Colapinto on 2/26/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#include "vsr/vsr.h"
+#include "vsr/vsr_op.h"
+#include "vsr/vsr_draw.h"
+
+#include "alloutil/al_OmniApp.hpp"
+
+using namespace al;
+
+struct MyApp : OmniApp {
+  Mesh mesh;
+  Light light;
+
+  MyApp() {
+
+//    mesh.primitive(Graphics::TRIANGLES);
+//    addSphere(mesh, 1.0, 32, 32);
+//
+//    for (int i = 0; i < mesh.vertices().size(); ++i) {
+//      float f = (float)i / mesh.vertices().size();
+//      mesh.color(Color(HSV(f, 1 - f, 1), 1));
+//    }
+//    mesh.generateNormals();
+//    
+//    light.ambient(Color(0.4, 0.4, 0.4, 1.0));
+//    light.pos(5, 5, 5);
+  
+  }
+
+  virtual ~MyApp() {}
+  
+  virtual void onDraw(Graphics& g) {
+    light();
+    // say how much lighting you want
+    shader().uniform("lighting", 1.0);
+    
+    
+    //g.draw(mesh);
+  }
+
+  virtual void onAnimate(al_sec dt) {
+    //light.pos(nav().pos());
+    //std::cout << dt << std::endl;
+  }
+
+  virtual void onSound(AudioIOData& io) {
+    while (io()) {
+      io.out(0) = rnd::uniformS() * 0.05;
+    }
+  }
+
+  virtual void onMessage(osc::Message& m) {
+    OmniApp::onMessage(m);
+  }
+
+  virtual bool onKeyDown(const Keyboard& k){
+    return true;
+  }
+};
+
+int main(int argc, char * argv[]) {
+  MyApp().start();
+  return 0;
+}
