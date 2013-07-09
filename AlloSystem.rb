e2316f142fa9133bd9e56510ed8dce4b5f549b14
Karl Yerkes <karl.yerkes@gmail.com>
2013-07-09 15:46:09 -0700

    added Homebrew formula

---
+++ AlloSystem.rb
@@ -0,0 +1,21 @@
+# must do 'brew install --HEAD ...'
+#
+require 'formula'
+
+class Allosystem < Formula
+  head 'https://github.com/mantaraya36/AlloSystem.git', :tag => 'homebrew_testing'
+
+  depends_on 'cmake' => :build
+  depends_on 'libsndfile'
+	depends_on 'portaudio'
+	depends_on 'glew'
+	depends_on 'assimp'
+	depends_on 'freeimage'
+	depends_on 'freetype'
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make"
+    system "make install"
+  end
+end
