c7cc19f2af8bbc78cce6dba774fac6b8f5584fc3
Andres Cabrera <andres@lenovo-W530>
2013-08-20 10:00:27 -0700

    Added custom paths setting for windows.

--- cmake_modules/FindAssimp.cmake
+++ cmake_modules/FindAssimp.cmake
@@ -10,48 +10,48 @@ include(LibFindMacros)
 
 # Dependencies
 #libfind_package(LO Magick)
+if((NOT ASSIMP_INCLUDE_DIR) AND (NOT ASSIMP_LIBRARY)) 
+  # Use pkg-config to get hints about paths
+  libfind_pkg_check_modules(ASSIMP_PKGCONF libassimp)
 
-# Use pkg-config to get hints about paths
-libfind_pkg_check_modules(ASSIMP_PKGCONF libassimp)
+  # Include dir
+  find_path(ASSIMP2_INCLUDE_DIR
+    NAMES assimp/assimp.h
+    PATHS ${ASSIMP_PKGCONF_INCLUDE_DIRS} /usr/include /usr/local/include /opt/local/include
+    )
 
-# Include dir
-find_path(ASSIMP2_INCLUDE_DIR
-  NAMES assimp/assimp.h
-  PATHS ${ASSIMP_PKGCONF_INCLUDE_DIRS} /usr/include /usr/local/include /opt/local/include
-)
+  find_path(ASSIMP3_INCLUDE_DIR
+    NAMES assimp/types.h
+    PATHS ${ASSIMP_PKGCONF_INCLUDE_DIRS} /usr/include /usr/local/include /opt/local/include
+    )
 
-find_path(ASSIMP3_INCLUDE_DIR
-  NAMES assimp/types.h
-  PATHS ${ASSIMP_PKGCONF_INCLUDE_DIRS} /usr/include /usr/local/include /opt/local/include
-)
+  if (ASSIMP2_INCLUDE_DIR)
+    message("Assimp 2 found")
+    set(ASSIMP_INCLUDE_DIR ${ASSIMP2_INCLUDE_DIR})
+  endif(ASSIMP2_INCLUDE_DIR)
 
-if (ASSIMP2_INCLUDE_DIR)
-message("Assimp 2 found")
-set(ASSIMP_INCLUDE_DIR ${ASSIMP2_INCLUDE_DIR})
-endif(ASSIMP2_INCLUDE_DIR)
+  if (ASSIMP3_INCLUDE_DIR)
+    message("Assimp 3 found")
+    set(ASSIMP_INCLUDE_DIR ${ASSIMP3_INCLUDE_DIR})
+    add_definitions(-DUSE_ASSIMP3)
+  endif(ASSIMP3_INCLUDE_DIR)
 
-if (ASSIMP3_INCLUDE_DIR)
-message("Assimp 3 found")
-set(ASSIMP_INCLUDE_DIR ${ASSIMP3_INCLUDE_DIR})
-add_definitions(-DUSE_ASSIMP3)
-endif(ASSIMP3_INCLUDE_DIR)
+  # Finally the library itself
+  find_library(ASSIMP_LIBRARY
+    NAMES assimp
+    PATHS ${ASSIMP_PKGCONF_LIBRARY_DIRS}
+    )
 
-# Finally the library itself
-find_library(ASSIMP_LIBRARY
-  NAMES assimp
-  PATHS ${ASSIMP_PKGCONF_LIBRARY_DIRS}
-)
+  #/usr/include/assimp
+  #/usr/local/include
+  #/opt/local/include/assimp
+  #/usr/local/Cellar/assimp/2.0.863/include/assimp
 
-#/usr/include/assimp
-#/usr/local/include
-#/opt/local/include/assimp
-#/usr/local/Cellar/assimp/2.0.863/include/assimp
+  # Set the include dir variables and the libraries and let libfind_process do the rest.
+  # NOTE: Singular variables for this library, plural for libraries this this lib depends on.
+  set(ASSIMP_PROCESS_INCLUDES ASSIMP_INCLUDE_DIR)
+  set(ASSIMP_PROCESS_LIBS ASSIMP_LIBRARY)
+  libfind_process(ASSIMP)
 
-# Set the include dir variables and the libraries and let libfind_process do the rest.
-# NOTE: Singular variables for this library, plural for libraries this this lib depends on.
-set(ASSIMP_PROCESS_INCLUDES ASSIMP_INCLUDE_DIR)
-set(ASSIMP_PROCESS_LIBS ASSIMP_LIBRARY)
-libfind_process(ASSIMP)
-
-
+endif((NOT ASSIMP_INCLUDE_DIR) AND (NOT ASSIMP_LIBRARY)) 
 
