4cc51e6a8a2d7be7de5f281dfb9e26f73b4a2871
Andres Cabrera <andres@lenovo-W530>
2013-08-27 21:49:13 -0700

    Only run cmake again in buildandrun.sh if the target does not exist

--- buildandrun.sh
+++ buildandrun.sh
@@ -1,6 +1,7 @@
 
 
 FILENAME=`echo $1 |cut -d'.' -f1 | sed -e "s|/|_|g"`
+TARGET=${FILENAME}_run
 ALLOVSR_BUILD=0
 
 if [[ $FILENAME == allovsr* ]]
@@ -9,22 +10,26 @@ if [[ $FILENAME == allovsr* ]]
 fi
 
 if [ ! -f Makefile ]; then
-    .distclean
+    ./distclean
     cmake . -DBUILD_ALLOGLV=1 -DBUILD_GLV=1 -DBUILD_ALLOVSR=$ALLOVSR_BUILD
     if [ $? != 0 ]; then
 	exit 1
     fi
 fi 
 
-make `echo $1 |cut -d'.' -f1 | sed -e "s|/|_|g"`_run -j4 $*
+make $TARGET -j4 $*
 
 # If command failed, try again after running cmake (in case it is a new target)
 if [ $? != "0" ]
     then
-    echo "Can't find target --------------- Running CMAKE again"
-    cmake . -DBUILD_ALLOGLV=1 -DBUILD_GLV=1  -DBUILD_ALLOVSR=$ALLOVSR_BUILD
-    if [ $? != 0 ]; then
-	exit 1
-    fi
-    make `echo $1 |cut -d'.' -f1 | sed -e "s|/|_|g"`_run -j4 $*
+    TARGETS=`make -qp | awk -F':' '/^[a-zA-Z0-9][^$#\/\t=]*:([^=]|$)/ {split($1,A,/ /);for(i in A)print A[i]}'`
+    case $TARGETS in
+	*"$TARGET"*) echo "Target exists." ;;
+	*) echo "Can't find target --------------- Running CMAKE again"
+	    cmake . -DBUILD_ALLOGLV=1 -DBUILD_GLV=1  -DBUILD_ALLOVSR=$ALLOVSR_BUILD
+	    if [ $? != 0 ]; then
+		exit 1
+	    fi
+	    make `echo $1 |cut -d'.' -f1 | sed -e "s|/|_|g"`_run -j4 $* ;;
+    esac
 fi
