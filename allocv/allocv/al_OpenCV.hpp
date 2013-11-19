16b7a610668f4363b1b328ef56da4821f5e3f928
Lance Putnam <putnam.lance@gmail.com>
2013-11-19 13:06:57 +0100

    add missing implementation in fromCV functions

--- allocv/allocv/al_OpenCV.hpp
+++ allocv/allocv/al_OpenCV.hpp
@@ -1,7 +1,7 @@
 #ifndef INCLUDE_AL_OPENCV_HPP
 #define INCLUDE_AL_OPENCV_HPP
 
-/*	Allocore --
+/*	AlloSystem --
 	Multimedia / virtual environment application class library
 	
 	Copyright (C) 2009. AlloSphere Research Group, Media Arts & Technology, UCSB.
@@ -40,161 +40,45 @@
 
 	File author(s):
 	Graham Wakefield, 2010, grrrwaaa@gmail.com
+	Lance Putnam, 2013, putnam.lance@gmail.com
 */
 
-// include OpenCV:
-#include "opencv/cv.h"
-#include "opencv/cv.hpp"
-
+#include <opencv2/opencv.hpp>
 #include "allocore/types/al_Array.hpp"
-
 
 namespace al{
 
+/// Create an Array from an OpenCV Mat
+
+/// @param[in] arr			Destination array
+/// @param[in] mat			Source OpenCV matrix
+/// @param[in] copyPolicy	Policy on copying data:
+///							 1 = copy data with same row order (default),
+///							-1 = copy data with rows flipped.
+/// @param[in] rowByteAlign	row byte alignment (1, 2, 4, or 8)
+void fromCV(Array& arr, const cv::Mat& mat, int copyPolicy=1, int rowByteAlign=1);
+
+/// Fill in an Array header from an OpenCV Mat
+
+/// @param[in] hdr			Destination array header
+/// @param[in] mat			Source OpenCV matrix
+/// @param[in] rowByteAlign	row byte alignment (1, 2, 4, or 8)
+void fromCV(AlloArrayHeader& hdr, const cv::Mat& mat, int rowByteAlign=1);
+
+
+// Sets the AlloTy and components fields in an AlloArrayHeader
+void fromCV(AlloArrayHeader& hdr, int CV_TYPE);
+
+
 /// Converts an AlloTy type and component count into an OpenCV type flag:
-inline int toCV(AlloTy type, uint8_t components);
+int toCV(AlloTy type, uint8_t components);
 
-// sets the AlloTy and components fields in an AlloArrayHeader
-inline void fromCV(AlloArrayHeader hdr, int CV_TYPE);
+/// Creates a cv::Mat which directly uses an Array's data (no copying)
 
-/// creates a cv::Mat which directly uses an Array's data (no copying)
 /// This implies that no memory management will be performed by OpenCV.
 /// To copy the data (to persist beyond the Array), call clone() on the result.
-inline CvMat toCV(const Array& arr) {
-	// This for OpenCV2:
-//	size_t steps[ALLO_ARRAY_MAX_DIMS];
-//	int sizes[ALLO_ARRAY_MAX_DIMS];
-//	for (int i=0; i<arr.header.dimcount; i++) {
-//		steps[i] = arr.header.stride[i];
-//		sizes[i] = arr.header.dim[i];
-//	}
-//	return CvMat(
-//		arr.header.dimcount,
-//		sizes,
-//		toCV(arr.header.type, arr.header.components),
-//		arr.data.ptr,
-//		steps
-//	);
-
-	int type = toCV(arr.header.type, arr.header.components);
-	return cvMat(arr.dim(1), arr.dim(0), type, (uchar *)arr.data.ptr);
-}
-
-
-/// create an Array from an OpenCV Mat
-/// default behavior is to copy the data
-//inline void fromCV(Array& arr, const cv::Mat& mat, bool copyData=true) {
-//	AlloArrayHeader hdr;
-//	cv::Size size = mat.size();
-//
-//	fromCV(hdr, mat.type());
-//
-//	arr.format(hdr); // allocates.
-//	arr.data.ptr = (char *)mat.ptr();
-//}
-
-/// Converts an AlloTy type and component count into an OpenCV type flag:
-inline int toCV(AlloTy type, uint8_t components) {
-	switch(type) {
-		case AlloFloat32Ty:
-			switch(components) {
-				case 1:	return CV_32FC1;
-				case 2:	return CV_32FC2;
-				case 3:	return CV_32FC3;
-				case 4:	return CV_32FC4;
-			}
-			break;
-		case AlloFloat64Ty:
-			switch(components) {
-				case 1:	return CV_64FC1;
-				case 2:	return CV_64FC2;
-				case 3:	return CV_64FC3;
-				case 4:	return CV_64FC4;
-			}
-			break;
-		case AlloSInt8Ty:
-			switch(components) {
-				case 1:	return CV_8SC1;
-				case 2:	return CV_8SC2;
-				case 3:	return CV_8SC3;
-				case 4:	return CV_8SC4;
-			}
-			break;
-		case AlloSInt16Ty:
-			switch(components) {
-				case 1: return CV_16SC1;
-				case 2: return CV_16SC2;
-				case 3: return CV_16SC3;
-				case 4: return CV_16SC4;
-			}
-			break;
-		case AlloSInt32Ty:
-			switch(components) {
-				case 1: return CV_32SC1;
-				case 2: return CV_32SC2;
-				case 3: return CV_32SC3;
-				case 4: return CV_32SC4;
-			}
-			break;
-		case AlloUInt16Ty:
-			switch(components) {
-				case 1: return CV_16UC1;
-				case 2: return CV_16UC2;
-				case 3: return CV_16UC3;
-				case 4: return CV_16UC4;
-			}
-			break;
-		default:
-			// unhandled type:
-			return 0;
-	}
-	return 0;
-}
-
-// sets the AlloTy and components fields in an AlloArrayHeader
-inline void fromCV(AlloArrayHeader hdr, int CV_TYPE) {
-	switch (CV_TYPE) {
-		case CV_8UC1: hdr.type = AlloUInt8Ty;	hdr.components = 1;	break;
-		case CV_8UC2: hdr.type = AlloUInt8Ty;	hdr.components = 2;	break;
-		case CV_8UC3: hdr.type = AlloUInt8Ty;	hdr.components = 3;	break;
-		case CV_8UC4: hdr.type = AlloUInt8Ty;	hdr.components = 4;	break;
-
-		case CV_16UC1: hdr.type = AlloUInt16Ty;	hdr.components = 1;	break;
-		case CV_16UC2: hdr.type = AlloUInt16Ty;	hdr.components = 2;	break;
-		case CV_16UC3: hdr.type = AlloUInt16Ty;	hdr.components = 3;	break;
-		case CV_16UC4: hdr.type = AlloUInt16Ty;	hdr.components = 4;	break;
-
-		case CV_8SC1: hdr.type = AlloSInt8Ty;	hdr.components = 1;	break;
-		case CV_8SC2: hdr.type = AlloSInt8Ty;	hdr.components = 2;	break;
-		case CV_8SC3: hdr.type = AlloSInt8Ty;	hdr.components = 3;	break;
-		case CV_8SC4: hdr.type = AlloSInt8Ty;	hdr.components = 4;	break;
-
-		case CV_16SC1: hdr.type = AlloSInt16Ty;	hdr.components = 1;	break;
-		case CV_16SC2: hdr.type = AlloSInt16Ty;	hdr.components = 2;	break;
-		case CV_16SC3: hdr.type = AlloSInt16Ty;	hdr.components = 3;	break;
-		case CV_16SC4: hdr.type = AlloSInt16Ty;	hdr.components = 4;	break;
-
-		case CV_32SC1: hdr.type = AlloSInt32Ty;	hdr.components = 1;	break;
-		case CV_32SC2: hdr.type = AlloSInt32Ty;	hdr.components = 2;	break;
-		case CV_32SC3: hdr.type = AlloSInt32Ty;	hdr.components = 3;	break;
-		case CV_32SC4: hdr.type = AlloSInt32Ty;	hdr.components = 4;	break;
-
-		case CV_32FC1: hdr.type = AlloFloat32Ty;	hdr.components = 1;	break;
-		case CV_32FC2: hdr.type = AlloFloat32Ty;	hdr.components = 2;	break;
-		case CV_32FC3: hdr.type = AlloFloat32Ty;	hdr.components = 3;	break;
-		case CV_32FC4: hdr.type = AlloFloat32Ty;	hdr.components = 4;	break;
-
-		case CV_64FC1: hdr.type = AlloFloat64Ty;	hdr.components = 1;	break;
-		case CV_64FC2: hdr.type = AlloFloat64Ty;	hdr.components = 2;	break;
-		case CV_64FC3: hdr.type = AlloFloat64Ty;	hdr.components = 3;	break;
-		case CV_64FC4: hdr.type = AlloFloat64Ty;	hdr.components = 4;	break;
-
-		default:
-			break;
-	}
-}
+inline CvMat toCV(const Array& arr);
 
 } // al::
 
 #endif
-
