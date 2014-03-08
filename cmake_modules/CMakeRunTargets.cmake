2eb966269b4684b57f97b2b74aabb836442de658
Andres Cabrera <andres@lenovo-W530>
2014-03-08 13:36:38 -0800

    Better handling of run targets (any file or directory can now be chosen)

---
+++ cmake_modules/CMakeRunTargets.cmake
@@ -0,0 +1,128 @@
+
+
+if(BUILD_DIR)
+  file(GLOB ALLOPROJECT_APP_SRC RELATIVE ${CMAKE_CURRENT_SOURCE_DIR} ${BUILD_APP_DIR}/*.cpp)
+  string(REPLACE "/" "_" APP_NAME ${BUILD_APP_DIR})
+  set(SOURCE_DIR ${BUILD_APP_DIR})
+else()
+  set(ALLOPROJECT_APP_SRC ${BUILD_APP_FILE})
+  string(REPLACE "/" "_" APP_NAME ${BUILD_APP_FILE})
+  get_filename_component(APP_NAME ${APP_NAME} NAME)
+  STRING(REGEX REPLACE "\\.[^.]*\$" "" APP_NAME "${APP_NAME}")
+  string(REPLACE "." "_" APP_NAME ${APP_NAME})
+#  get_filename_component(APP_NAME ${APP_NAME} NAME_WE) # Get name w/o extension (extension is anything after first dot!)
+  get_filename_component(SOURCE_DIR ${BUILD_APP_FILE} PATH)
+endif(BUILD_DIR)
+
+set(EXECUTABLE_OUTPUT_PATH ${CMAKE_CURRENT_SOURCE_DIR}/build/bin)
+
+add_executable(${APP_NAME} EXCLUDE_FROM_ALL "${ALLOPROJECT_APP_SRC}")
+if(APPLE)
+  set_target_properties(${APP_NAME} PROPERTIES
+    LINK_FLAGS "-pagezero_size 10000 -image_base 100000000")
+endif(APPLE)
+
+
+if(EXISTS "${SOURCE_DIR}/flags.txt")
+  file(READ "${SOURCE_DIR}/flags.txt" EXTRA_COMPILER_FLAGS)
+  set_target_properties(${APP_NAME} PROPERTIES
+    COMPILE_FLAGS "${EXTRA_COMPILER_FLAGS}")
+  message("WARNING: Using additional flags from ${SOURCE_DIR}/flags.txt: ${EXTRA_COMPILER_FLAGS}")
+endif()
+
+message("Target: ${APP_NAME}")
+message("From sources: ${ALLOPROJECT_APP_SRC}")
+
+# Dependencies (check if targets exist and set variables)
+get_target_property(ALLOCORE_LIBRARY allocore LOCATION)
+get_target_property(ALLOCORE_INCLUDE_DIR allocore ALLOCORE_INCLUDE_DIR)
+get_target_property(ALLOCORE_LINK_LIBRARIES allocore ALLOCORE_LINK_LIBRARIES)
+add_dependencies(${APP_NAME} allocore) 
+
+message("Using allocore headers from: ${ALLOCORE_INCLUDE_DIR}")
+
+if(TARGET Gamma)
+  set(GAMMA_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/Gamma)
+  get_target_property(GAMMA_LIBRARY Gamma LOCATION)
+  add_dependencies(${APP_NAME} Gamma)
+else()
+  if(NOT GAMMA_LIBRARY)
+    set(GAMMA_LIBRARY "")
+    message("Not building GAMMA and no usable GAMMA binary found. Not linking application to GAMMA")
+  endif(NOT GAMMA_LIBRARY)  
+endif(TARGET Gamma)
+
+if(TARGET GLV)
+  set(GLV_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/GLV/GLV)
+  get_target_property(GLV_LIBRARY GLV LOCATION)
+  add_dependencies(${APP_NAME} GLV)
+else()
+  if(NOT GLV_LIBRARY)
+    set(GLV_LIBRARY "")
+    message("Not building GLV and no usable GLV binary found. Not linking application to GLV")
+  endif(NOT GLV_LIBRARY)  
+endif(TARGET GLV)
+
+if(TARGET VSR)
+  if(TARGET ALLOVSR)
+    set(VSR_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/vsr)
+    set_target_properties(vsr PROPERTIES GLV_INCLUDE_DIR "${GLV_RELATIVE_DIR}"
+      GLV_LIBRARY "${GLV_LIBRARY}")
+    get_target_property(VSR_LIBRARY vsr LOCATION)
+  add_dependencies(${APP_NAME} vsr)
+  endif(TARGET ALLOVSR)
+else()
+  if(NOT VSR_LIBRARY)
+    set(VSR_LIBRARY "")
+    message("Not building VSR and no usable VSR binary found. Not linking application to VSR")
+  endif(NOT VSR_LIBRARY) 
+endif(TARGET VSR)
+
+if(TARGET alloutil)
+  get_target_property(ALLOUTIL_LIBRARY alloutil LOCATION)
+  get_target_property(ALLOUTIL_INCLUDE_DIR alloutil ALLOUTIL_INCLUDE_DIR)
+  get_target_property(ALLOUTIL_LINK_LIBRARIES alloutil ALLOUTIL_LINK_LIBRARIES)
+  add_dependencies(${APP_NAME} alloutil)
+else()
+  if(NOT ALLOUTIL_LIBRARY)
+    set(ALLOUTIL_LIBRARY "")
+    message("Not building ALLOUTIL and no usable ALLOUTIL binary found. Not linking application to ALLOUTIL")
+  endif(NOT ALLOUTIL_LIBRARY) 
+endif(TARGET alloutil)
+
+if(TARGET alloGLV)
+  get_target_property(ALLOGLV_LIBRARY alloGLV LOCATION)
+  get_target_property(ALLOGLV_INCLUDE_DIR alloGLV ALLOGLV_INCLUDE_DIR)
+  get_target_property(ALLOGLV_LINK_LIBRARIES alloGLV ALLOGLV_LINK_LIBRARIES)
+  add_dependencies(${APP_NAME} alloGLV)
+else()
+  if(NOT ALLOGLV_LIBRARY)
+    set(ALLOGLV_LIBRARY "")
+    message("Not building alloGLV and no usable alloGLV binary found. Not linking application to alloGLV")
+  endif(NOT ALLOGLV_LIBRARY) 
+endif(TARGET alloGLV)
+
+
+# TODO copy resources to build directory
+
+#file(GLOB ALLOPROJECT_APP_SRC RELATIVE ${CMAKE_CURRENT_SOURCE_DIR} ${BUILD_APP_DIR}/*.*)
+
+include_directories(${ALLOCORE_INCLUDE_DIR}
+  ${ALLOUTIL_INCLUDE_DIR}
+  ${ALLOGLV_INCLUDE_DIR}
+  ${ALLOVSR_INCLUDE_DIR}
+  ${GAMMA_INCLUDE_DIR} )
+#    message("Gamma : ${GAMMA_INCLUDE_DIRs}")
+target_link_libraries(${APP_NAME}
+  ${ALLOCORE_LIBRARY} 
+  ${ALLOUTIL_LIBRARY} 
+  ${ALLOGLV_LIBRARY}
+  ${GAMMA_LIBRARY} ${GLV_LIBRARY} ${VSR_LIBRARY}
+  ${ALLOCORE_LINK_LIBRARIES} ${ALLOUTIL_LINK_LIBRARIES} ${ALLOGLV_LINK_LIBRARIES})
+#list(REMOVE_ITEM PROJECT_RES_FILES ${ALLOPROJECT_APP_SRC})
+add_custom_target("${APP_NAME}_run"
+  COMMAND "${APP_NAME}"
+  DEPENDS "${APP_NAME}"
+  WORKING_DIRECTORY "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}"
+  SOURCES ${ALLOPROJECT_APP_SRC}
+  COMMENT "Running: ${APP_NAME}")
--- run.sh
+++ run.sh
@@ -1,51 +1,40 @@
-#! /bin/sh
+#!/bin/bash
 
-ALLOVSR_BUILD=0
-if [[ $FILENAME == allovsr* ]]
-    then
-    ALLOVSR_BUILD=1
-fi
-# Change default flags if there's need:
-DEFAULT_FLAGS="-DBUILD_ALLOGLV=0 -DBUILD_GLV=0 -DBUILD_ALLOVSR=$ALLOVSR_BUILD" 
+# Change this to suit your needs
+BUILD_ALLOGLV=1
+BUILD_GLV=0
+BUILD_VSR=0
+BUILD_GAMMA=0
 
-# Users shouldn't need to change anything below
-FILENAME=`echo $1 |cut -d'.' -f1 | sed -e "s|/|_|g"`
-TARGET=${FILENAME}_run
+# ------------------------------------------------
+# You shouldn't need to touch the stuff below
 
-if [ ! -f Makefile ]; then
-    ./distclean
-    cmake .  ${DEFAULT_FLAGS} &> cmake_output.txt
-    if [ $? != 0 ]; then
-	cat cmake_output.txt
-	exit 1
-    fi
-fi 
+CMAKE_FLAGS="-DBUILD_ALLOGLV=${BUILD_ALLOGLV} -DBUILD_GLV=${BUILD_GLV} -DBUILD_VSR=${BUILD_VSR} -DBUILD_GAMMA=${BUILD_GAMMA}"
 
-echo "Building $1"
-make ${FILENAME} -j4 $* &> make_output.txt
-
-
-# If command failed, try again after running cmake (in case it is a new target)
-if [ $? != "0" ]
-    then
-    TARGETS=`make -qp | awk -F':' '/^[a-zA-Z0-9][^$#\/\t=]*:([^=]|$)/ {split($1,A,/ /);for(i in A)print A[i]}'`
-    case $TARGETS in
-	*"$TARGET"*) cat make_output.txt 
-		exit 1
-		;;
-	
-	*) echo "Can't find target --------------- Running CMAKE again"
-	    cmake .  ${DEFAULT_FLAGS} &> cmake_output.txt
-	    if [ $? != 0 ]; then
-		cat cmake_output.txt
-		exit 1
-	    fi
-	    make ${FILENAME} -j4 $* &> make_output.txt
-	    if [ $? != "0" ]
-	    then
-		cat make_output.txt
-	    fi
-    esac
+if [ $# == 0 ]
+then
+    echo Aborting: You must provide a source filename or a directory
+    exit 1
 fi
 
-make ${TARGET}
+FILENAME=$(basename "$1")
+DIRNAME=$(dirname "$1")
+FILENAME="${DIRNAME//./_}_${FILENAME%.*}"
+
+echo FILENAME: ${FILENAME}
+TARGET=${FILENAME//\//_}_run
+echo TARGET: ${TARGET}
+
+if [ -f $1 ]
+then
+  TARGET_FLAG="-DBUILD_APP_FILE=$1 -DBUILD_DIR=0"
+elif [ -d $1 ]
+then
+  TARGET_FLAG="-DBUILD_APP_DIR=$1 -DBUILD_DIR=1"
+else
+  echo Aborting: $1 is neither a file nor directory
+  exit 1
+fi
+
+cmake . ${CMAKE_FLAGS} ${TARGET_FLAG} -DNO_EXAMPLES=1
+make $TARGET -j4 $*
