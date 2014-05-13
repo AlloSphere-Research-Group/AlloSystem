00eb475c2b07afba83973c1584bc630449221bc1
Andres Cabrera <andres@lenovo-W530>
2014-05-12 21:34:20 -0700

    Added debug.sh script to run files in gdb

--- debug.sh
+++ debug.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # Change this to suit your needs
-BUILD_ALLOUTIL=1
 BUILD_ALLOGLV=1
 BUILD_GLV=0
 BUILD_VSR=0
@@ -10,7 +9,7 @@ BUILD_GAMMA=0
 # ------------------------------------------------
 # You shouldn't need to touch the stuff below
 
-CMAKE_FLAGS="-DBUILD_ALLOUTIL=${BUILD_ALLOUTIL} -DBUILD_ALLOGLV=${BUILD_ALLOGLV} -DBUILD_GLV=${BUILD_GLV} -DBUILD_VSR=${BUILD_VSR} -DBUILD_GAMMA=${BUILD_GAMMA}"
+CMAKE_FLAGS="-DBUILD_ALLOGLV=${BUILD_ALLOGLV} -DBUILD_GLV=${BUILD_GLV} -DBUILD_VSR=${BUILD_VSR} -DBUILD_GAMMA=${BUILD_GAMMA}"
 
 if [ $# == 0 ]
 then
@@ -22,22 +21,20 @@ FILENAME=$(basename "$1")
 DIRNAME=$(dirname "$1")
 FILENAME="${DIRNAME//./_}_${FILENAME%.*}"
 
-#echo FILENAME: ${FILENAME}
+echo FILENAME: ${FILENAME}
 TARGET=${FILENAME//\//_}_run
-#echo TARGET: ${TARGET}
+echo TARGET: ${TARGET}
 
 if [ -f $1 ]
 then
   TARGET_FLAG="-DBUILD_APP_FILE=$1 -DBUILD_DIR=0"
-  echo RUN SCRIPT: Building file $1
 elif [ -d $1 ]
 then
   TARGET_FLAG="-DBUILD_APP_DIR=$1 -DBUILD_DIR=1"
-  echo RUN SCRIPT: Building all files in dir $1
 else
   echo Aborting: $1 is neither a file nor directory
   exit 1
 fi
 
-cmake . ${CMAKE_FLAGS} ${TARGET_FLAG} -DNO_EXAMPLES=1 -DRUN_IN_DEBUGGER=0 -DCMAKE_BUILD_TYPE=Release
+cmake . ${CMAKE_FLAGS} ${TARGET_FLAG} -DNO_EXAMPLES=1 -DRUN_IN_DEBUGGER=1 -DCMAKE_BUILD_TYPE=Debug
 make $TARGET -j4 $*
