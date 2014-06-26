c76eecdeab8cb04f9edba98d63de87f9183afc5a
Drew Waranis <drew@waran.is>
2014-06-26 00:26:03 -0700

    Removed bashisms from allonect/install_dependencies.sh .

--- allonect/install_dependencies.sh
+++ allonect/install_dependencies.sh
@@ -1,36 +1,49 @@
 #!/bin/sh
 
-if [ `which apt-get` ]; then
-	echo "Found apt-get"
+# Helper functions.
+binary_exists(){
+	which "$1" >/dev/null 2>&1;
+}
+# End helper functions.
+
+if (binary_exists apt-get); then
+	echo 'Found apt-get'
 #	sudo apt-get update
-#	sudo apt-get install libfreenect-dev # no good; this version is too old
-	sudo apt-get install libusb-1.0-0-dev libxmu-dev libxi-dev cmake
+	sudo apt-get install libfreenect-dev
+	sudo apt-get install libusb-1.0-0-dev libxmu-dev libxi-dev
+	# Saucy's libfreenect-dev should be new enough now.
 	# this needs to be run from the allocore root folder
 	# grab libfreenect:
-	git submodule init allonect/libfreenect && git submodule update allonect/libfreenect
-	cd allonect/libfreenect
-	cmake .
-	make
-	sudo make install
-	cd ../
-	echo You need to give freenect permissions:
-	echo sudo cp 66-kinect.rules /etc/udev/rules.d/66-kinect.rules
-	echo sudo adduser YOURNAME video
-	echo and reboot
+	# sudo apt-get install cmake
+	# DIR=$PWD
+	# cd /tmp
+	# git submodule init allonect/libfreenect && git submodule update allonect/libfreenect
+	# cd allonect/libfreenect
+	# cmake .
+	# make
+	# sudo make install
+	# cd $DIR
+	# echo You need to give freenect permissions:
+	# echo sudo cp 66-kinect.rules /etc/udev/rules.d/66-kinect.rules
+	# echo sudo adduser YOURNAME video
+	# echo and reboot
 
-elif [ `which port` ]; then
-	echo "Found MacPorts"
+elif (binary_exists brew); then
+	echo 'Found Homebrew'
+	DIR=$PWD
+	cd /usr/local/Library/Formula
+		curl --insecure -O "https://raw.github.com/OpenKinect/libfreenect/master/platform/osx/homebrew/libfreenect.rb"
+		curl --insecure -O "https://raw.github.com/OpenKinect/libfreenect/master/platform/osx/homebrew/libusb-freenect.rb"
+		brew install libfreenect
+	cd "$DIR"
+
+elif (binary_exists port); then
+	echo 'Tell us if a port appears in MacPorts'
 	sudo port selfupdate
+	port search freenect
 #	port install freenect
 
-elif [ `which brew` ]; then
-	echo "Found Homebrew"
-	cd /usr/local/Library/Formula
-	curl --insecure -O "https://raw.github.com/OpenKinect/libfreenect/master/platform/osx/homebrew/libfreenect.rb"
-	curl --insecure -O "https://raw.github.com/OpenKinect/libfreenect/master/platform/osx/homebrew/libusb-freenect.rb"
-	brew install libfreenect
-
 else
-	echo "Error: No suitable package manager found."
-	echo "Error: Install apt-get, MacPorts, or Homebrew and try again."
+	echo 'Error: No suitable package manager found.'
+	echo 'Error: Install apt-get, MacPorts, or Homebrew and try again.'
 fi
