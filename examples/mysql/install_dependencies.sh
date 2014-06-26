0d0d2e162e83a5860fa6ab93d534bca6004d680d
Drew Waranis <drew@waran.is>
2014-06-26 00:08:06 -0700

    Removed bashisms from examples/mysql/install_dependencies.sh .

--- examples/mysql/install_dependencies.sh
+++ examples/mysql/install_dependencies.sh
@@ -1,24 +1,35 @@
 #!/bin/sh
 
-if [ `which apt-get` ]; then
-	echo "Found apt-get"
+ # AlloSystem mysql example dependencies install script
+
+# Helper functions.
+binary_exists(){
+	which "$1" >/dev/null 2>&1;
+}
+# End helper functions.
+
+# Put flags.txt next to the mysql example.
+MYSQL_DIR=$( cd "$( dirname "$0" )" && pwd )
+
+if (binary_exists apt-get); then
+	echo 'Found apt-get'
 	sudo apt-get update
 	sudo apt-get install libmysql++-dev
-  echo "-I/usr/include/mysql -lmysqlpp" > flags.txt
+  echo '-I/usr/include/mysql -lmysqlpp' > "${MYSQL_DIR}/flags.txt"
 
-elif [ `which port` ]; then
-	echo "Found MacPorts"
-	sudo port selfupdate
-	sudo port install mysqlxx +universal
-  echo "-L/opt/local/lib -I/opt/local/include -lmysqlpp" > flags.txt
-
-elif [ `which brew` ]; then
-	echo "Found Homebrew"
+elif (binary_exists brew); then
+	echo 'Found Homebrew'
 	brew update
 	brew install mysql++
-  echo "-L/usr/local/lib -I/usr/local/include -lmysqlpp" > flags.txt
+  echo '-L/usr/local/lib -I/usr/local/include -lmysqlpp' > "${MYSQL_DIR}/flags.txt"
+
+elif (binary_exists port); then
+	echo 'Found MacPorts'
+	sudo port selfupdate
+	sudo port install mysqlxx +universal
+  echo '-L/opt/local/lib -I/opt/local/include -lmysqlpp' > "${MYSQL_DIR}/flags.txt"
 
 else
-	echo "Error: No suitable package manager found."
-	echo "Error: Install apt-get, MacPorts, or Homebrew and try again."
+	echo 'Error: No suitable package manager found.'
+	echo 'Error: Install apt-get, MacPorts, or Homebrew and try again.'
 fi
