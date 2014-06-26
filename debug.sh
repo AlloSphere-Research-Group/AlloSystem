86aab0f2e2245ba3e3dcb6b8d9d83ebdbde90fab
Drew Waranis <drew@waran.is>
2014-06-26 01:31:50 -0700

    Removed bashisms from run.sh and debug.sh .

--- debug.sh
+++ debug.sh
@@ -1,34 +1,36 @@
-#!/bin/bash
+#!/bin/sh
 
 DEBUGGER=gdb
 
 # ------------------------------------------------
 # You shouldn't need to touch the stuff below
 
-if [ $# == 0 ]
+if [ $# = 0 ]
 then
     echo Aborting: You must provide a source filename or a directory
     exit 1
 fi
 
-FILENAME=$(basename "$1")
+# Remove file extension.
+FILENAME=$(basename "$1" | sed 's/\.[^.]*$//')
 DIRNAME=$(dirname "$1")
-FILENAME="${DIRNAME//./_}_${FILENAME%.*}"
 
-echo FILENAME: ${FILENAME}
-TARGET=${FILENAME//\//_}_run
-echo TARGET: ${TARGET}
+# Combine and replace periods and slashes with underscores.
+TARGET=$(echo "${DIRNAME}_${FILENAME}_run" | sed 's/\./_/g' | sed 's/\//_/g')
 
-if [ -f $1 ]
-then
-  TARGET_FLAG="-DBUILD_APP_FILE=$1 -DBUILD_DIR=0"
-elif [ -d $1 ]
-then
-  TARGET_FLAG="-DBUILD_APP_DIR=$1 -DBUILD_DIR=1"
+# Had to separate out DBUILD_FLAG to ensure correct globbing in the cmake line.
+if [ -f "$1" ]; then
+  TARGET_FLAG="-DBUILD_APP_FILE=$1"
+  DBUILD_FLAG="-DBUILD_DIR=0"
+  echo RUN SCRIPT: Building file "$1"
+elif [ -d "$1" ]; then
+  TARGET_FLAG="-DBUILD_APP_DIR=$1"
+  DBUILD_FLAG="-DBUILD_DIR=1"
+  echo RUN SCRIPT: Building all files in dir "$1"
 else
-  echo Aborting: $1 is neither a file nor directory
+  echo Aborting: "$1" is neither a file nor directory
   exit 1
 fi
 
-cmake . ${CMAKE_FLAGS} ${TARGET_FLAG} -DRUN_IN_DEBUGGER=1 -DALLOSYSTEM_DEBUGGER=${DEBUGGER} -DCMAKE_BUILD_TYPE=Debug
-make $TARGET -j4 $*
+cmake . "$CMAKE_FLAGS" "$TARGET_FLAG" "$DBUILD_FLAG" -DRUN_IN_DEBUGGER=1 -DALLOSYSTEM_DEBUGGER=${DEBUGGER} -DCMAKE_BUILD_TYPE=Debug
+make "$TARGET" -j4 "$*"
--- run.sh
+++ run.sh
@@ -1,40 +1,41 @@
-#!/bin/bash
+#!/bin/sh
 
 # ------------------------------------------------
 # You shouldn't need to touch the stuff below
 
-if [ $# == 0 ]
+if [ $# = 0 ]
 then
     echo Aborting: You must provide a source filename or a directory
     exit 1
 fi
 
-FILENAME=$(basename "$1")
+# Remove file extension.
+FILENAME=$(basename "$1" | sed 's/\.[^.]*$//')
 DIRNAME=$(dirname "$1")
 
-if [ ${DIRNAME} == "." ]
-then
-  FILENAME="${FILENAME%.*}"
-else
-  FILENAME="${DIRNAME//./_}_${FILENAME%.*}"
+# Replace all forward slashes with underscores.
+TARGET=$(echo "${DIRNAME}_${FILENAME}_run" | sed 's/\//_/g')
+
+if [ "$DIRNAME" != "." ]; then
+  # Replace all periods with underscores.
+  TARGET=$(echo "${TARGET}" | sed 's/\./_/g')
 fi
 
 #echo FILENAME: ${FILENAME}
-TARGET=${FILENAME//\//_}_run
 #echo TARGET: ${TARGET}
 
-if [ -f $1 ]
-then
-  TARGET_FLAG="-DBUILD_APP_FILE=$1 -DBUILD_DIR=0"
-  echo RUN SCRIPT: Building file $1
-elif [ -d $1 ]
-then
-  TARGET_FLAG="-DBUILD_APP_DIR=$1 -DBUILD_DIR=1"
-  echo RUN SCRIPT: Building all files in dir $1
+if [ -f "$1" ]; then
+  TARGET_FLAG="-DBUILD_APP_FILE=$1"
+  DBUILD_FLAG="-DBUILD_DIR=0"
+  echo RUN SCRIPT: Building file "$1"
+elif [ -d "$1" ]; then
+  TARGET_FLAG="-DBUILD_APP_DIR=$1"
+  DBUILD_FLAG="-DBUILD_DIR=1"
+  echo RUN SCRIPT: Building all files in dir "$1"
 else
-  echo Aborting: $1 is neither a file nor directory
+  echo Aborting: "$1" is neither a file nor directory
   exit 1
 fi
 
-cmake . ${TARGET_FLAG} -DRUN_IN_DEBUGGER=0 -DCMAKE_BUILD_TYPE=Release -Wno-dev
-make $TARGET -j7 $*
+cmake . "$TARGET_FLAG" "$DBUILD_FLAG" -DRUN_IN_DEBUGGER=0 -DCMAKE_BUILD_TYPE=Release -Wno-dev
+make "$TARGET" -j7 "$*"
