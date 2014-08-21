fa3139988e4aecc231756675b652d5635c603c84
Drew Waranis <drew@waran.is>
2014-08-21 13:20:25 -0700

    Added linux support to generate_system_info.sh .

--- doc/generate_system_info.sh
+++ doc/generate_system_info.sh
@@ -1,9 +1,24 @@
 #!/bin/sh
-echo > bug_report_system_info.txt
-system_profiler SPSoftwareDataType >> bug_report_system_info.txt
-echo "----------------------------" >> bug_report_system_info.txt
-uname -a >> bug_report_system_info.txt
-echo "----------------------------" >> bug_report_system_info.txt
-gcc -v -x c++ /dev/null -fsyntax-only 2>> bug_report_system_info.txt
-echo "----------------------------" >> bug_report_system_info.txt
-xcodebuild -version >> bug_report_system_info.txt
+
+OS=$(uname -s)
+
+# OS X
+if [ "$OS" = "Darwin" ]; then
+	{
+		system_profiler SPSoftwareDataType
+		echo '----------------------------'
+		uname -a
+		echo '----------------------------'
+		gcc -v -x c++ /dev/null -fsyntax-only
+		echo '----------------------------'
+		xcodebuild -version
+	} >> bug_report_system_info.txt 2>&1 # Also pickup stderr.
+elif [ "$OS" = "Linux" ]; then
+	{
+		lsb_release -idrc
+		echo '----------------------------'
+		uname -a
+		echo '----------------------------'
+		gcc -v -x c++ /dev/null -fsyntax-only
+	} >> bug_report_system_info.txt 2>&1
+fi
