ad2abb272de1ef83a9bbfbeb86d4496e54a4f96a
Lance Putnam <putnam.lance@gmail.com>
2014-10-25 16:36:08 +0200

    use * operator

--- allocore/allocore/math/al_Matrix4.hpp
+++ allocore/allocore/math/al_Matrix4.hpp
@@ -501,18 +501,18 @@ public:
 	}
 	
 	/// Computes product of matrix multiplied by column vector, r = m * vCol
-	// This is typically what is required to project a vertex through a transform
-	// For a better explanation, @see http://xkcd.com/184/ -g
+	
+	/// This is typically what is required to project a vertex through a transform.
+	/// For a better explanation, @see http://xkcd.com/184/ -g
+	Vec<4,T> transform(const Vec<4,T>& vCol) const {
+		return *this * vCol;
+	}
+
+	/// Computes product of matrix multiplied by column vector, r = m * vCol
 	Vec<4,T> transform(const Vec<3,T>& vCol) const {
-		Vec<4,T> r, v(vCol[0], vCol[1], vCol[2], 1.);
-		Mat<4,T>::multiply(r, *this, v);
-		return r;
+		return transform(Vec<4,T>(vCol, T(1)));
 	}
-	Vec<4,T> transform(const Vec<4,T>& v) const {
-		Vec<4,T> r;
-		Mat<4,T>::multiply(r, *this, v);
-		return r;
-	}
+
 
 	
 	/// Get the inverse of a matrix
