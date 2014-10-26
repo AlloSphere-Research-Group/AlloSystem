92d99dcf3aa6925084d1043cbcd7c0595c6a9e5b
Lance Putnam <putnam.lance@gmail.com>
2014-10-26 17:35:55 +0100

    added simple OSC client/server examples

---
+++ allocore/examples/io/OSCClient.cpp
@@ -0,0 +1,47 @@
+/*
+Allocore Example: OSC Client
+
+Description:
+This is a simple OSC client that periodically sends out a packet with the
+address "/test" and containing a string and int.
+
+You should run the OSC server example BEFORE running this program.
+
+Author:
+Lance Putnam, Oct. 2014
+*/
+
+#include <iostream>
+#include <string>
+#include "allocore/al_Allocore.hpp"
+using namespace al;
+
+int main(){
+
+	// The port over which to send packets
+	short port = 16447;
+	
+	// The IP address to send packets to
+	const char * addr = "127.0.0.1";
+	//const char * addr = "172.28.247.249";
+
+	// Create an OSC client
+	osc::Send client(port, addr);
+
+	// Here, we use a loop to send a packet out every half second.
+	//int i=1; while(i++){
+	for(int i=0; i<10; ++i){
+		std::string str = "Hello!";
+		
+		// The send function takes an OSC address pattern as the first argument
+		// followed by the packet data.
+		int b = client.send("/test", str, i);
+		
+		std::cout << "CLIENT: sent message (" << b << " bytes)\n";
+		
+		// Wait before sending the next packet, otherwise they will all get
+		// sent at once.
+		al::wait(0.5);
+	}
+
+}
---
+++ allocore/examples/io/OSCServer.cpp
@@ -0,0 +1,67 @@
+/*
+Allocore Example: OSC Server
+
+Description:
+This is a simple OSC server that listens for packets with the address "/test"
+and containing a string and int. 
+
+You should run the OSC client example AFTER running this program.
+
+Author:
+Lance Putnam, Oct. 2014
+*/
+
+#include <iostream>
+#include <string>
+#include "allocore/io/al_App.hpp"
+using namespace al;
+
+class MyApp : public App, public osc::PacketHandler{
+public:
+
+	osc::Recv server;
+
+	MyApp()
+		/* Constructor args:
+			port, client IP address ("" for any), timeout in seconds
+		
+		The port MUST match the client's port. The timeout specifies how often 
+		the server checks for new data on the on the port. You will	want to tune
+		this to rate at which the client is sending packets.*/
+	:	server(16447, "", 0.05)
+	{
+		// Print out our IP address
+		//std::cout << "SERVER: My IP is " << Socket::hostIP() << "\n";
+	
+		// Register ourself (osc::PacketHandler) with the server so onMessage
+		// gets called.
+		server.handler(*this);
+
+		// Start a thread to handle incoming packets
+		server.start();
+		
+		initWindow(Window::Dim(160), "OSC server");
+	}
+
+	// This gets called whenever we receive a packet
+	void onMessage(osc::Message& m){
+
+		// Check that the address and tags match what we expect
+		if(m.addressPattern() == "/test" && m.typeTags() == "si"){
+		
+			// Extract the data out of the packet
+			std::string str;
+			int val;
+			m >> str >> val;
+			
+			// Print out the extracted packet data
+			std::cout << "SERVER: recv " << str << " " << val << "\n";
+		}
+	}
+
+};
+
+
+int main(){
+	MyApp().start();
+}
