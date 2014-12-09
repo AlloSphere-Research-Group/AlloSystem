1aa6b55ab4d6d61432b1d542505019e73874f62b
Andres Cabrera <mantaraya36@gmail.com>
2014-12-08 22:07:49 -0800

    Added example for flags.cmake

---
+++ examples/dependencies/flags.cmake
@@ -0,0 +1,18 @@
+find_library(CUTTLEBONE_LIB cuttlebone 
+	PATHS cuttlebone/build/ /usr/local/lib)
+
+find_path(CUTTLEBONE_INCLUDE_DIR Cuttlebone/Cuttlebone.hpp
+          PATHS cuttlebone /usr/local/include)
+
+if(NOT(CUTTLEBONE_INCLUDE_DIR AND CUTTLEBONE_LIB))
+    message("Cuttlebone not found. Can't build application")
+    return()
+endif(NOT(CUTTLEBONE_INCLUDE_DIR AND CUTTLEBONE_LIB))
+
+include_directories(${CUTTLEBONE_INCLUDE_DIR})
+
+target_link_libraries("${APP_NAME}" ${CUTTLEBONE_LIB})
+
+# You can also use all of CMake here:
+
+set( CMAKE_CXX_FLAGS  "${CMAKE_CXX_FLAGS} -std=c++11 -stdlib=libc++")
