695676a9f5645b8678eae34955889a83e51bde0f
Drew Waranis <drew@waran.is>
2014-12-09 16:38:30 -0800

    Updated example flags.cmake to find Cuttlebone alongside AlloSystem.

--- examples/dependencies/flags.cmake
+++ examples/dependencies/flags.cmake
@@ -1,12 +1,16 @@
-find_library(CUTTLEBONE_LIB cuttlebone 
-	PATHS cuttlebone/build/ /usr/local/lib)
+find_library(CUTTLEBONE_LIB
+	NAMES cuttlebone
+	PATHS ${CMAKE_SOURCE_DIR}/../cuttlebone/build/ /usr/local/lib)
 
-find_path(CUTTLEBONE_INCLUDE_DIR Cuttlebone/Cuttlebone.hpp
-          PATHS cuttlebone /usr/local/include)
+find_path(CUTTLEBONE_INCLUDE_DIR
+	NAMES Cuttlebone/Cuttlebone.hpp
+	PATHS ${CMAKE_SOURCE_DIR}/../cuttlebone/ /usr/local/include)
 
 if(NOT(CUTTLEBONE_INCLUDE_DIR AND CUTTLEBONE_LIB))
     message("Cuttlebone not found. Can't build application")
     return()
+else(NOT(CUTTLEBONE_INCLUDE_DIR AND CUTTLEBONE_LIB))
+	message("Cuttlebone found.")
 endif(NOT(CUTTLEBONE_INCLUDE_DIR AND CUTTLEBONE_LIB))
 
 include_directories(${CUTTLEBONE_INCLUDE_DIR})
