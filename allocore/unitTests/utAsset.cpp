c7dccc049a69b770d72593dfe3d046439e73b531
Andres Cabrera <mantaraya36@gmail.com>
2014-12-14 18:16:09 -0800

    Moved allocore unit test to use cmake.

--- allocore/unitTests/utAsset.cpp
+++ allocore/unitTests/utAsset.cpp
@@ -3,10 +3,9 @@
 #include "allocore/graphics/al_Asset.hpp"
 
 int utAsset() {
-	
-	
-	Scene * scene = Scene::import(getSearchPaths().find("ducky.obj").filepath());
+	SearchPaths searchPaths;
+	searchPaths.addAppPaths();
+	Scene * scene = Scene::import(searchPaths.find("ducky.obj").filepath());
 	scene->dump();
-		
 	return 0;
 }
