8425cbf6dcf38ea70439f332b9b36ada5b7cf14d
Andres Cabrera <mantaraya36@gmail.com>
2015-10-13 22:11:33 -0700

    Added doxygen documentation for the Parameter and ParameterServer classes

--- allocore/allocore/system/al_Parameter.hpp
+++ allocore/allocore/system/al_Parameter.hpp
@@ -51,27 +51,47 @@ namespace al
 /**
  * @brief The Parameter class
  * 
+ * The Parameter class offers a convenient mechanism for safely passing 
+ * parameter values from a low priority ot a high priority thread, for example
+ * from a computation thread to an audio thread (i.e. the audio callback).
+ * 
+ * The function is thread safe and there can be any number of readers and writers.
+ * 
+ * Parameters are created with:
  * @code
-	Parameter freq("Frequency", "Group", default, "/path/prefix"); // This recieves on osc address "/path/prefix/Group/Frequency" 
-	
-	
+	Parameter freq("Frequency", "Group", default, "/path/prefix");
+ * @endcode
+ * 
+ * Then values can be set from a low priority thread:
+ * @code
 	// In a simulator thread
 	freq.set(var);
-	
+ * @endcode
+ * 
+ * And read back from a high priority thread:
+ * @code
 	// In the audio thread
 	float curFreq = freq.get()
  * @endcode
+ * 
+ * The values are clamped between a minimum and maximum set using the min() and
+ * max() functions.
+ * 
+ * The mechanism used to protect data is locking a mutex within the set() function
+ * and doing try_lock() on the mutex to update a cached value in the get()
+ * function. In the worst case this might incur some jitter when reading the value.
+ * 
+ * The ParameterServer class allows exposing Parameter objects via OSC.
  */
 class Parameter{
 public:
 	/**
    * @brief Parameter
    * 
-   * 
-   * @param parameterName
-   * @param Group
-   * @param defaultValue
-   * @param prefix
+   * @param parameterName The name of the parameter
+   * @param Group The group the parameter belongs to
+   * @param defaultValue The initial value for the parameter
+   * @param prefix An address prefix that is prepended to the parameter's OSC address
    */
 	Parameter(std::string parameterName, std::string Group,
 	          float defaultValue,
@@ -79,15 +99,45 @@ public:
 	
 	~Parameter();
 	
+	/**
+	 * @brief set the parameter's value
+	 * 
+	 * This function is thread-safe and can be called from any number of threads
+	 */
 	void set(float value);
+	/**
+	 * @brief get the parameter's value
+	 * 
+	 * This function is thread-safe and can be called from any number of threads
+	 * 
+	 * @return the parameter value
+	 */
 	float get();
 	
-	float min() {return mMin;}
+	/**
+	 * @brief set the minimum value for the parameter
+	 * 
+	 * The value returned by the get() function will be clamped and will not go
+	 * under the value set by this function.
+	 */
 	void min(float minValue) {mMin = minValue;}
+	float min() {return mMin;}
 	
-	float max() {return mMax;}
+	/**
+	 * @brief set the maximum value for the parameter
+	 * 
+	 * The value returned by the get() function will be clamped and will not go
+	 * over the value set by this function.
+	 */
 	void max(float maxValue) {mMax = maxValue;}
+	float max() {return mMax;}
 	
+	/**
+	 * @brief return the full OSC address for the parameter
+	 * 
+	 * The parameter needs to be registered to a ParameterServer to listen to
+	 * OSC values on this address
+	 */
 	std::string getFullAddress();
 	
 private:
@@ -104,15 +154,31 @@ private:
 	std::mutex mMutex;
 };
 
-
+/**
+ * @brief The ParameterServer class creates an OSC server to receive parameter values
+ * 
+ * Parameter objects that are registered with a ParameterServer will receive 
+ * incoming messages on their OSC address.
+ */
 class ParameterServer : public osc::PacketHandler
 {
 public:
+	/**
+	 * @brief ParameterServer constructor
+	 * 
+	 * @param oscAddress The network address on which to listen to. If empty use all available network interfaces. Defaults to "127.0.0.1".
+	 * @param oscPort The network port on which to listen. Defaults to 9010.
+	 */
 	ParameterServer(std::string oscAddress = "127.0.0.1", int oscPort = 9010);
 	~ParameterServer();
 	
+	/**
+	 * Register a parameter with the server.
+	 */
 	void registerParameter(Parameter &param);
-	
+	/**
+	 * Remove a parameter from the server.
+	 */
 	void unregisterParameter(Parameter &param);
 	
 	virtual void onMessage(osc::Message& m);
