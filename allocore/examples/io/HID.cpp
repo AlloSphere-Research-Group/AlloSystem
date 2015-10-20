2930faf6d3b7dcc054886690ba73247db62b747c
Lance Putnam <putnam.lance@gmail.com>
2015-10-20 20:52:42 +0200

    add comments

--- allocore/examples/io/HID.cpp
+++ allocore/examples/io/HID.cpp
@@ -1,18 +1,29 @@
+/*
+Allocore Example: HID
+
+Description:
+This shows how to read data from a HID (human interface device).
+
+Author:
+Lance Putnam, June 2013
+*/
 #include <stdio.h>
 #include "allocore/system/al_Time.hpp"
 #include "allocore/io/al_HID.hpp"
 
 int main(){
 
+	// Print all recognized HID devices
 	al::HID::printDevices();
 
+	// Create a HID object
 	al::HID hid;
 
 	//int vid=0x046d, pid=0xc06c; // Logitech, USB Optical Mouse
-	//int vid=0x046d, pid=0xc218; // Logitech, Logitech RumblePad 2 USB
-	int vid=0x045e, pid=0x0040; // Microsoft 3-Button Mouse with IntelliEye(TM)
-	//int vid=0x045e, pid=0x00dd;
+	int vid=0x046d, pid=0xc218; // Logitech, Logitech RumblePad 2 USB
+	//int vid=0x045e, pid=0x0040; // Microsoft 3-Button Mouse with IntelliEye(TM)
 
+	// Attempt to open HID with given vendor and product IDs
 	if(!hid.open(vid, pid)){
 		printf("Unable to open HID device\n");
 		return -1;
@@ -22,15 +33,19 @@ int main(){
 	printf("Product:       %ls\n", hid.product().c_str());
 	printf("Serial number: %ls\n", hid.serialNumber().c_str());
 
+	// Create a do-wait loop to read data from the HID
 	unsigned char buf[256];
-	while(1){
-		int num = hid.read(buf, sizeof(buf));
-		if(num){
+	while(true){
+		// Here we just read all available bytes on the bus
+		int num;
+		while(num = hid.read(buf, sizeof(buf))){
 			printf("Data read: ");
 			for(int i=0; i<num; ++i)
 				printf("%2x ", buf[i]);
 			printf("\n");
 		}
-		al::wait(1./10);
+
+		// Wait a bit since reads are non-blocking by default
+		al::wait(1./20);
 	}
 }
