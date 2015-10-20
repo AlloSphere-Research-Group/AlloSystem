f6c3160b8c226a11077ad6b225adfcf65df7c89a
Lance Putnam <putnam.lance@gmail.com>
2015-10-20 21:49:18 +0200

    HID: add find function

--- allocore/examples/io/HID.cpp
+++ allocore/examples/io/HID.cpp
@@ -10,24 +10,31 @@ Lance Putnam, June 2013
 #include <stdio.h>
 #include "allocore/system/al_Time.hpp"
 #include "allocore/io/al_HID.hpp"
+using namespace al;
 
 int main(){
 
 	// Print all recognized HID devices
-	al::HID::printDevices();
+	HID::printDevices();
 
 	// Create a HID object
-	al::HID hid;
+	HID hid;
 
 	//int vid=0x046d, pid=0xc06c; // Logitech, USB Optical Mouse
 	int vid=0x046d, pid=0xc218; // Logitech, Logitech RumblePad 2 USB
 	//int vid=0x045e, pid=0x0040; // Microsoft 3-Button Mouse with IntelliEye(TM)
 
-	// Attempt to open HID with given vendor and product IDs
+	//* Attempt to open HID with given vendor and product IDs
 	if(!hid.open(vid, pid)){
 		printf("Unable to open HID device\n");
 		return -1;
-	}
+	}//*/
+
+	/* Or open by searching for product
+	if(!hid.open(HID::find("Mouse"))){
+		printf("Unable to open HID device\n");
+		return -1;
+	}//*/
 
 	printf("Manufacturer:  %ls\n", hid.manufacturer().c_str());
 	printf("Product:       %ls\n", hid.product().c_str());
@@ -38,7 +45,7 @@ int main(){
 	while(true){
 		// Here we just read all available bytes on the bus
 		int num;
-		while(num = hid.read(buf, sizeof(buf))){
+		while((num = hid.read(buf, sizeof(buf)))){
 			printf("Data read: ");
 			for(int i=0; i<num; ++i)
 				printf("%2x ", buf[i]);
