2e415aa178b65487b435c92f19b2995599542e5c
Kurt Kaminski <kurtoon@gmail.com>
2016-01-31 13:59:36 -0800

    MeshVBO::bind(): allocates if vertices are present. allows user to not have to call either update or allocate on a static mesh

--- allocore/src/graphics/al_MeshVBO.cpp
+++ allocore/src/graphics/al_MeshVBO.cpp
@@ -176,6 +176,10 @@ void MeshVBO::updateData(const T *src, uint32_t *bufferId, int total, int buffer
 ////////////////////////////////////////////////////////////////////////////////
 // Bind buffers and get pointers
 void MeshVBO::bind(){
+  if (vertices().size() && !mAllocated) {
+    allocate();
+  }
+
   glBindBuffer(GL_ARRAY_BUFFER, mVertId);
   glEnableClientState(GL_VERTEX_ARRAY);
   glVertexPointer(3, GL_FLOAT, mVertStride, 0);
