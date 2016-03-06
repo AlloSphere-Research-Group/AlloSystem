5784e95964a93ae36aeef793ee40f8c241bf2c93
Andres Cabrera <mantaraya36@gmail.com>
2016-03-05 16:36:54 -0800

    Moved oscpack to externals folder

--- allocore/modules/osc_module.cmake
+++ allocore/modules/osc_module.cmake
@@ -18,13 +18,17 @@ else()
   add_definitions(-DOSC_HOST_BIG_ENDIAN)
 endif()
 
+set(OSCPACK_ROOT_DIR "${CMAKE_CURRENT_SOURCE_DIR}/external/oscpack")
+
 list(APPEND ALLOCORE_SRC
-  src/protocol/oscpack/osc/OscOutboundPacketStream.cpp
-  src/protocol/oscpack/osc/OscPrintReceivedElements.cpp
-  src/protocol/oscpack/osc/OscReceivedElements.cpp
-  src/protocol/oscpack/osc/OscTypes.cpp
+  ${OSCPACK_ROOT_DIR}/osc/OscOutboundPacketStream.cpp
+  ${OSCPACK_ROOT_DIR}/osc/OscPrintReceivedElements.cpp
+  ${OSCPACK_ROOT_DIR}/osc/OscReceivedElements.cpp
+  ${OSCPACK_ROOT_DIR}/osc/OscTypes.cpp
   src/protocol/al_OSC.cpp
   src/system/al_Parameter.cpp)
+
+INCLUDE_DIRECTORIES("${CMAKE_CURRENT_SOURCE_DIR}/external")
 
 list(APPEND ALLOCORE_HEADERS ${OSC_HEADERS})
 
