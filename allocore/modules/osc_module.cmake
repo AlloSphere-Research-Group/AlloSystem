2799cefe1c4312b9d1f0ca86f5ee4f5f75877180
Andres Cabrera <mantaraya36@gmail.com>
2016-03-06 15:40:36 -0800

    Cleaner path handling for oscpath

--- allocore/modules/osc_module.cmake
+++ allocore/modules/osc_module.cmake
@@ -18,17 +18,17 @@ else()
   add_definitions(-DOSC_HOST_BIG_ENDIAN)
 endif()
 
-set(OSCPACK_ROOT_DIR "${CMAKE_CURRENT_SOURCE_DIR}/external/oscpack")
+set(OSCPACK_ROOT_DIR "${CMAKE_CURRENT_SOURCE_DIR}/external/")
 
 list(APPEND ALLOCORE_SRC
-  ${OSCPACK_ROOT_DIR}/osc/OscOutboundPacketStream.cpp
-  ${OSCPACK_ROOT_DIR}/osc/OscPrintReceivedElements.cpp
-  ${OSCPACK_ROOT_DIR}/osc/OscReceivedElements.cpp
-  ${OSCPACK_ROOT_DIR}/osc/OscTypes.cpp
+  ${OSCPACK_ROOT_DIR}/oscpack/osc/OscOutboundPacketStream.cpp
+  ${OSCPACK_ROOT_DIR}/oscpack/osc/OscPrintReceivedElements.cpp
+  ${OSCPACK_ROOT_DIR}/oscpack/osc/OscReceivedElements.cpp
+  ${OSCPACK_ROOT_DIR}/oscpack/osc/OscTypes.cpp
   src/protocol/al_OSC.cpp
   src/system/al_Parameter.cpp)
 
-INCLUDE_DIRECTORIES("${CMAKE_CURRENT_SOURCE_DIR}/external")
+INCLUDE_DIRECTORIES("${OSCPACK_ROOT_DIR}")
 
 list(APPEND ALLOCORE_HEADERS ${OSC_HEADERS})
 
