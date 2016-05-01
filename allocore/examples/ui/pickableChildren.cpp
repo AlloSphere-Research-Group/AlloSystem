2114681dfde28816ca668c0e68c1cf041287b43b
Tim Wood <fishuyo@gmail.com>
2016-05-01 00:57:16 -0700

    clean up ui examples

--- allocore/examples/ui/pickableChildren.cpp
+++ allocore/examples/ui/pickableChildren.cpp
@@ -1,9 +1,9 @@
 /*
-Allocore Example: Pickable
+Allocore Example: PickableChildren
 
 Description:
-This example demonstrates how associate a mesh with a pickable
-and interact with it via the mouse
+This example demonstrates how to write your own implementation of PickableBase
+and add children pickables and interact with them
 
 Author:
 Tim Wood, April 2016
@@ -14,17 +14,94 @@ Tim Wood, April 2016
 #include "allocore/ui/al_Pickable.hpp"
 using namespace al;
 
+
+// Make a new pickable implmentation
+struct TestPickable : PickableBase {
+
+  bool childHover;
+  
+  Vec3f selectOffset;
+  float selectDist;
+
+  TestPickable(){}
+
+  // this method is used for all intersection tests
+  double intersect(Rayd &r){
+    return r.intersectBox(pose.pos(), Vec3f(2)*scale);
+  }
+
+  TestPickable* addChild(Pose p, Vec3f scl){
+    TestPickable *pick = new TestPickable();
+    pick->pose.set(p);
+    pick->scale.set(scl);
+    children.push_back(pick);
+    return pick;
+  }
+
+  void draw(Graphics &g){
+    Mesh &m = g.mesh();
+    m.reset();
+    addWireBox(g.mesh());
+    m.primitive(g.LINES);
+    
+    if(hover) g.color(1,0,0);
+    else if(childHover) g.color(0,1,0);
+    else if(selected) g.color(0,0,1);
+    else g.color(1,1,1);
+    pushMatrix(g);
+    g.draw(m);
+    popMatrix(g);
+    drawChildren(g);
+  }
+
+  bool onPoint(Rayd &r, double t, bool child){
+    if(t > 0.0){
+      if(child){
+        childHover = true;
+        hover = false;
+      } else {
+        hover = true;
+        childHover = false;
+      }
+    } else hover = false;
+    return hover || childHover;
+  }
+
+  bool onPick(Rayd &r, double t, bool child){
+    if(t > 0.0){
+      if(child){
+        selected = false;
+        hover = false;
+      } else {
+        selectDist = t;
+        selectOffset = pose.pos() - r(t);
+        selected = true;
+      }
+    } else selected = false;
+    return selected || child;
+  }
+  
+  bool onDrag(Rayd &r, double t, bool child){
+    // if(t > 0.0){
+      if(child){
+        return true;
+      } else if(selected){
+        Vec3f newPos = r(selectDist) + selectOffset;
+        pose.pos().set(newPos);
+        return true;
+      }
+    // }
+    return false;
+  }
+};
+
+
+// App
 class MyApp : public App {
 public:
 
   bool loadedFont;
-  double t;
-  Light light;      
-  Material material; 
   
-  Mesh mesh; 
-
-  BoundingBox bb;
   TestPickable pickable;
   Font font;
 
@@ -38,20 +115,6 @@ public:
     nav().pos(0,0,10);
     navControl().useMouse(false);
 
-    // Create a red spheres mesh
-    addSphere(mesh,1);
-    mesh.translate(-3,3,0);
-    addSphere(mesh,2);
-    mesh.translate(-2.7,2.7,0);
-    addSphere(mesh,0.7);
-    mesh.translate(2.7,-2.7,0);
-
-    mesh.generateNormals();
-    mesh.color(RGB(1,0,0));
-
-    // Initialize BoundingBox
-    // pickable.set(mesh);
-    // pickable.bb.glUnitLength = 10; // ??? this behaves weird
     auto p = pickable.addChild(Pose(Vec3f(-0.5,0,0),Quatf()),0.5);
     p = p->addChild(Pose(),0.5);
     p = p->addChild(Pose(),0.5);
@@ -65,42 +128,10 @@ public:
     initWindow();
   }
 
-  void onAnimate(double dt){
-    // move light in a circle
-    t += dt;
-    // light.pos(10*cos(t),0,10*sin(t));
-  }
+  void onAnimate(double dt){}
 
   void onDraw(Graphics& g){
-
-    pickable.draw(g, &pickable); 
-    //pushMatrix(g);
-    
-    // // draw lit mesh
-    // g.lighting(true);
-    // light();
-    // material();
-    // g.draw(mesh);
-
-    // // draw boundingbox
-    // if(pickable.hover || pickable.selected){
-    //   g.lighting(false);
-    //   if(pickable.selected) g.color(0,1,1);
-    //   else g.color(1,1,1);
-    //   g.draw(pickable.bb.mesh);
-    //   g.draw(pickable.bb.tics);
-
-    //   if(loadedFont){
-    //     g.blendAdd();
-    //     g.color(1,1,1);
-    //     pickable.bb.drawLabels(g, font, nav(), Pose(), 1);
-    //     g.blendOff();
-    //   }
-    // }
-
-    // pickable.popMatrix(g);
-
-
+    pickable.draw(g); 
   }
 
   virtual void onMouseMove(const ViewpointWindow& w, const Mouse& m){
