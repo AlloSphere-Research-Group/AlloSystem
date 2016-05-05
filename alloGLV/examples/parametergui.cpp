324707fbfae744d556ea0420a2ccb63c09b610cc
Andres Cabrera <mantaraya36@gmail.com>
2016-05-04 23:30:19 -0700

    Added MIDI control to GLV parameter example

--- alloGLV/examples/parametergui.cpp
+++ alloGLV/examples/parametergui.cpp
@@ -2,6 +2,7 @@
 #include "allocore/io/al_App.hpp"
 #include "allocore/graphics/al_Shapes.hpp"
 #include "alloGLV/al_ParameterGUI.hpp"
+#include "allocore/ui/al_ParameterMIDI.hpp"
 
 using namespace al;
 
@@ -18,13 +19,22 @@ public:
 		initWindow();
 		mParameterGUI.setParentApp(this);
 		mParameterGUI << new glv::Label("Parameter GUI example");
+		// Add parameters to GUI
 		mParameterGUI << x << y << z ;
 		nav() = Vec3d(0, 0, 2);
+		// Add parameters to OSC server
 		mServer << x << y << z;
 		mServer.print();
 
 		addSphere(graphics().mesh(), 0.1);
 		graphics().mesh().generateNormals();
+
+		// Connect MIDI CC #1 on channel 1 to parameter x
+		mParameterMIDI.connectControl(x, 1, 1);
+
+		// Broadcast parameter changes to localhost por 9011
+		mServer.addListener("127.0.0.1", 9011);
+
 	}
 
 	virtual void onDraw(Graphics &g) {
@@ -39,6 +49,8 @@ public:
 private:
 	ParameterGUI mParameterGUI;
 	ParameterServer mServer;
+	ParameterMIDI mParameterMIDI;
+
 	Light light;
 };
 
