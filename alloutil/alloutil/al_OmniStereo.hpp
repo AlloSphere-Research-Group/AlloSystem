96b92191d37eb0419aa3e79e6f7b94952e1dda08
Keehong Youn <younkeehong@gmail.com>
2016-06-16 23:51:54 -0700

    OmniStereo: getters for near & far

--- alloutil/alloutil/al_OmniStereo.hpp
+++ alloutil/alloutil/al_OmniStereo.hpp
@@ -270,7 +270,9 @@ class OmniStereo {
   // the radius of the sphere in OpenGL units, default to 1e10, when we can ignore the effect.
   float sphereRadius() const { return mSphereRadius; }
   float sphereRadius(float value) { return mSphereRadius = value; }
-
+  float near() const { return mNear; }
+  float far() const { return mFar; }
+  
   // create GPU resources:
   void onCreate();
   void onDestroy();
