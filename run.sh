53bd416e02b35c1f940be83d73c672e89fe21e4f
Lance Putnam <putnam.lance@gmail.com>
2016-09-07 18:18:54 +0100

    run.sh: Pass all arguments to make

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
-#TODO: hand other flags to make?
-make $1 RUN_DIRS=$(dirname "$1")/
+if [ $# -eq 0 ]; then
+	echo "Usage: ./$(basename "$0") [SOURCE FILE]"
+	exit
+fi
+
+make $* RUN_DIRS=$(dirname "$1")/
