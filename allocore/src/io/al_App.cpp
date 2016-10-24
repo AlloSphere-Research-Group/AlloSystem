32c58ba2d5ef8085abeb5b7bd9b0940b3e68bb13
Lance Putnam <putnam.lance@gmail.com>
2016-10-24 23:32:31 +0100

    ViewpointWindow: add user-defined draw call list

--- allocore/src/io/al_App.cpp
+++ allocore/src/io/al_App.cpp
@@ -150,12 +150,14 @@ bool SceneWindowHandler::onFrame(){
 	struct DrawFunc : public Drawable {
 		App& app;
 		Viewpoint& vp;
+		ViewpointWindow& win;
 
-		DrawFunc(App& a, Viewpoint& v)
-		:	app(a), vp(v){}
+		DrawFunc(App& a, Viewpoint& v, ViewpointWindow& w)
+		:	app(a), vp(v), win(w){}
 
 		void onDraw(Graphics& g){
 			g.pushMatrix(g.MODELVIEW);
+			for(auto& drawCall : win.drawCalls()) drawCall();
 			app.onDraw(g,vp);
 			g.popMatrix(g.MODELVIEW);
 		}
@@ -217,7 +219,7 @@ bool SceneWindowHandler::onFrame(){
 			}
 		}
 
-		DrawFunc drawFunc(app, vp);
+		DrawFunc drawFunc(app, vp, win);
 		stereo.draw(g, lens, vp.worldTransform(), vp.viewport(), drawFunc, doClear);
 		stereo.clearColor(defaultClearColor);
 	}
