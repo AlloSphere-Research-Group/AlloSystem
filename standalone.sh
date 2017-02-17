d9ece3b15659b38f3ddbbdfbaf9fa1b116f2d7bb
Lance Putnam <putnam.lance@gmail.com>
2017-02-17 15:57:52 +0000

    Improved inclusion of library dependencies (MinGW)

--- standalone.sh
+++ standalone.sh
@@ -9,25 +9,37 @@ files_exist(){
 	ls -u "$@" >/dev/null 2>&1;
 }
 
-# MSYS
-if files_exist /msys.bat; then
-	LIBDIR="/usr/local/bin"
-	LIBNAMES="`ls $LIBDIR`"
-	for v in $LIBNAMES; do LIBS="$LIBS $LIBDIR/$v"; done
-	LIBS="$LIBS /mingw/bin/libstdc++-6.dll /mingw/bin/libgcc_s_dw2-1.dll"
-	#echo $LIBS
-else
-	echo "Platform not supported."
-	exit
-fi
+# Build executable first as we may need it to check for library dependencies
+./run.sh $1 AUTORUN=0
 
 BUILDDIR="build"
 EXENAME=$(basename "$1" | cut -d. -f1)
 EXEDIR="$BUILDDIR/$EXENAME"
+LIBS=
+
+# MSYS
+if files_exist /msys.bat; then
+
+	LIBNAMES="`objdump -p $BUILDDIR/bin/${EXENAME}.exe | grep "DLL Name"`"
+	LIBNAMES="${LIBNAMES//DLL Name: /}"
+	#echo $LIBNAMES
+	LIBDIRS="/mingw/bin/ /usr/local/bin/"
+
+	for dir in $LIBDIRS; do
+		for lib in $LIBNAMES; do
+			DIR_LIB=$dir$lib
+			if files_exist $DIR_LIB; then LIBS="$LIBS $DIR_LIB"; fi
+		done
+	done
+	#echo $LIBS
+
+else
+	echo "Error: Could not build standalone. Platform not supported."
+	exit
+fi
 
 mkdir -p $EXEDIR
 cp -u $LIBS $EXEDIR
-./run.sh $1 AUTORUN=0
 mv $BUILDDIR/bin/$EXENAME $EXEDIR
 
 echo "Created standalone in $EXEDIR/"
