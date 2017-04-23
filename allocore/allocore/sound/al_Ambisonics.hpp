9013811b70993a3785c4ad5a667bcafafde82f7f
Lance Putnam <putnam.lance@gmail.com>
2017-04-23 15:11:50 +0100

    Ambisonics: merge changes in devel branch

--- allocore/allocore/sound/al_Ambisonics.hpp
+++ allocore/allocore/sound/al_Ambisonics.hpp
@@ -116,12 +116,11 @@ public:
 	/// Get Ambisonic channel weights
 	const float * weights() const { return mWeights; }
 
-	/// Returns total number of Ambisonic domain channels
+	/// Returns total number of Ambisonic domain (B-format) channels
 	int channels() const { return mChannels; }
 
 	/// Set the order
 	void order(int order);
-
 
 	/// Called whenever the number of Ambisonic channels changes
 	virtual void onChannelsChange(){}
@@ -144,6 +143,9 @@ public:
 	static int orderToChannels(int dim, int order);
 	static int orderToChannelsH(int orderH);
 	static int orderToChannelsV(int orderV);
+
+	static int channelsToOrder(int channels);
+	static int channelsToDimensions(int channels);
 
 protected:
 	int mDim;			// dimensions - 2d or 3d
@@ -175,7 +177,7 @@ public:
 	/// @param[out] dec				output time domain buffers (non-interleaved)
 	/// @param[in ] enc				input Ambisonic domain buffers (non-interleaved)
 	/// @param[in ] numDecFrames	number of frames in time domain buffers
-	void decode(float * dec, const float * enc, int numDecFrames) const;
+	virtual void decode(float * dec, const float * enc, int numDecFrames) const;
 
 	float decodeWeight(int speaker, int channel) const {
 		return mWeights[channel] * mDecodeMatrix[speaker * channels() + channel];
@@ -326,6 +328,44 @@ inline int AmbiBase::orderToChannels(int dim, int order){
 
 inline int AmbiBase::orderToChannelsH(int orderH){ return (orderH << 1) + 1; }
 inline int AmbiBase::orderToChannelsV(int orderV){ return orderV * orderV; }
+
+inline int AmbiBase::channelsToOrder(int channels)
+{
+	int order = -1;
+	switch(channels) {
+	case 3:
+	case 4:
+		order = 1;
+		break;
+	case 9:
+		order = 2;
+		break;
+	case 16:
+		order = 3;
+		break;
+	default:
+		order = -1;
+	}
+	return order;
+}
+
+inline int AmbiBase::channelsToDimensions(int channels)
+{
+	int dim = 3;
+	switch(channels) {
+	case 3:
+		dim = 2;
+		break;
+	case 4:
+	case 9:
+	case 16:
+		dim = 3;
+		break;
+	default:
+		dim = -1;
+	}
+	return dim;
+}
 
 template<typename T>
 void AmbiBase::resize(T *& a, int n){
