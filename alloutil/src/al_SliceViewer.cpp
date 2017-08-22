eae831ac38670326a80e61c03ed1f5b1353b5f39
Kon Hyong Kim <konhyong@gmail.com>
2017-08-21 18:00:15 -0700

    removed one more cout msg for sliceviewer

--- alloutil/src/al_SliceViewer.cpp
+++ alloutil/src/al_SliceViewer.cpp
@@ -104,7 +104,7 @@ bool SliceViewer::parallelLinespace(Vec3f p0, Vec3f p1, Vec3f p2, Vec3f p3, std:
   if ((p0-p1).mag()/(p0-p2).mag() == aDirection/oDirection){ 
     n = oDirection; 
   }
-  std::cout.flush();
+  // std::cout.flush();
   // std::cout << "t = " << t << "\n";
   if (t == -1.0){
     list = linspace(p0,p1,n);
@@ -173,7 +173,7 @@ Array SliceViewer::slice(Vec3f planeCenter, Vec3f planeNormal, std::vector<Vec3f
     Vec2f *p2D = new Vec2f[P.size()];
     p2D[0] = Vec2f(0,0);
     float x = sqrt(pow(P[1].x-P[0].x,2.0)+pow(P[1].y-P[0].y,2.0)+pow(P[1].z-P[0].z,2.0));
-    std::cout << x << "\n";
+    // std::cout << x << "\n";
     p2D[1] = Vec2f(x,0);
     if(P.size() == 2){
       //super easy, it's just a line :)
