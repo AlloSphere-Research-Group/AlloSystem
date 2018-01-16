d9c5fc2432533b622a6226e71799e175f68121ed
Lance Putnam <putnam.lance@gmail.com>
2018-01-16 00:01:26 +0000

    File: replace APR with native backend

--- allocore/src/io/al_File.cpp
+++ allocore/src/io/al_File.cpp
@@ -1,38 +1,364 @@
 #include <cstring>
-#include <sys/types.h>
-#include <sys/stat.h>
+#include <fstream> // ifstream, ofstream
 #include <memory>
 #include <regex>
 #include "allocore/io/al_File.hpp"
 #include "allocore/system/al_Config.h"
-#include <stdlib.h> // realpath (POSIX), _fullpath (Windows)
-#ifdef AL_WINDOWS
-	#define WIN32_LEAN_AND_MEAN
-	#define VC_EXTRALEAN
-	#include <windows.h> // TCHAR, LPCTSTR
-	#include <direct.h> // _getcwd
-	#define platform_getcwd _getcwd
-	#ifndef PATH_MAX
-	#define PATH_MAX 260
-	#endif
-#else
-	#include <unistd.h> // getcwd (POSIX)
-	#define platform_getcwd getcwd
-#endif
 
-namespace al{
+using namespace al;
 
-//void path2dir(char* dst, const char* src) {
-//    char* s;
-//	snprintf(dst, AL_PATH_MAX, "%s", src);
-//    s = strrchr(dst, '/');
-//    if (s)
-//        s[1] = '\0';
-//    else
-//        dst[0] = '\0';
-//}
+#if defined(AL_OSX) || defined(AL_LINUX)
+#include <dirent.h>
+#include <unistd.h> // rmdir, getcwd
+#include <stdlib.h> // realpath
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/param.h> // DEV_BSIZE
+#include <errno.h>
 
-void File::dtor(){
+al_sec File::modified() const {
+	struct stat s;
+	if(::stat(path().c_str(), &s) == 0){
+		//const auto& t = s.st_mtim;
+		//return t.tv_sec + t.tv_usec/1e9;
+		return s.st_mtime;
+	}
+	return 0.;
+}
+
+al_sec File::accessed() const {
+	struct stat s;
+	if(::stat(path().c_str(), &s) == 0){
+		return s.st_atime;
+	}
+	return 0.;
+}
+
+al_sec File::created() const {
+	// Not supported on POSIX systems (st_ctime is last status change):
+	// https://lwn.net/Articles/397442/
+	return 0.;
+}
+
+size_t File::sizeFile() const {
+	struct stat s;
+	if(::stat(path().c_str(), &s) == 0){
+		return s.st_size;
+	}
+	return 0;
+}
+
+size_t File::storage() const {
+	struct stat s;
+	if(::stat(path().c_str(), &s) == 0){
+		return s.st_blocks*DEV_BSIZE;
+	}
+	return 0;
+}
+
+/*static*/ std::string File::absolutePath(const std::string& path){
+	char temp[PATH_MAX];
+	char * result = realpath(path.c_str(), temp);
+	return result ? result : "";
+}
+
+
+class Dir::Impl{
+public:
+	DIR * mDir = NULL;
+	struct dirent * mEnt = NULL;
+	std::string mPath;
+
+	bool open(const std::string& path){
+		if((mDir = opendir(path.c_str())) != NULL){
+			mPath = path;
+			return true;
+		}
+		return false;
+	}
+
+	bool close(){
+		if(mDir && closedir(mDir) == 0){
+			mPath = "";
+			return true;
+		}
+		return false;
+	}
+
+	bool read(FileInfo& fileInfo){
+		if((mEnt = readdir(mDir)) != NULL){
+
+			fileInfo.name(mEnt->d_name);
+
+			#if defined(_DIRENT_HAVE_D_TYPE) || defined(AL_OSX)
+			switch(mEnt->d_type){
+				case DT_REG: fileInfo.type(FileInfo::REG); break;
+				case DT_DIR: fileInfo.type(FileInfo::DIR); break;
+				case DT_CHR: fileInfo.type(FileInfo::CHR); break;
+				case DT_BLK: fileInfo.type(FileInfo::BLOCK); break;
+				case DT_LNK: fileInfo.type(FileInfo::LINK); break;
+				case DT_SOCK:fileInfo.type(FileInfo::SOCKET); break;
+				case DT_FIFO: fileInfo.type(FileInfo::PIPE); break;
+				default: fileInfo.type(FileInfo::UNKNOWN); break;
+			}
+			#else // fallback that uses much slower stat command
+			struct stat s;
+			if(::stat((mPath + "/" + fileInfo.name()).c_str(), &s) == 0){
+				switch(s.st_mode & S_IFMT){
+					case S_IFREG: fileInfo.type(FileInfo::REG); break;
+					case S_IFDIR: fileInfo.type(FileInfo::DIR); break;
+					case S_IFCHR: fileInfo.type(FileInfo::CHR); break;
+					case S_IFBLK: fileInfo.type(FileInfo::BLOCK); break;
+					case S_IFLNK: fileInfo.type(FileInfo::LINK); break;
+					case S_IFSOCK:fileInfo.type(FileInfo::SOCKET); break;
+					case S_IFIFO: fileInfo.type(FileInfo::PIPE); break;
+					default: fileInfo.type(FileInfo::UNKNOWN); break;
+				}
+			}
+			#endif
+
+			return true;
+		}
+		return false;
+	}
+
+	bool rewind(){
+		if(mDir) rewinddir(mDir);
+		return true;
+	}
+};
+
+/*static*/ bool Dir::make(const std::string& path, bool recursive){
+	auto mode = S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH;
+	if(recursive){
+		// Create all intermediate dirs up to last one
+		for(unsigned i=0; i<path.size(); ++i){
+			if(path[i] == '/'){
+				if(mkdir(path.substr(0, i+1).c_str(), mode) != 0){
+					if(errno != EEXIST) return false;
+				}
+			}
+		}
+	}
+
+	return mkdir(path.c_str(), mode) == 0;
+}
+
+/*static*/ bool Dir::remove(const std::string& path){
+	return rmdir(path.c_str()) == 0;
+}
+
+/*static*/ std::string Dir::cwd(){
+	char buf[AL_PATH_MAX];
+	if(getcwd(buf, sizeof(buf))){
+		return std::string(buf);
+	}
+	return "";
+}
+
+// end POSIX
+
+
+#elif defined(AL_WINDOWS)
+#define WIN32_LEAN_AND_MEAN
+#define VC_EXTRALEAN
+#include <windows.h>
+#include <direct.h> // _getcwd
+
+struct ReadOnlyFileHandle{
+	HANDLE mHandle = INVALID_HANDLE_VALUE;
+	ReadOnlyFileHandle(const std::string& path){
+		mHandle = CreateFile(path.c_str(), GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, 0, NULL);
+	}
+	~ReadOnlyFileHandle(){
+		if(opened()) CloseHandle(mHandle);
+	}
+	bool opened() const { return mHandle != INVALID_HANDLE_VALUE; }
+};
+
+unsigned long long toUInt64(DWORD lo, DWORD hi){
+	ULARGE_INTEGER t;
+	t.LowPart = lo;
+	t.HighPart = hi;
+	return t.QuadPart;
+}
+
+al_sec fileTimeToSec(const FILETIME& fileTime){
+	// File times in 100-nanosecond intervals that have elapsed since
+	// 12:00 A.M. January 1, 1601 Coordinated Universal Time (UTC).
+	// Need to return seconds since Jan 1, 1970. The difference is 134774 days.
+	// https://msdn.microsoft.com/en-us/library/ms724290(v=vs.85).aspx
+	return toUInt64(fileTime.dwLowDateTime, fileTime.dwHighDateTime) * 1e-7 - 11644473600.;
+}
+
+al_sec File::modified() const {
+	ReadOnlyFileHandle fh(path());
+    if(fh.opened()){
+		FILETIME ftCreate, ftAccess, ftWrite;
+		if(GetFileTime(fh.mHandle, &ftCreate, &ftAccess, &ftWrite)){
+			return fileTimeToSec(ftWrite);
+		}
+	}
+	return 0.;
+}
+
+al_sec File::accessed() const {
+	ReadOnlyFileHandle fh(path());
+    if(fh.opened()){
+		FILETIME ftCreate, ftAccess, ftWrite;
+		if(GetFileTime(fh.mHandle, &ftCreate, &ftAccess, &ftWrite)){
+			return fileTimeToSec(ftAccess);
+		}
+	}
+	return 0.;
+}
+
+al_sec File::created() const {
+	ReadOnlyFileHandle fh(path());
+    if(fh.opened()){
+		FILETIME ftCreate, ftAccess, ftWrite;
+		if(GetFileTime(fh.mHandle, &ftCreate, &ftAccess, &ftWrite)){
+			return fileTimeToSec(ftCreate);
+		}
+	}
+	return 0.;
+}
+
+size_t File::sizeFile() const {
+	ReadOnlyFileHandle fh(path());
+    if(fh.opened()){
+		/*BY_HANDLE_FILE_INFORMATION info;
+		if(GetFileInformationByHandle(fh.mHandle, &info)){
+			return toUInt64(info.nFileSizeLow, info.nFileSizeHigh);
+		}*/
+		return GetFileSize(fh.mHandle, NULL);
+	}
+	return 0;
+}
+
+size_t File::storage() const {
+	return GetCompressedFileSize(path().c_str(), NULL);
+}
+
+/*static*/ std::string File::absolutePath(const std::string& path){
+	TCHAR dirPart[4096];
+	TCHAR ** filePart={NULL};
+	GetFullPathName((LPCTSTR)path.c_str(), sizeof(dirPart), dirPart, filePart);
+	std::string result = (char *)dirPart;
+	if(filePart != NULL && *filePart != 0){
+		result += (char *)*filePart;
+	}
+	return result;
+}
+
+
+class Dir::Impl{
+public:
+
+	bool open(const std::string& path){
+		mPath = path;
+		return rewind();
+	}
+
+	bool close(){
+		if(opened()){
+			return FindClose(mHandle) != 0;
+		}
+		return false;
+	}
+
+	bool read(FileInfo& fileInfo){
+		if(opened()){
+			if(mFindNextFile){
+				if(FindNextFile(mHandle, &mSearchData) == 0){
+					return false;
+				}
+			}
+
+			auto attr = mSearchData.dwFileAttributes;
+			//printf("dwFileAttributes: %d\n", attr);
+			if(attr & (FILE_ATTRIBUTE_NORMAL | FILE_ATTRIBUTE_ARCHIVE)){
+				fileInfo.type(FileInfo::REG);
+			}
+			else if(attr & FILE_ATTRIBUTE_DIRECTORY){
+				fileInfo.type(FileInfo::DIR);
+			}
+			else if(attr & FILE_ATTRIBUTE_REPARSE_POINT){
+				fileInfo.type(FileInfo::LINK);
+			}
+			else{
+				fileInfo.type(FileInfo::UNKNOWN);
+			}
+
+			fileInfo.name(mSearchData.cFileName);
+
+			mFindNextFile = true;
+			return true;
+		}
+		return false;
+	}
+
+	bool rewind(){
+		auto searchPath = mPath + "\\*"; // must append wildcard to list all entries
+		mHandle = FindFirstFile(searchPath.c_str(), &mSearchData);
+		mFindNextFile = false;
+		return opened();
+	}
+
+	bool opened() const { return mHandle != INVALID_HANDLE_VALUE; }
+
+private:
+	std::string mPath;
+	HANDLE mHandle = INVALID_HANDLE_VALUE;
+	WIN32_FIND_DATA mSearchData;
+	bool mFindNextFile = false;
+};
+
+/*static*/ bool Dir::make(const std::string& path, bool recursive){
+	if(recursive){
+		// Create all intermediate dirs up to last one
+		for(unsigned i=0; i<path.size(); ++i){
+			if(path[i] == '/'){
+				if(CreateDirectory(path.substr(0, i+1).c_str(), NULL) != 0){
+					if(GetLastError() != ERROR_ALREADY_EXISTS) return false;
+				}
+			}
+		}
+	}
+
+	return CreateDirectory(path.c_str(), NULL) != 0;
+}
+
+/*static*/ bool Dir::remove(const std::string& path){
+	if(RemoveDirectory(path.c_str()) != 0){
+		return true;
+	}
+	return false;
+}
+
+/*static*/ std::string Dir::cwd(){
+	char buf[AL_PATH_MAX];
+	if(_getcwd(buf, sizeof(buf))){
+		return std::string(buf);
+	}
+	return "";
+}
+
+#endif // End Windows
+
+
+
+// Platform independent code
+
+File::File(const std::string& path, const std::string& mode, bool open_)
+:	mPath(path), mMode(mode), mContent(0), mSizeBytes(0), mFP(0)
+{	if(open_) open(); }
+
+File::File(const FilePath& path, const std::string& mode, bool open_)
+:	File(path.filepath(), mode, open_)
+{}
+
+File::~File(){
 	close();
 	freeContent();
 }
@@ -153,8 +479,6 @@ bool File::copy(const std::string &srcPath, const std::string &dstPath, unsigned
 	return writeComplete;
 }
 
-
-
 std::string File::conformDirectory(const std::string& path){
 	if(path[0]){
 		if(AL_FILE_DELIMITER != path[path.size()-1]){
@@ -181,23 +505,6 @@ std::string File::conformPathToOS(const std::string& path){
 		return conformDirectory(res);
 	}
 	return res;
-}
-
-std::string File::absolutePath(const std::string& path){
-#ifdef AL_WINDOWS
-	TCHAR dirPart[4096];
-	TCHAR ** filePart={NULL};
-	GetFullPathName((LPCTSTR)path.c_str(), sizeof(dirPart), dirPart, filePart);
-	std::string result = (char *)dirPart;
-	if(filePart != NULL && *filePart != 0){
-		result += (char *)*filePart;
-	}
-	return result;
-#else
-	char temp[PATH_MAX];
-	char * result = realpath(path.c_str(), temp);
-	return result ? result : "";
-#endif
 }
 
 std::string File::baseName(const std::string& path, const std::string& suffix){
@@ -269,6 +576,51 @@ bool File::searchBack(std::string& path, int maxDepth){
 }
 
 
+Dir::Dir()
+:	mImpl(new Impl)
+{}
+
+Dir::Dir(const std::string& dirToOpen)
+:	Dir()
+{
+	open(dirToOpen);
+}
+
+Dir::~Dir(){
+	close();
+	delete mImpl;
+}
+
+bool Dir::open(const std::string& dirPath){ return mImpl->open(dirPath); }
+bool Dir::close(){ return mImpl->close(); }
+bool Dir::rewind(){ return mImpl->rewind(); }
+bool Dir::read(){ return mImpl->read(mEntry); }
+
+/*static*/ bool Dir::removeRecursively(const std::string& path){
+	Dir dir(path);
+	bool ok = true;
+	while(dir.read()) {
+		FileInfo entryInfo = dir.entry();
+		if (entryInfo.type() == FileInfo::DIR &&
+				entryInfo.name() != ".." &&
+				entryInfo.name() != "." ) {
+			if (!Dir::remove(path + "/" + entryInfo.name())) {
+				ok = false;
+				break;
+			}
+		} else if (entryInfo.type() == FileInfo::REG) {
+			if (!File::remove(path + "/" + entryInfo.name())) {
+				ok = false;
+				break;
+			}
+		}
+	}
+	if (!Dir::remove(path)) {
+		ok = false;
+	}
+	return ok;
+}
+
 
 
 FilePath::FilePath(const std::string& fullpath) {
@@ -326,9 +678,9 @@ void SearchPaths::addAppPaths(int argc, char * const argv[], bool recursive) {
 }
 
 void SearchPaths::addAppPaths(bool recursive) {
-	char cwd[4096];
-	if(platform_getcwd(cwd, sizeof(cwd))){
-		mAppPath = std::string(cwd) + "/";
+	auto cwd = Dir::cwd();
+	if(!cwd.empty()){
+		mAppPath = cwd + AL_FILE_DELIMITER_STR;
 		addSearchPath(mAppPath, recursive);
 	}
 }
@@ -410,7 +762,3 @@ void SearchPaths::print() const {
 		printf("SearchPath %p path: %s (recursive: %d)\n", this, sp.first.c_str(), sp.second);
 	}
 }
-
-
-} // al::
-
