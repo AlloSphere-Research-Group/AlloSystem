ec25e0b82b782d160a6f2cab7f9c70ecbd8e9c14
Lance Putnam <putnam.lance@gmail.com>
2018-03-01 14:53:22 +0000

    Mat: specialized 3x3 invert

--- allocore/allocore/math/al_Mat.hpp
+++ allocore/allocore/math/al_Mat.hpp
@@ -613,39 +613,11 @@ inline Vec<N,U> operator* (const Vec<N,U>& vRow, const Mat<N,T>& m){
 	Vec<N,U> r; return Mat<N,T>::multiply(r, vRow,m);
 }
 
-
-/// Get determinant
-///
-/// @ingroup allocore
-template <class T>
-T determinant(const Mat<1,T>& m){
-	return m(0,0);
-}
-
-/// Get determinant
-///
-/// @ingroup allocore
-template <class T>
-T determinant(const Mat<2,T>& m){
-	return m(0,0)*m(1,1) - m(0,1)*m(1,0);
-}
-
-/// Get determinant
-///
-/// @ingroup allocore
-template <class T>
-T determinant(const Mat<3,T>& m){
-	return
-		m(0,0)*(m(1,1)*m(2,2) - m(1,2)*m(2,1)) +
-		m(0,1)*(m(1,2)*m(2,0) - m(1,0)*m(2,2)) +
-		m(0,2)*(m(1,0)*m(2,1) - m(1,1)*m(2,0));
-}
-
 /// Get determinant
 
 /// This computes the determinant using cofactor (or Laplace) expansion.
 /// The algorithm operates by recursively computing determinants of submatrices.
-///
+/// For small matrices, optimized versions are used.
 /// @ingroup allocore
 template<int N, class T>
 T determinant(const Mat<N,T>& m){
@@ -659,37 +631,26 @@ T determinant(const Mat<N,T>& m){
 	return res;
 }
 
-/// Invert matrix, returns whether matrix was able to be inverted
-///
-/// @ingroup allocore
 template <class T>
-bool invert(Mat<1,T>& m){
-	T det = determinant(m);
-	if(det != 0){
-		m(0,0) /= det;
-		return true;
-	}
-	return false;
+T determinant(const Mat<1,T>& m){
+	return m(0,0);
+}
+
+template <class T>
+T determinant(const Mat<2,T>& m){
+	return m(0,0)*m(1,1) - m(0,1)*m(1,0);
+}
+
+template <class T>
+T determinant(const Mat<3,T>& m){
+	return
+		m(0,0)*(m(1,1)*m(2,2) - m(1,2)*m(2,1)) +
+		m(0,1)*(m(1,2)*m(2,0) - m(1,0)*m(2,2)) +
+		m(0,2)*(m(1,0)*m(2,1) - m(1,1)*m(2,0));
 }
 
 /// Invert matrix, returns whether matrix was able to be inverted
-///
-/// @ingroup allocore
-template <class T>
-bool invert(Mat<2,T>& m){
-	T det = determinant(m);
-	if(det != 0){
-		m = Mat<2,T>(
-			 m(1,1),-m(0,1),
-			-m(1,0), m(0,0)
-		) /= det;
-		return true;
-	}
-	return false;
-}
-
-/// Invert matrix, returns whether matrix was able to be inverted
-///
+/// For small matrices, optimized versions are used.
 /// @ingroup allocore
 template<int N, class T>
 bool invert(Mat<N,T>& m){
@@ -708,6 +669,46 @@ bool invert(Mat<N,T>& m){
 		return true;
 	}
 
+	return false;
+}
+
+template <class T>
+bool invert(Mat<1,T>& m){
+	T det = determinant(m);
+	if(det != 0){
+		m(0,0) /= det;
+		return true;
+	}
+	return false;
+}
+
+template <class T>
+bool invert(Mat<2,T>& m){
+	T det = determinant(m);
+	if(det != T(0)){
+		m = Mat<2,T>(
+			 m(1,1),-m(0,1),
+			-m(1,0), m(0,0)
+		) *= T(1)/det;
+		return true;
+	}
+	return false;
+}
+
+template <class T>
+bool invert(Mat<3,T>& m){
+	auto nx = cross(m.col(1), m.col(2));
+	auto ny = cross(m.col(2), m.col(0));
+	auto nz = cross(m.col(0), m.col(1));
+	auto det= m(0,0)*nx.x + m(1,0)*ny.x + m(2,0)*nz.x;
+	if(det != T(0)){
+		m.set(
+			nx.x, nx.y, nx.z,
+			ny.x, ny.y, ny.z,
+			nz.x, nz.y, nz.z
+		) *= T(1)/det;
+		return true;
+	}
 	return false;
 }
 
