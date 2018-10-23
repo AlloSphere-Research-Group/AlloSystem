9213ac8e811be87606723b17fe29bd978ad9f62c
Lance Putnam <putnam.lance@gmail.com>
2018-10-23 23:06:21 +0100

    Add Screenshot class
    This is meant to be a quick and dirty way to get screen pixels to disk. Image writing is self-contained avoiding dependency on (big) image libs like FreeImage.

---
+++ allocore/allocore/graphics/al_Screenshot.hpp
@@ -0,0 +1,82 @@
+#ifndef INCLUDE_AL_GRAPHICS_SCREENSHOT_HPP
+#define INCLUDE_AL_GRAPHICS_SCREENSHOT_HPP
+
+/*	Allocore --
+	Multimedia / virtual environment application class library
+
+	Copyright (C) 2009. AlloSphere Research Group, Media Arts & Technology, UCSB.
+	Copyright (C) 2012. The Regents of the University of California.
+	All rights reserved.
+
+	Redistribution and use in source and binary forms, with or without
+	modification, are permitted provided that the following conditions are met:
+
+		Redistributions of source code must retain the above copyright notice,
+		this list of conditions and the following disclaimer.
+
+		Redistributions in binary form must reproduce the above copyright
+		notice, this list of conditions and the following disclaimer in the
+		documentation and/or other materials provided with the distribution.
+
+		Neither the name of the University of California nor the names of its
+		contributors may be used to endorse or promote products derived from
+		this software without specific prior written permission.
+
+	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+	AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+	IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+	ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+	LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+	CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+	SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+	INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+	CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+	ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+	POSSIBILITY OF SUCH DAMAGE.
+
+
+	File description:
+	Screenshot utility (with minimal dependencies)
+
+	File author(s):
+	Lance Putnam, 2018, putnam.lance@gmail.com
+*/
+
+#include <string>
+
+namespace al{
+
+/// Save screen pixels to an image file
+class Screenshot{
+public:
+
+	/// Set directory for output file (must include trailing slash)
+	Screenshot& path(const std::string& v){ mPath=v; return *this; }
+
+	/// Set format of image file
+	//Screenshot& imageFormat(const std::string& ext){ mExt=ext; return *this; }
+
+	/// Save screen pixels to an image file
+
+	/// @param[in] w			width of capture, in pixels
+	/// @param[in] h			height of capture, in pixels
+	/// @param[in] filePath		if not empty, the file path to the saved image
+	bool save(unsigned w, unsigned h, const std::string& filePath="");
+
+	/// Save screen pixels to an image file
+
+	/// @param[in] rect			dimensions of capture;
+	///							any object with width() and height() member functions
+	/// @param[in] filePath		if not empty, the file path to the saved image
+	template <class Rect>
+	bool save(const Rect& rect, const std::string& path=""){
+		return save(rect.width(), rect.height(), path);
+	}
+
+private:
+	std::string mExt = "tga";
+	std::string mPath = "";
+};
+
+} // al::
+#endif
---
+++ allocore/src/graphics/al_Screenshot.cpp
@@ -0,0 +1,69 @@
+#include <fstream>
+#include <vector>
+#include "allocore/system/al_Time.hpp"
+#include "allocore/graphics/al_OpenGL.hpp"
+#include "allocore/graphics/al_Screenshot.hpp"
+
+namespace al{
+
+bool Screenshot::save(unsigned w, unsigned h, const std::string& filePath){
+	std::ofstream fs;
+	auto derivedFilePath = filePath;
+	if(derivedFilePath.empty()){
+		derivedFilePath = mPath + "screenshot_" + timecodeNow("D_HMS") + "." + mExt;
+	}
+
+	fs.open(derivedFilePath);
+
+	if(!fs.is_open()) return false;
+
+	std::vector<unsigned char> pix(w*h*3, 0);
+	glReadPixels(0,0, w,h, GL_RGB, GL_UNSIGNED_BYTE, (GLvoid*)&pix[0]);
+	//printf("%d %d %d\n", pix[0], pix[1], pix[2]);
+
+	auto lsB = [](unsigned x){ return (unsigned char)(x&255); };
+	auto msB = [](unsigned x){ return (unsigned char)((x>>8)&255); };
+
+	// TGA (simplest format, widely supported)
+	// http://www.paulbourke.net/dataformats/tga/
+	unsigned char hdr[18] = {
+		0,			// image ID length
+		0,			// color map
+		2,			// uncompressed true-color
+		0,0,0,0,0,	// color map
+		0,0, 0,0,	// image origin
+		lsB(w), msB(w), lsB(h), msB(h), // width,height in pixels
+		24,			// bits/pixel
+		0			// image descriptor
+	};
+	fs.write((char*)hdr, sizeof(hdr));
+	// Annoyingly, TGA is little endian, so we have to write BGR
+	for(unsigned i=0; i<pix.size(); i+=3) std::swap(pix[i], pix[i+2]);
+	fs.write((char*)&pix[0], pix.size());
+
+	// Other potential formats to support in future...
+
+	// PNG (really complicated)
+	//unsigned char hdr[] = {137,'P','N','G',13,10,26,10};
+
+	// PBM (simple, but ASCII format so big files)
+
+	/* TIFF (a bit complicated)
+	// http://paulbourke.net/dataformats/tiff/
+	// http://www.fileformat.info/format/tiff/corion.htm
+	fs.open(RUN_MAIN_SOURCE_PATH "screenshot.tif.txt");
+	unsigned char hdr[] = {
+		'M','M',0,42,0,0,0,8, // big endian
+		0x01,0x00, 0,3, 0,0,0,1, msB(w), lsB(w), 0,0), // width
+		0x01,0x01, 0,3, 0,0,0,1, msB(h), lsB(h), 0,0), // height
+		0x01,0x02, 0,3, 0,0,0,1, 0, 24, 0,0, // bits/sample
+		0x01,0x03, 0,3, 0,0,0,1, 0,  1, 0,0, // compression
+		0x01,0x06, 0,3, 0,0,0,1, 0,  2, 0,0, // RGB
+	};
+	//fs.write((char*)hdr,8);
+	//*/
+
+	return true;
+}
+
+} // al::
