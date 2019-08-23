10ab5db0601c73f8cbcddd5bf2addfc6e191d24b
Lance Putnam <putnam.lance@gmail.com>
2019-08-23 21:49:49 +0100

    Remove al_Serialize from header

--- allocore/allocore/al_Allocore.hpp
+++ allocore/allocore/al_Allocore.hpp
@@ -74,7 +74,6 @@
 #include "allocore/math/al_Spherical.hpp"
 #include "allocore/math/al_Vec.hpp"
 #include "allocore/protocol/al_OSC.hpp"
-#include "allocore/protocol/al_Serialize.hpp"
 #include "allocore/sound/al_Reverb.hpp"
 #include "allocore/sound/al_Speaker.hpp"
 #include "allocore/sound/al_AudioScene.hpp"
