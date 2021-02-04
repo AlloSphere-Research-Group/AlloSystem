e89b290ec09a3e902c133b37b3f72166d1acbdff
Lance Putnam <putnam.lance@gmail.com>
2021-02-04 12:28:04 +0000

    Fixes to timing unit tests

--- allocore/unitTests/utSystem.cpp
+++ allocore/unitTests/utSystem.cpp
@@ -8,46 +8,44 @@ int utSystem(){
 	// Timing
 //	UT_PRINTF("system: timing\n");
 	{
-		al_nsec slop = 1e7;
+		al_nsec slop = 2e7;
 		al_nsec sleepns = 1e8;
 		al_sec sleeps = 0.5;
-		al_sec slops = 0.005;
+		al_sec slops = 0.007;
 
 		assert(al_time_ns2s * 1e9 == 1);
 		assert(al_time_s2ns * 1 == 1e9);
 
-		al_nsec t, dt;
-		al_sec now, dts;
+		{
+			auto t = al_steady_time_nsec();
+			al_sleep_nsec(sleepns);
+			auto dt = al_steady_time_nsec() - t;
+			assert(aboutEqual(dt, sleepns, slop));
+		}
 
-//	UT_PRINTF("\tSleeping %ld nsec... ", (long int)sleepns);
-		t = al_time_nsec();
-		al_sleep_nsec(sleepns);
-		dt = al_time_nsec() - t;
-//	UT_PRINTF("done.\n");
-		assert(aboutEqual(dt, sleepns, slop));
+		{
+			auto t = al_steady_time_nsec();
+			al_sleep(al_time_ns2s * sleepns);
+			auto dt = al_steady_time_nsec() - t;
+			assert(aboutEqual(dt, sleepns, slop));
+		}
 
-//	UT_PRINTF("\tSleeping %g sec... ", al_time_ns2s * sleepns);
-		t = al_time_nsec();
-		al_sleep(al_time_ns2s * sleepns);
-		dt = al_time_nsec() - t;
-//	UT_PRINTF("done.\n");
-		assert(aboutEqual(dt, sleepns, slop));
+		{
+			auto t = al_system_time();
+			al_sleep_until(t + sleeps);
+			auto dt = al_system_time() - t;
+			assert(aboutEqual(dt, sleeps, slops));
+		}
 
-//	UT_PRINTF("\tSleeping from %.12g to %.12g sec... ", al_time(), al_time()+sleeps);
-		now = al_time();
-		al_sleep_until(now + sleeps);
-		dts = al_time() - now;
-//	UT_PRINTF("done.\n");
-		assert(aboutEqual(dts, sleeps, slops));
-
-		Timer tm;
-
-		tm.start();
-		al_sleep_nsec(sleepns);
-		tm.stop();
-		dt = tm.elapsed();
-		assert(aboutEqual(dt, sleepns, slop));
-		assert(al_time_ns2s * tm.elapsed() == tm.elapsedSec());
+		{
+			Timer tm;
+			tm.start();
+			al_sleep_nsec(sleepns);
+			tm.stop();
+			auto dt = tm.elapsed();
+			assert(aboutEqual(dt, sleepns, slop));
+			assert(al_time_ns2s * tm.elapsed() == tm.elapsedSec());
+		}
 	}
 
 	return 0;
