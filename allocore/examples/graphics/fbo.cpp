967a2078514567323dd53b0825f1fc96927e9031
Lance Putnam <putnam.lance@gmail.com>
2021-04-21 19:18:42 +0100

    Rework examples into al::App
    A lot of the code in these examples was boilerplate setting up the window.

--- allocore/examples/graphics/fbo.cpp
+++ allocore/examples/graphics/fbo.cpp
@@ -8,61 +8,57 @@ MipMaps:
 FBOs won't generate the mip maps automatically
 If texture filterMin is set to a MIPMAP option, then the texture will need to have mipmaps generated manually (after the FBO is unbound), using tex.generateMipmap();
 
-
 Author:
 Graham Wakefield, 2012
-
-
+Reworked into al::App by Lance Putnam, 2021
 */
 
-#include "allocore/al_Allocore.hpp"
-
+#include "allocore/io/al_App.hpp"
+#include "allocore/graphics/al_FBO.hpp"
+#include "allocore/graphics/al_Texture.hpp"
+#include "allocore/math/al_Random.hpp"
 using namespace al;
 
-int w=256, h=256;
 
-Graphics gl;
-Texture fbotex(w, h);
-RBO rbo;
-FBO fbo;
+struct MyApp : App {
 
-rnd::Random<> rng;
+	RBO rbo;
+	FBO fbo;
+	Texture fbotex{256, 256};
 
-struct MyWindow : Window{
+	MyApp(){
+		nav().pullBack(4);
+		initWindow();
+	}
 
-	bool onCreate(){
-
+	void onCreate(const ViewpointWindow& win) override {
 		// both depth and color attachees must be valid on the GPU before use:
-		rbo.resize(w, h);
+		rbo.resize(fbotex.width(), fbotex.height());
 		//fbotex.filterMin(Texture::LINEAR_MIPMAP_LINEAR);
 		fbotex.validate();
 
 		fbo.attachRBO(rbo, FBO::DEPTH_ATTACHMENT);
 		fbo.attachTexture2D(fbotex.id(), FBO::COLOR_ATTACHMENT0);
 		printf("fbo status %s\n", fbo.statusString());
-
-		return true;
 	}
 
-	bool onFrame(){
+	void onDraw(Graphics& g) override {
 
 		fbo.begin();
-
 			// capture green-world to texture:
-			gl.viewport(0, 0, fbotex.width(), fbotex.height());
-			gl.clearColor(0, 0.5, 0, 0);
-			gl.clear(Graphics::COLOR_BUFFER_BIT | Graphics::DEPTH_BUFFER_BIT);
+			g.viewport(0, 0, fbotex.width(), fbotex.height());
+			g.clearColor(0, 0.5, 0, 0);
+			g.clear(Graphics::COLOR_BUFFER_BIT | Graphics::DEPTH_BUFFER_BIT);
 
-			gl.projection(Matrix4d::perspective(45, 1, 0.1, 10));
-			gl.modelView(Matrix4d::identity());
+			g.projection(Matrix4d::perspective(45, 1, 0.1, 10));
+			g.modelView(Matrix4d::identity());
 
-			gl.begin(gl.TRIANGLES);
-			gl.color(1, 1, 0);
-				gl.vertex(rng.uniformS()*0.5, rng.uniformS()*0.5, -5);
-				gl.vertex(rng.uniformS()*0.5, rng.uniformS()*0.5, -5);
-				gl.vertex(rng.uniformS()*0.5, rng.uniformS()*0.5, -5);
-			gl.end();
-
+			g.begin(g.TRIANGLES);
+			g.color(1, 1, 0);
+				g.vertex(rnd::uniformS()*0.5, rnd::uniformS()*0.5, -5);
+				g.vertex(rnd::uniformS()*0.5, rnd::uniformS()*0.5, -5);
+				g.vertex(rnd::uniformS()*0.5, rnd::uniformS()*0.5, -5);
+			g.end();
 		fbo.end();
 
 		// generation of Mipmaps must be done manually for FBO-bound textures:
@@ -70,25 +66,16 @@ struct MyWindow : Window{
 		//fbotex.generateMipmap();
 
 		// show in blue-world:
-		gl.viewport(0, 0, width(), height());
-		gl.clearColor(0, 0, 0.5, 0);
-		gl.clear(Graphics::COLOR_BUFFER_BIT | Graphics::DEPTH_BUFFER_BIT);
+		g.viewport(0, 0, window().width(), window().height());
+		g.clearColor(0, 0, 0.5, 0);
+		g.clear(Graphics::COLOR_BUFFER_BIT | Graphics::DEPTH_BUFFER_BIT);
 
-		gl.projection(Matrix4d::ortho(0, 1, 1, 0, -1, 1));
-		gl.modelView(Matrix4d::identity());
-		fbotex.quad(gl, 0.8, 0.8, 0.1, 0.1);
-
-		return true;
+		g.projection(Matrix4d::ortho(0, 1, 1, 0, -1, 1));
+		g.modelView(Matrix4d::identity());
+		fbotex.quad(g, 0.8, 0.8, 0.1, 0.1);
 	}
 };
 
-MyWindow win;
-
 int main(){
-
-	win.append(*new StandardWindowKeyControls);
-	win.create(Window::Dim(800, 600));
-
-	MainLoop::start();
-	return 0;
+	MyApp().start();
 }
--- allocore/examples/spatial/hashSpace.cpp
+++ allocore/examples/spatial/hashSpace.cpp
@@ -1,7 +1,3 @@
-#include "allocore/al_Allocore.hpp"
-#include "allocore/spatial/al_HashSpace.hpp"
-#include "allocore/math/al_Functions.hpp"
-
 /*
 Allocore Example: Hashspace for nearest neighbor lookup.
 
@@ -11,88 +7,79 @@ This example only uses a 2D slice, but it works just as well in 3D.
 
 Author:
 Graham Wakefield 2012
+Reworked into al::App by Lance Putnam 2021
 */
 
-
-#include <stdio.h>
-
-#include <map>
-
+#include <cmath>
+#include <cstdio>
+#include "allocore/io/al_App.hpp"
+#include "allocore/math/al_Random.hpp"
+#include "allocore/spatial/al_HashSpace.hpp"
 using namespace al;
 
-Window win;
-Graphics gl;
-rnd::Random<> rng;
-Stereographic stereo;
-
-// the space has 2^6 (64) voxels per side
-// (i.e., for each of the 3 sides) and up to 10000 objects:
-HashSpace space(6, 10000);
-unsigned maxradius = space.maxRadius();
-
-// a query object to be re-used for finding neighbors
-// it will match up to 500 neighbors within a radius.
-HashSpace::Query qmany(500);
-
-// this is used for finding the nearest neighbor.
-// it will consider 6 matches and return the best.
-HashSpace::Query qnearest(6);
-
-class World : public WindowEventHandler, public InputEventHandler {
+class MyApp : public App{
 public:
-	World() {
-		win.append(*(WindowEventHandler *)this);
-		win.append(*(InputEventHandler *)this);
 
-		radius = maxradius * 0.1;
+	// the space has 2^6 (64) voxels per side
+	// (i.e., for each of the 3 sides) and up to 10000 objects:
+	HashSpace space{6, 10000};
+
+	// a query object to be re-used for finding neighbors
+	// it will match up to 500 neighbors within a radius.
+	HashSpace::Query qmany{500};
+
+	// this is used for finding the nearest neighbor.
+	// it will consider 6 matches and return the best.
+	HashSpace::Query qnearest{6};
+
+	double radius;
+
+	MyApp() {
+		radius = space.maxRadius() * 0.1;
 
 		// initialize all objects with random position
 		// just 2D for this demo
 		for (unsigned id=0; id<space.numObjects(); id++) {
 			space.move(id,
-				space.dim()*rng.uniform()*rng.uniform(),
-				space.dim()*rng.uniform()*rng.uniform(),
+				space.dim()*rnd::uniform()*rnd::uniform(),
+				space.dim()*rnd::uniform()*rnd::uniform(),
 				0
 			);
 		}
+
+		initWindow(Window::Dim(800,800));
 	}
 
-	virtual bool onMouseDrag(const Mouse& m) {
-		double dx = m.dx()/(double)win.width();
-		radius += dx * maxradius;
-		return false;
+	void onMouseDrag(const Mouse& m) override {
+		double dx = m.dx()/(double)window().width();
+		radius += dx * space.maxRadius();
 	}
 
-	virtual bool onFrame(){
-		al_sec t = al_time();
-		double mx, my;
-		mx = space.dim() * win.mouse().x() / (double)win.width();
-		my = space.dim() * win.mouse().y() / (double)win.height();
+	void onDraw(Graphics& g) override {
+		auto t = animateTime();
+		double mx = space.dim() * mouseX1();
+		double my = space.dim() * mouseY1();
 
-		Viewport vp(win.width(), win.height());
-
-		gl.viewport(vp);
-		gl.clear(Graphics::COLOR_BUFFER_BIT | Graphics::DEPTH_BUFFER_BIT);
-		gl.projection(Matrix4d::ortho(0, space.dim(), space.dim(), 0, -1, 1));
-		gl.modelView(Matrix4d::identity());
-		gl.depthTesting(0);
+		g.projection(Matrix4d::ortho(0, space.dim(), space.dim(), 0, -1, 1));
+		g.modelView(Matrix4d::identity());
+		g.depthTesting(0);
 
 		// draw space grid:
-		gl.color(0.2, 0.2, 0.2, 1);
-		gl.begin(gl.LINES);
+		g.color(0.2, 0.2, 0.2, 1);
+		g.begin(g.LINES);
 		for (unsigned x=0; x<=space.dim(); x++) {
-			gl.vertex(x, 0, 0);
-			gl.vertex(x, space.dim(), 0);
+			g.vertex(x, 0, 0);
+			g.vertex(x, space.dim(), 0);
 		}
 		for (unsigned y=0; y<=space.dim(); y++) {
-			gl.vertex(0, y, 0);
-			gl.vertex(space.dim(), y, 0);
+			g.vertex(0, y, 0);
+			g.vertex(space.dim(), y, 0);
 		}
-		gl.end();
+		g.end();
 
-		gl.pointSize(2);
-		gl.color(0.2, 0.5, 0.5);
-		gl.begin(gl.POINTS);
+		g.pointSize(2);
+		g.color(0.2, 0.5, 0.5);
+		g.begin(g.POINTS);
 		for (unsigned id=0; id<space.numObjects(); id++) {
 			HashSpace::Object& o =  space.object(id);
 
@@ -100,37 +87,37 @@ public:
 			double x = 0.5 + o.pos.x * 0.05;
 			double y = 0.5 + o.pos.y * 0.05;
 			double speed = 0.2 * sin(t + atan2(y,x));
-			space.move(id, o.pos + Vec3d(speed*rng.uniformS(), speed*rng.uniformS(), 0.));
+			space.move(id, o.pos + Vec3d(speed*rnd::uniformS(), speed*rnd::uniformS(), 0.));
 
-			gl.vertex(o.pos);
+			g.vertex(o.pos);
 		}
-		gl.end();
+		g.end();
 
 		qmany.clear();
 		int results = qmany(space, Vec3d(mx, my, 0), radius);
 
 		// draw active points:
-		gl.color(1, 0.5, 0.2);
-		gl.begin(gl.POINTS);
+		g.color(1, 0.5, 0.2);
+		g.begin(g.POINTS);
 		for (int i=0; i<results; i++) {
-			gl.vertex(qmany[i]->pos);
+			g.vertex(qmany[i]->pos);
 		}
-		gl.end();
+		g.end();
 
 		// draw a circle around the mouse:
-		gl.color(0.6, 0.1, 0.1, 1);
-		gl.begin(gl.LINE_LOOP);
+		g.color(0.6, 0.1, 0.1, 1);
+		g.begin(g.LINE_LOOP);
 		unsigned res = 30;
 		for (unsigned i=0; i<=res; i++) {
 			double p = M_2PI * i/(double)res;
-			gl.vertex(mx + sin(p)*radius, my + cos(p)*radius, 0);
+			g.vertex(mx + sin(p)*radius, my + cos(p)*radius, 0);
 		}
-		gl.end();
+		g.end();
 
 		// draw nearest neighbor links:
 		double limit = space.maxRadius()*space.maxRadius()/4.;
-		gl.color(0.2, 0.6, 0.2);
-		gl.begin(gl.LINES);
+		g.color(0.2, 0.6, 0.2);
+		g.begin(g.LINES);
 		for (unsigned id=0; id<space.numObjects(); id++) {
 			HashSpace::Object& o =  space.object(id);
 			HashSpace::Object * n = qnearest.nearest(space, &o);
@@ -138,30 +125,15 @@ public:
 				Vec3d& v = n->pos;
 				// don't draw if it is too long:
 				if ((o.pos - v).magSqr() < limit) {
-					gl.vertex(o.pos);
-					gl.vertex(v);
+					g.vertex(o.pos);
+					g.vertex(v);
 				}
 			}
 		}
-		gl.end();
-
-		return true;
+		g.end();
 	}
-
-	virtual bool onCreate(){ return true; }
-	virtual bool onDestroy(){ return true; }
-	virtual bool onResize(int dw, int dh){ return true; }
-	virtual bool onVisibility(bool v){ return true; }
-
-	double radius;
 };
 
 int main(){
-	win.create(Window::Dim(0,0, 600, 600), "HashSpace collisions");
-	win.add(new StandardWindowKeyControls());
-	World w;
-
-	MainLoop::start();
-	return 0;
+	MyApp().start();
 }
-
