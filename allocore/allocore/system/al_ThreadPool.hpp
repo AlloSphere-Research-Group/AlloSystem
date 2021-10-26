0238b6cbdf190c27d2d9657bfbc1c21c02e9c0be
Lance Putnam <putnam.lance@gmail.com>
2021-10-26 14:23:04 +0100

    Add ThreadPool class

---
+++ allocore/allocore/system/al_ThreadPool.hpp
@@ -0,0 +1,85 @@
+#ifndef INCLUDE_AL_THREAD_POOL_HPP
+#define INCLUDE_AL_THREAD_POOL_HPP
+
+/*	Allocore --
+	Multimedia / virtual environment application class library
+
+	Copyright (C) 2009. AlloSphere Research Group, Media Arts & Technology, UCSB.
+	Copyright (C) 2012. The Regents of the University of California.
+	All rights reserved.
+
+	Redistribution and use in source and binary forms, with or without
+	modification, are permitted provided that the following conditions are met:
+
+		Redistributions of source code must retain the above copyright notice,
+		this list of conditions and the following disclaimer.
+
+		Redistributions in binary form must reproduce the above copyright
+		notice, this list of conditions and the following disclaimer in the
+		documentation and/or other materials provided with the distribution.
+
+		Neither the name of the University of California nor the names of its
+		contributors may be used to endorse or promote products derived from
+		this software without specific prior written permission.
+
+	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+	AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+	IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+	ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+	LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+	CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+	SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+	INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+	CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+	ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+	POSSIBILITY OF SUCH DAMAGE.
+
+
+	File description:
+	Pool of threads to run concurrent tasks
+
+	File author(s):
+	Lance Putnam, 2021
+*/
+
+#include <condition_variable>
+#include <functional>
+#include <mutex>
+#include <thread>
+#include <queue>
+#include <vector>
+
+namespace al{
+
+/// A pool of threads for running concurrent tasks
+
+/// Creating a thread can be more expensive than the task it executes.
+/// A thread pool allows us to create a number of threads once and then simply 
+/// reuse them for arbitrary tasks.
+class ThreadPool{
+public:
+
+	typedef std::function<void(void)> Task;
+
+	/// Construct with a number of threads matching hardware concurrency
+	ThreadPool();
+
+	/// Construct with a specific number of threads
+	ThreadPool(int numThreads);
+
+	/// Joins all threads
+	~ThreadPool();
+
+	/// Push a new task onto the queue; execution begins immediately
+	void push(Task task);
+
+private:
+	std::vector<std::thread> mThreads;
+	std::queue<Task> mTasks;
+	std::condition_variable mCondition;
+	std::mutex mTasksMutex;
+	bool mTerminate = false;
+};
+
+} // namespace
+#endif
