3b3dcbbaa9d95c2b34e078b09c93bb70e003f4be
Lance Putnam <putnam.lance@gmail.com>
2022-10-26 18:01:12 +0100

    Hide RtMidi backend
    Also includes RtMidi update and eases future updates.

--- allocore/allocore/io/al_MIDI.hpp
+++ allocore/allocore/io/al_MIDI.hpp
@@ -1,52 +1,50 @@
 #ifndef INCLUDE_AL_IO_MIDI_HPP
 #define INCLUDE_AL_IO_MIDI_HPP
+/*	Allocore --
+	Multimedia / virtual environment application class library
 
-#include <exception>
-#include <iostream>
-#include <string>
-#include <vector>
-#include <queue>
+	Copyright (C) 2009. AlloSphere Research Group, Media Arts & Technology, UCSB.
+	Copyright (C) 2012. The Regents of the University of California.
+	All rights reserved.
 
-/**********************************************************************/
-/* \class MIDI
-    \brief An abstract base class for realtime MIDI input/output.
+	Redistribution and use in source and binary forms, with or without
+	modification, are permitted provided that the following conditions are met:
 
-    This class implements some common functionality for the realtime
-    MIDI input/output subclasses RtMidiIn and RtMidiOut.
+		Redistributions of source code must retain the above copyright notice,
+		this list of conditions and the following disclaimer.
 
-    RtMidi WWW site: http://music.mcgill.ca/~gary/rtmidi/
+		Redistributions in binary form must reproduce the above copyright
+		notice, this list of conditions and the following disclaimer in the
+		documentation and/or other materials provided with the distribution.
 
-    RtMidi: realtime MIDI i/o C++ classes
-    Copyright (c) 2003-2010 Gary P. Scavone
+		Neither the name of the University of California nor the names of its
+		contributors may be used to endorse or promote products derived from
+		this software without specific prior written permission.
 
-    Permission is hereby granted, free of charge, to any person
-    obtaining a copy of this software and associated documentation files
-    (the "Software"), to deal in the Software without restriction,
-    including without limitation the rights to use, copy, modify, merge,
-    publish, distribute, sublicense, and/or sell copies of the Software,
-    and to permit persons to whom the Software is furnished to do so,
-    subject to the following conditions:
+	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+	AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+	IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+	ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+	LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+	CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+	SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+	INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+	CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+	ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+	POSSIBILITY OF SUCH DAMAGE.
 
-    The above copyright notice and this permission notice shall be
-    included in all copies or substantial portions of the Software.
 
-    Any person wishing to distribute modifications to the Software is
-    requested to send the modifications to the original developer so that
-    they can be incorporated into the canonical version.
+	File description:
+	MIDI input
 
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-    IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR
-    ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
-    CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-    WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+	File author(s):
+	Lance Putnam, 2022, putnam.lance@gmail.com
 */
-/**********************************************************************/
+
+#include <functional>
+#include "allocore/system/al_Pimpl.hpp"
 
 namespace al{
-
-class MIDIIn;
 
 /// @addtogroup allocore
 /// @{
@@ -129,13 +127,10 @@ class MIDIMessage {
 public:
 	unsigned char bytes[3];
 
-	MIDIMessage(double timeStamp, unsigned port,
+	MIDIMessage(double timeStamp,
 		unsigned char b1, unsigned char b2=0, unsigned char b3=0,
-		unsigned char * data = NULL
+		unsigned char * data = nullptr
 	);
-
-	/// Get the MIDI device port
-	unsigned port() const { return mPort; }
 
 	/// Get time stamp of message
 	double timeStamp() const { return mTimeStamp; }
@@ -183,335 +178,48 @@ public:
 
 protected:
 	double mTimeStamp;
-	unsigned mPort;
 	unsigned char * mData;
 };
 
 
-/// Handles receipt of MIDI messages
-class MIDIMessageHandler{
+
+/// Receives messages from a MIDI input device
+class MIDIIn {
 public:
-	virtual ~MIDIMessageHandler(){}
+
+	MIDIIn();
 
 	/// Called when a MIDI message is received
-	virtual void onMIDIMessage(const MIDIMessage& m) = 0;
+	std::function<void(const MIDIMessage&)> onMessage;
 
-	/// Bind handler to a MIDI input
+	/// Open device on port
+	bool open(int port);
 
-	/// Calling this will override the callback function of the MIDIIn.
-	///
-	void bindTo(MIDIIn& midiIn, unsigned port=0);
+	/// Close device
+	void close();
 
-protected:
-	struct Binding{
-		MIDIIn * midiIn;
-		MIDIMessageHandler * handler;
-		unsigned port;
-	};
+	/// Whether a port was opened
+	bool opened() const;
+	operator bool() const { return opened(); }
 
-	std::vector<Binding> mBindings;
-};
+	/// Get current port (or -1 if none)
+	int port() const { return mPort; }
 
+	/// Get name of current port or empty string if none
+	std::string name() const;
 
+	/// Get number of ports with MIDI devices
+	int portCount();
 
+	/// Get name of device on port
+	std::string portName(int i);
 
-/// MIDI error reporting
-class MIDIError : public std::exception
-{
- public:
-  //! Defined RtError types.
-  enum Type {
-    WARNING,           /*!< A non-critical error. */
-    DEBUG_WARNING,     /*!< A non-critical error which might be useful for debugging. */
-    UNSPECIFIED,       /*!< The default, unspecified error type. */
-    NO_DEVICES_FOUND,  /*!< No devices found on system. */
-    INVALID_DEVICE,    /*!< An invalid device ID was specified. */
-    MEMORY_ERROR,      /*!< An error occured during memory allocation. */
-    INVALID_PARAMETER, /*!< An invalid parameter was specified to a function. */
-    INVALID_USE,       /*!< The function was called incorrectly. */
-    DRIVER_ERROR,      /*!< A system driver error occured. */
-    SYSTEM_ERROR,      /*!< A system error occured. */
-    THREAD_ERROR       /*!< A thread error occured. */
-  };
+	MIDIIn& ignoreTypes(bool sysex = true, bool midiTime = true, bool sense = true);
 
-  //! The constructor.
-  MIDIError( const std::string& message, Type type = MIDIError::UNSPECIFIED ) throw() : message_(message), type_(type) {}
-
-  //! The destructor.
-  virtual ~MIDIError( void ) throw() {}
-
-  //! Prints thrown error message to stderr.
-  virtual void printMessage( void ) const throw() { std::cerr << '\n' << message_ << "\n\n"; }
-
-  //! Returns the thrown error message type.
-  virtual const Type& getType(void) const throw() { return type_; }
-
-  //! Returns the thrown error message string.
-  virtual const std::string& getMessage(void) const throw() { return message_; }
-
-  //! Returns the thrown error message as a c-style string.
-  virtual const char* what( void ) const throw() { return message_.c_str(); }
-
- protected:
-  std::string message_;
-  Type type_;
-};
-
-
-class MIDI
-{
- public:
-
-  //! Pure virtual openPort() function.
-  virtual void openPort( unsigned int portNumber = 0, const std::string portName = std::string( "MIDI" ) ) = 0;
-
-  //! Pure virtual openVirtualPort() function.
-  virtual void openVirtualPort( const std::string portName = std::string( "MIDI" ) ) = 0;
-
-  //! Pure virtual getPortCount() function.
-  virtual unsigned int getPortCount() = 0;
-
-  //! Pure virtual getPortName() function.
-  virtual std::string getPortName( unsigned int portNumber = 0 ) = 0;
-
-  //! Pure virtual closePort() function.
-  virtual void closePort( void ) = 0;
-
- protected:
-
-  MIDI();
-  virtual ~MIDI() {};
-
-  // A basic error reporting function for internal use in the MIDI
-  // subclasses.  The behavior of this function can be modified to
-  // suit specific needs.
-  void error( MIDIError::Type type );
-
-  void *apiData_;
-  bool connected_;
-  std::string errorString_;
-};
-
-
-
-/**********************************************************************/
-/*! \class MIDIIn
-    \brief A realtime MIDI input class.
-
-    This class provides a common, platform-independent API for
-    realtime MIDI input.  It allows access to a single MIDI input
-    port.  Incoming MIDI messages are either saved to a queue for
-    retrieval using the getMessage() function or immediately passed to
-    a user-specified callback function.  Create multiple instances of
-    this class to connect to more than one MIDI device at the same
-    time.  With the OS-X and Linux ALSA MIDI APIs, it is also possible
-    to open a virtual input port to which other MIDI software clients
-    can connect.
-
-    by Gary P. Scavone, 2003-2008.
-*/
-/**********************************************************************/
-class MIDIIn : public MIDI
-{
- public:
-
-  //! User callback function type definition.
-  typedef void (*MIDICallback)( double timeStamp, std::vector<unsigned char> *message, void *userData);
-
-  //! Default constructor that allows an optional client name.
-  /*!
-      An exception will be thrown if a MIDI system initialization error occurs.
-  */
-  MIDIIn( const std::string clientName = std::string( "MIDI Input Client") );
-
-  //! If a MIDI connection is still open, it will be closed by the destructor.
-  ~MIDIIn();
-
-  //! Open a MIDI input connection.
-  /*!
-      An optional port number greater than 0 can be specified.
-      Otherwise, the default or first port found is opened.
-  */
-  void openPort( unsigned int portNumber = 0, const std::string Portname = std::string( "MIDI Input" ) );
-
-  //! Create a virtual input port, with optional name, to allow software connections (OS X and ALSA only).
-  /*!
-      This function creates a virtual MIDI input port to which other
-      software applications can connect.  This type of functionality
-      is currently only supported by the Macintosh OS-X and Linux ALSA
-      APIs (the function does nothing for the other APIs).
-  */
-  void openVirtualPort( const std::string portName = std::string( "MIDI Input" ) );
-
-  //! Set a callback function to be invoked for incoming MIDI messages.
-  /*!
-      The callback function will be called whenever an incoming MIDI
-      message is received.  While not absolutely necessary, it is best
-      to set the callback function before opening a MIDI port to avoid
-      leaving some messages in the queue.
-  */
-  void setCallback( MIDICallback callback, void *userData = 0 );
-
-  //! Cancel use of the current callback function (if one exists).
-  /*!
-      Subsequent incoming MIDI messages will be written to the queue
-      and can be retrieved with the \e getMessage function.
-  */
-  void cancelCallback();
-
-  //! Close an open MIDI connection (if one exists).
-  void closePort( void );
-
-  //! Return the number of available MIDI input ports.
-  unsigned int getPortCount();
-
-  //! Return a string identifier for the specified MIDI input port number.
-  /*!
-      An exception is thrown if an invalid port specifier is provided.
-  */
-  std::string getPortName( unsigned int portNumber = 0 );
-
-  //! Set the maximum number of MIDI messages to be saved in the queue.
-  /*!
-      If the queue size limit is reached, incoming messages will be
-      ignored.  The default limit is 1024.
-  */
-  void setQueueSizeLimit( unsigned int queueSize );
-
-  //! Specify whether certain MIDI message types should be queued or ignored during input.
-  /*!
-      By default, MIDI timing and active sensing messages are ignored
-      during message input because of their relative high data rates.
-      MIDI sysex messages are ignored by default as well.  Variable
-      values of "true" imply that the respective message type will be
-      ignored.
-  */
-  void ignoreTypes( bool midiSysex = true, bool midiTime = true, bool midiSense = true );
-
-  //! Fill the user-provided vector with the data bytes for the next available MIDI message in the input queue and return the event delta-time in seconds.
-  /*!
-      This function returns immediately whether a new message is
-      available or not.  A valid message is indicated by a non-zero
-      vector size.  An exception is thrown if an error occurs during
-      message retrieval or an input connection was not previously
-      established.
-  */
-  double getMessage( std::vector<unsigned char> *message );
-
-  // A MIDI structure used internally by the class to store incoming
-  // messages.  Each message represents one and only one MIDI message.
-  struct MIDIMessage {
-    std::vector<unsigned char> bytes;
-    double timeStamp;
-
-    // Default constructor.
-    MIDIMessage()
-      :bytes(3), timeStamp(0.0) {}
-  };
-
-  // The MIDIInData structure is used to pass private class data to
-  // the MIDI input handling function or thread.
-  struct MIDIInData {
-    std::queue<MIDIMessage> queue;
-    MIDIMessage message;
-    unsigned int queueLimit;
-    unsigned char ignoreFlags;
-    bool doInput;
-    bool firstMessage;
-    void *apiData;
-    bool usingCallback;
-    void *userCallback;
-    void *userData;
-    bool continueSysex;
-
-    // Default constructor.
-    MIDIInData()
-      : queueLimit(1024), ignoreFlags(7), doInput(false), firstMessage(true),
-        apiData(0), usingCallback(false), userCallback(0), userData(0),
-        continueSysex(false) {}
-  };
-
- private:
-
-  void initialize( const std::string& clientName );
-  MIDIInData inputData_;
-
-};
-
-
-
-
-
-
-
-/**********************************************************************/
-/*! \class MIDIOut
-    \brief A realtime MIDI output class.
-
-    This class provides a common, platform-independent API for MIDI
-    output.  It allows one to probe available MIDI output ports, to
-    connect to one such port, and to send MIDI bytes immediately over
-    the connection.  Create multiple instances of this class to
-    connect to more than one MIDI device at the same time.
-
-    by Gary P. Scavone, 2003-2008.
-*/
-/**********************************************************************/
-class MIDIOut : public MIDI
-{
- public:
-
-  //! Default constructor that allows an optional client name.
-  /*!
-      An exception will be thrown if a MIDI system initialization error occurs.
-  */
-  MIDIOut( const std::string clientName = std::string( "MIDI Output Client" ) );
-
-  //! The destructor closes any open MIDI connections.
-  ~MIDIOut();
-
-  //! Open a MIDI output connection.
-  /*!
-      An optional port number greater than 0 can be specified.
-      Otherwise, the default or first port found is opened.  An
-      exception is thrown if an error occurs while attempting to make
-      the port connection.
-  */
-  void openPort( unsigned int portNumber = 0, const std::string portName = std::string( "MIDI Output" ) );
-
-  //! Close an open MIDI connection (if one exists).
-  void closePort();
-
-  //! Create a virtual output port, with optional name, to allow software connections (OS X and ALSA only).
-  /*!
-      This function creates a virtual MIDI output port to which other
-      software applications can connect.  This type of functionality
-      is currently only supported by the Macintosh OS-X and Linux ALSA
-      APIs (the function does nothing with the other APIs).  An
-      exception is thrown if an error occurs while attempting to create
-      the virtual port.
-  */
-  void openVirtualPort( const std::string portName = std::string( "MIDI Output" ) );
-
-  //! Return the number of available MIDI output ports.
-  unsigned int getPortCount();
-
-  //! Return a string identifier for the specified MIDI port type and number.
-  /*!
-      An exception is thrown if an invalid port specifier is provided.
-  */
-  std::string getPortName( unsigned int portNumber = 0 );
-
-  //! Immediately send a single message out an open MIDI output port.
-  /*!
-      An exception is thrown if an error occurs during output or an
-      output connection was not previously established.
-  */
-  void sendMessage( std::vector<unsigned char> *message );
-
- private:
-
-  void initialize( const std::string& clientName );
+private:
+	class Impl;
+	Pimpl<Impl> mImpl;
+	int mPort = -1;
 };
 
 /// @} // end allocore group
