645576db820826a08132205d8e93519bc3bd32d2
Lance Putnam <putnam.lance@gmail.com>
2025-03-27 13:36:46 +0000

    Emscripten: change default backend from SDL2 to SDL3

--- emRun.sh
+++ emRun.sh
@@ -39,7 +39,7 @@ if [ $EXT != "c" ]; then #since so many C++ extensions
 	CXXFLAGS="-std=c++14"
 	#CXXFLAGS+=" -fno-rtti"
 fi
-EMFLAGS+=" --use-port=sdl2"
+EMFLAGS+=" --use-port=sdl3"
 #EMFLAGS+=" -s LEGACY_GL_EMULATION=1"
 #EMFLAGS+=" -s USE_WEBGL2=1" #default, recommended setting
 EMFLAGS+=" -sFULL_ES2" #OpenGL ES 2.0 emulation (req'd for client-side arrays)
