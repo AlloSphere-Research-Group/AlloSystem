b0021da9a933730896bdfdc7a307d8342bffac6a
Lance Putnam <putnam.lance@gmail.com>
2025-04-24 21:18:38 +0100

    Graphics: macOS compilation fix

--- allocore/src/graphics/al_Graphics.cpp
+++ allocore/src/graphics/al_Graphics.cpp
@@ -1065,12 +1065,14 @@ void Graphics::FrameBuffer::clear(){
 	// Check support for requested format/type
 	// ES2 has very specific rules: "... format GL_RGBA in conjunction with type GL_UNSIGNED_BYTE is always allowed..." (see https://registry.khronos.org/OpenGL-Refpages/es2.0/ and https://registry.khronos.org/OpenGL-Refpages/es2.0/xhtml/glGet.xml)
 	// Using glGet gives us the best supported values for all GL implementations with minimal fuss.
+	#ifdef GL_VERSION_3_0 /* req's gl3 */
 	{	GLint f; glGetIntegerv(GL_IMPLEMENTATION_COLOR_READ_FORMAT, &f);
 		GLint t; glGetIntegerv(GL_IMPLEMENTATION_COLOR_READ_TYPE, &t);
 		//printf("frmt:%s type:%s\n", toString(Graphics::Format(f)), toString(Graphics::DataType(t)));
 		format = Graphics::Format(f);
 		type = Graphics::DataType(t);
 	}
+	#endif
 
 	unsigned nBytes = w*h*Graphics::numComponents(format)*Graphics::numBytes(type);
 	Graphics::FrameBuffer fb;
