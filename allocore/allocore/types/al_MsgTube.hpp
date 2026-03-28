4799979bc72a44267121fa42c04baea25f0fdced
Lance Putnam <putnam.lance@gmail.com>
2026-03-28 21:24:11 +0000

    MsgTube: move code to new .cpp

--- allocore/allocore/types/al_MsgTube.hpp
+++ allocore/allocore/types/al_MsgTube.hpp
@@ -12,16 +12,9 @@
 
 #include "allocore/system/al_Config.h" // al_sec
 #include "allocore/types/al_SingleRWRingBuffer.hpp"
-#include <cstring> // memcpy
 #include <queue>
 
 #define AL_MSGTUBE_DEFAULT_SIZE_BITS (14) // 16384 bytes
-
-/*
-	A C++ class for deferred function calls
-	Using templates for type-checked functions of variable arguments
-	Adding a cache queue to seamlessly handle buffer overflow
-*/
 
 namespace al {
 
@@ -36,9 +29,7 @@ namespace al {
 class MsgTube {
 public:
 
-	/*
-		Messages in the ringbuffer have the following header structure:
-	*/
+	/// Header for messages in the queue
 	struct Header {
 		size_t size;
 		al_sec t;
@@ -46,38 +37,31 @@ public:
 	};
 
 
-	/*
-		Timestamp applied to sent messages (should increase monotonically)
-	*/
+	/// Timestamp applied to sent messages (should increase monotonically)
 	al_sec now;
 
-	/*
-		(single-reader single-writer lock-free fifo)
-	*/
 	size_t memsize;
 	SingleRWRingBuffer rb;
 
-	/*
-		Cache of messages, when the ringbuffer is full
-		TODO: set a cache limit? track when flushing fails for a prolonged period?
-	*/
+	/// Cache of messages, when the ringbuffer is full
+	// TODO: set a cache limit? track when flushing fails for a prolonged period?
 	std::queue<char *> cacheq;
+
 
 	MsgTube(int bits = AL_MSGTUBE_DEFAULT_SIZE_BITS);
 	~MsgTube();
 
+
 	void executeUntil(al_sec until);
 
-	/*
-		Copies 'data', so you can safely free it after this call
-	*/
+	// Copies 'data', so you can safely free it after this call
 	void send_data(void (*func)(al_sec t, char * data), char * data, size_t size);
 
-	void send(void (*f)(al_sec t)) {
+	void send(void (*f)(al_sec t)){
 		struct Data {
 			Header header;
 			void (*f)(al_sec t);
-			static void call(char * args) {
+			static void call(char * args){
 				const Data * d = (Data *)args;
 				(d->f)(d->header.t);
 			}
@@ -87,12 +71,12 @@ public:
 	}
 
 	template<typename A1>
-	void send(void (*f)(al_sec t, A1 a1), A1 a1) {
+	void send(void (*f)(al_sec t, A1 a1), A1 a1){
 		struct Data {
 			Header header;
 			void (*f)(al_sec t, A1 a1);
 			A1 a1;
-			static void call(char * args) {
+			static void call(char * args){
 				const Data * d = (Data *)args;
 				(d->f)(d->header.t, d->a1);
 			}
@@ -102,12 +86,12 @@ public:
 	}
 
 	template<typename A1, typename A2>
-	void send(void (*f)(al_sec t, A1 a1, A2 a2), A1 a1, A2 a2) {
+	void send(void (*f)(al_sec t, A1 a1, A2 a2), A1 a1, A2 a2){
 		struct Data {
 			Header header;
 			void (*f)(al_sec t, A1 a1, A2 a2);
 			A1 a1; A2 a2;
-			static void call(char * args) {
+			static void call(char * args){
 				const Data * d = (Data *)args;
 				(d->f)(d->header.t, d->a1, d->a2);
 			}
@@ -117,12 +101,12 @@ public:
 	}
 
 	template<typename A1, typename A2, typename A3>
-	void send(void (*f)(al_sec t, A1 a1, A2 a2, A3 a3), A1 a1, A2 a2, A3 a3) {
+	void send(void (*f)(al_sec t, A1 a1, A2 a2, A3 a3), A1 a1, A2 a2, A3 a3){
 		struct Data {
 			Header header;
 			void (*f)(al_sec t, A1 a1, A2 a2, A3 a3);
 			A1 a1; A2 a2; A3 a3;
-			static void call(char * args) {
+			static void call(char * args){
 				const Data * d = (Data *)args;
 				(d->f)(d->header.t, d->a1, d->a2, d->a3);
 			}
@@ -132,12 +116,12 @@ public:
 	}
 
 	template<typename A1, typename A2, typename A3, typename A4>
-	void send(void (*f)(al_sec t, A1 a1, A2 a2, A3 a3, A4 a4), A1 a1, A2 a2, A3 a3, A4 a4) {
+	void send(void (*f)(al_sec t, A1 a1, A2 a2, A3 a3, A4 a4), A1 a1, A2 a2, A3 a3, A4 a4){
 		struct Data {
 			Header header;
 			void (*f)(al_sec t, A1 a1, A2 a2, A3 a3, A4 a4);
 			A1 a1; A2 a2; A3 a3; A4 a4;
-			static void call(char * args) {
+			static void call(char * args){
 				const Data * d = (Data *)args;
 				(d->f)(d->header.t, d->a1, d->a2, d->a3, d->a4);
 			}
@@ -147,12 +131,12 @@ public:
 	}
 
 	template<typename A1, typename A2, typename A3, typename A4, typename A5>
-	void send(void (*f)(al_sec t, A1 a1, A2 a2, A3 a3, A4 a4, A5 a5), A1 a1, A2 a2, A3 a3, A4 a4, A5 a5) {
+	void send(void (*f)(al_sec t, A1 a1, A2 a2, A3 a3, A4 a4, A5 a5), A1 a1, A2 a2, A3 a3, A4 a4, A5 a5){
 		struct Data {
 			Header header;
 			void (*f)(al_sec t, A1 a1, A2 a2, A3 a3, A4 a4, A5 a5);
 			A1 a1; A2 a2; A3 a3; A4 a4; A5 a5;
-			static void call(char * args) {
+			static void call(char * args){
 				const Data * d = (Data *)args;
 				(d->f)(d->header.t, d->a1, d->a2, d->a3, d->a4, d->a5);
 			}
@@ -162,12 +146,12 @@ public:
 	}
 
 	template<typename A1, typename A2, typename A3, typename A4, typename A5, typename A6>
-	void send(void (*f)(al_sec t, A1 a1, A2 a2, A3 a3, A4 a4, A5 a5, A6 a6), A1 a1, A2 a2, A3 a3, A4 a4, A5 a5, A6 a6) {
+	void send(void (*f)(al_sec t, A1 a1, A2 a2, A3 a3, A4 a4, A5 a5, A6 a6), A1 a1, A2 a2, A3 a3, A4 a4, A5 a5, A6 a6){
 		struct Data {
 			Header header;
 			void (*f)(al_sec t, A1 a1, A2 a2, A3 a3, A4 a4, A5 a5, A6 a6);
 			A1 a1; A2 a2; A3 a3; A4 a4; A5 a5; A6 a6;
-			static void call(char * args) {
+			static void call(char * args){
 				const Data * d = (Data *)args;
 				(d->f)(d->header.t, d->a1, d->a2, d->a3, d->a4, d->a5, d->a6);
 			}
@@ -177,100 +161,10 @@ public:
 	}
 
 protected:
-
-	void cache(const void * src, size_t size) {
-		char * mem = new char[size];
-		memcpy(mem, src, size);
-		cacheq.push(mem);
-	}
-
-	bool flushCache() {
-		while (!cacheq.empty()) {
-			char * data = cacheq.front();
-			size_t size = *((size_t *)data);
-			if (size >= rb.writeSpace()) {
-				return false;
-			}
-
-			// send cached message:
-			rb.write(data, size);
-			delete[] data;
-			cacheq.pop();
-		}
-		return true;
-	}
-
-	void writeData(char * data, size_t size) {
-		if (size >= memsize) {
-			AL_WARN("ERROR WRITING TO RINGBUFFER");
-		} else
-		if (flushCache() && rb.writeSpace() >= size) {
-			//printf("scheduled message\n");
-			rb.write(data, size);
-		} else {
-			//printf("cached message\n");
-			char * cpy = new char[size];
-			memcpy(cpy, data, size);
-			cache(cpy, size);
-		}
-	}
+	void cache(const void * src, size_t size);
+	bool flushCache();
+	void writeData(char * data, size_t size);
 };
-
-/*
-	Inline Implementation
-*/
-#pragma mark Inline Implementation
-
-inline MsgTube :: MsgTube(int bits)
-:	now(0),
-	memsize(1<<bits),
-	rb(memsize)
-{
-	//printf("created buffer of %d size\n", memsize);
-}
-
-inline MsgTube :: ~MsgTube() {
-	// TODO: empty the cache
-}
-
-inline void MsgTube :: executeUntil(al_sec until) {
-	Header header;
-	while (rb.readSpace() > sizeof(header)) {
-		rb.peek((char *)&header, sizeof(header));
-		if (header.t > until) {
-			return;
-		}
-		char buf[header.size];
-		rb.read(buf, header.size);
-		(header.func)(buf);
-	}
-}
-
-/*
-	Copies 'data', so you can safely free it after this call
-*/
-inline void MsgTube :: send_data(void (*func)(al_sec t, char * data), char * data, size_t size) {
-	struct Data {
-		Header header;
-		void (*f)(al_sec t, char * args);
-
-		static void call(char * args) {
-			const Data * d = (Data *)args;
-			(d->f)(d->header.t, args + sizeof(Data));
-		}
-	};
-
-	size_t packetsize = sizeof(Data) + size;
-	char packet[packetsize];
-
-	Data * d = (Data *)packet;
-	d->header.size = packetsize;
-	d->header.t = now;
-	d->header.func = Data::call;
-	d->f = func;
-	memcpy(packet+sizeof(Data), data, size);
-	writeData(packet, packetsize);
-}
 
 } // al::
 #endif
